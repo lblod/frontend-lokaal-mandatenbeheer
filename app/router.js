669040285a1114985b9b56174cdf8e7bd1d978bf
Karel Kremer <karel.kremer@redpencil.io>
2023-11-22 10:40:54 +0100

    remove unnecessary routes from router

--- app/router.js
+++ app/router.js
@@ -1,5 +1,4 @@
 import EmberRouter from '@ember/routing/router';
-import { macroCondition, getOwnConfig } from '@embroider/macros';
 import config from 'frontend-loket/config/environment';
 
 export default class Router extends EmberRouter {
@@ -34,94 +33,7 @@ Router.map(function () {
       this.route('edit', { path: '/:id/edit' });
     });
 
-    this.route('personen', function () {});
     this.route('fracties', function () {});
-  });
-
-  this.route('bbcdr', function () {
-    this.route('rapporten', function () {
-      this.route('new');
-      this.route('edit', { path: '/:id' });
-    });
-  });
-
-  this.route('supervision', function () {
-    this.route('submissions', function () {
-      this.route('new');
-      this.route('edit', { path: '/:id' });
-    });
-  });
-
-  this.route('toezicht', function () {
-    this.route('inzendingen', function () {
-      this.route('new');
-      this.route('edit', { path: '/:id' });
-    });
-  });
-
-  this.route('berichtencentrum', function () {
-    this.route('berichten', function () {
-      this.route('conversatie', { path: '/:id' }, function () {});
-      if (macroCondition(getOwnConfig().controle)) {
-        this.route('new');
-      }
-    });
-  });
-
-  this.route('leidinggevendenbeheer', function () {
-    this.route('bestuursfuncties', function () {
-      this.route(
-        'bestuursfunctie',
-        { path: '/:bestuursfunctie_id' },
-        function () {
-          this.route('contact-info');
-          this.route('functionarissen', function () {
-            this.route('edit', { path: '/:functionaris_id/edit' });
-            this.route('new-person');
-            this.route('new', function () {
-              this.route('periode', { path: '/:persoon_id/periode' });
-            });
-          });
-        }
-      );
-    });
-  });
-
-  this.route('personeelsbeheer', function () {
-    this.route('personeelsaantallen', function () {
-      this.route('latest', { path: '/:dataset_id/latest' });
-      this.route('periodes', { path: '/:dataset_id/periodes' }, function () {
-        this.route('edit', { path: '/:period_id' });
-      });
-    });
-  });
-
-  this.route('subsidy', function () {
-    this.route('applications', function () {
-      this.route('available-subsidies');
-      this.route('new');
-      this.route('edit', { path: '/:id' }, function () {
-        this.route('step', { path: '/steps/:step_id' }, function () {
-          this.route('new');
-          this.route('edit', { path: '/forms/:form_id' });
-        });
-      });
-    });
-  });
-
-  this.route(
-    'lpdc-external-redirect',
-    { path: '/producten-en-dienstencatalogus' },
-    function () {
-      // We need the child routes since `/producten-en-dienstencatalogus/*` won't match the case without a subpath
-      // By using an index and named route with a path we work around that issue. The lpdc-external-redirect route
-      // then retrieves the needed data from the transition object.
-      this.route('with-path', { path: '/*path' });
-    }
-  );
-
-  this.route('route-not-found', {
-    path: '/*wildcard',
   });
 
   this.route('eredienst-mandatenbeheer', function () {
@@ -152,4 +64,8 @@ Router.map(function () {
       );
     }
   );
+
+  this.route('route-not-found', {
+    path: '/*wildcard',
+  });
 });
