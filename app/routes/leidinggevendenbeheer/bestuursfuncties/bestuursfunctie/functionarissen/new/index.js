3b8c760644f229a1ae9481f7a66fafdbf57c78fe
Ben Fidlers <ben.fidlers@qmino.com>
2023-11-22 11:29:37 +0100

    prune unused routes

--- app/routes/leidinggevendenbeheer/bestuursfuncties/bestuursfunctie/functionarissen/new/index.js
+++
@@ -1,3 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default class LeidinggevendenbeheerBestuursfunctiesBestuursfunctieFunctionarissenNewIndexRoute extends Route {}
--- app/routes/supervision/submissions.js
+++
@@ -1,3 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default class SupervisionSubmissionsRoute extends Route {}
