3f325e1787e4f9ac5467f8eb47c70a6ce576eb56
Karel Kremer <karel.kremer@redpencil.io>
2023-11-22 11:45:56 +0100

    remove unused components

--- app/components/bbcdr/report-edit-header.js
+++
@@ -1,9 +0,0 @@
-import Component from '@glimmer/component';
-import { action } from '@ember/object';
-
-export default class BbcdrReportEditHeaderComponent extends Component {
-  @action
-  close() {
-    this.onClose();
-  }
-}
