77c01a44ed909c6401f73308ecf3ba721a6eb0fa
Ben Fidlers <ben.fidlers@qmino.com>
2024-01-02 16:47:11 +0100

    add back leiddinggevenden controllers

---
+++ app/controllers/bestuursfuncties/bestuursfunctie/functionarissen/index.js
@@ -0,0 +1,22 @@
+import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+export default class LeidinggevendenbeheerBestuursfunctiesBestuursfunctieFunctionarissenIndexController extends Controller {
+  @service() router;
+
+  sort = 'start';
+  page = 0;
+  size = 20;
+
+  @tracked searchData;
+  @tracked bestuursfunctie;
+
+  @action
+  handleVoegNieuweAanstellingsperiodeClick() {
+    this.router.transitionTo(
+      'leidinggevendenbeheer.bestuursfuncties.bestuursfunctie.functionarissen.new'
+    );
+  }
+}
