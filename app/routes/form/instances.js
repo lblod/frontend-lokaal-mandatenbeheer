84beb77d5a1fc5cc0cc2ac70810674a46edaa596
Ben Fidlers <ben.fidlers@qmino.com>
2024-01-09 12:12:56 +0100

    catch errors when fetching form instances

--- app/routes/form/instances.js
+++ app/routes/form/instances.js
@@ -4,6 +4,11 @@ export default class FormInstancesRoute extends Route {
   async model() {
     const definition = this.modelFor('form');
     const response = await fetch(`/form-content/${definition.id}/instances`);
+    if (!response.ok) {
+      let error = new Error(response.statusText);
+      error.status = response.status;
+      throw error;
+    }
     const { instances } = await response.json();
     return {
       instances: instances,
