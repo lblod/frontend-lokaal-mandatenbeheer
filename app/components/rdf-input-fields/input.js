9dc7608cd69151c6d7370161ab9f91aed2055655
Ruben Bottu <ruben.bottu@qmino.com>
2024-01-17 00:28:54 +0100

    Added dropdown with hardcoded options

--- app/components/rdf-input-fields/input.js
+++ app/components/rdf-input-fields/input.js
@@ -5,10 +5,33 @@ import SimpleInputFieldComponent from '@lblod/ember-submission-form-fields/compo
 export default class RdfInputFieldsInputComponent extends SimpleInputFieldComponent {
   inputId = 'input-' + guidFor(this);
 
+  constructor() {
+    super(...arguments);
+    this.loadOptions();
+  }
+
+  loadOptions() {
+    this.searchEnabled = true;
+
+    // TODO hardcoded for now
+    this.options = [
+      { subject: 'testSubject1', label: 'testLabel1' },
+      { subject: 'testSubject2', label: 'testLabel2' },
+    ];
+  }
+
   @action
   updateValue(e) {
     if (e && typeof e.preventDefault === 'function') e.preventDefault();
     this.value = e.target.value.trim();
     super.updateValue(this.value);
+  }
+
+  @action
+  updateSelection(option) {
+    this.selected = option;
+
+    this.hasBeenFocused = true;
+    super.updateValidations();
   }
 }
