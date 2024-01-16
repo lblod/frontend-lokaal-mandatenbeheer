addc40d2d1fa90be4a8288fecadd11cd5baf0ca2
Ben Fidlers <ben.fidlers@qmino.com>
2024-01-16 10:03:08 +0100

    refactor rdf namespaces

--- app/routes/form/new.js
+++ app/routes/form/new.js
@@ -1,6 +1,6 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-import { RDF, FORM } from '@lblod/submission-form-helpers';
+import { RDF, FORM } from '../../rdf/namespaces';
 import { NamedNode } from 'rdflib';
 import { ForkingStore } from '@lblod/ember-submission-form-fields';
 import { FORM_GRAPH, META_GRAPH, SOURCE_GRAPH } from '../../utils/constants';
