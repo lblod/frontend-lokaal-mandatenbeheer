ce6829561be9a84efa2edda85ba1ff3741645ffb
Ben Fidlers <ben.fidlers@qmino.com>
2024-01-17 19:19:44 +0100

    working version multi instance selector

--- app/components/rdf-input-fields/instance-multi-selector.js
+++ app/components/rdf-input-fields/instance-multi-selector.js
@@ -1,9 +1,13 @@
 import { action } from '@ember/object';
 import { guidFor } from '@ember/object/internals';
 import { tracked } from '@glimmer/tracking';
-import { triplesForPath } from '@lblod/submission-form-helpers';
+import {
+  triplesForPath,
+  updateSimpleFormValue,
+} from '@lblod/submission-form-helpers';
 import InputFieldComponent from '@lblod/ember-submission-form-fields/components/rdf-input-fields/input-field';
 import { restartableTask, timeout } from 'ember-concurrency';
+import { NamedNode } from 'rdflib';
 
 export default class RdfInstanceMultiSelectorComponent extends InputFieldComponent {
   inputId = 'select-' + guidFor(this);
@@ -19,45 +23,50 @@ export default class RdfInstanceMultiSelectorComponent extends InputFieldCompone
   constructor() {
     super(...arguments);
     this.loadOptions();
-    //this.loadProvidedValue();
+    this.loadProvidedValue();
   }
 
   loadOptions() {
     // TODO hardcoded for now
     this.options = [
-      { subject: 'testMultiSubject1', label: 'testMultiLabel1' },
-      { subject: 'testMultiSubject2', label: 'testMultiLabel2' },
+      {
+        subject: new NamedNode('http://example.org/MultiTest1'),
+        label: 'testLabel1',
+      },
+      {
+        subject: new NamedNode('http://example.org/MultiTest2'),
+        label: 'testLabel2',
+      },
     ];
   }
 
-  /* loadProvidedValue() {
+  loadProvidedValue() {
     if (this.isValid) {
       const matches = triplesForPath(this.storeOptions, true).values;
       this.selected = this.options.filter((opt) =>
         matches.find((m) => m.equals(opt.subject))
       );
     }
-  } */
+  }
 
   @action
   updateSelection(options) {
     this.selected = options;
 
     // Retrieve options in store
-    /* const matches = triplesForPath(this.storeOptions, true).values;
+    const matches = triplesForPath(this.storeOptions, true).values;
     const matchingOptions = matches.filter((m) =>
       this.options.find((opt) => m.equals(opt.subject))
-    ); */
+    );
 
     // Cleanup old value(s) in the store
-    /* matchingOptions
+    matchingOptions
       .filter((m) => !options.find((opt) => m.equals(opt.subject)))
       .forEach((m) => updateSimpleFormValue(this.storeOptions, undefined, m));
 
-    */
     // Insert new value in the store
     options
-      //.filter((opt) => !matchingOptions.find((m) => opt.subject.equals(m)))
+      .filter((opt) => !matchingOptions.find((m) => opt.subject.equals(m)))
       .forEach((option) =>
         updateSimpleFormValue(this.storeOptions, option.subject)
       );
