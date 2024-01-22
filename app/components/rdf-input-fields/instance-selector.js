fd860a70ed52dd1db3f040f854b84c1adba06d2a
Ben Fidlers <ben.fidlers@qmino.com>
2024-01-22 08:42:30 +0100

    poc fetch + search instance-selector

--- app/components/rdf-input-fields/instance-selector.js
+++ app/components/rdf-input-fields/instance-selector.js
@@ -2,14 +2,18 @@ import { action } from '@ember/object';
 import { guidFor } from '@ember/object/internals';
 import InputFieldComponent from '@lblod/ember-submission-form-fields/components/rdf-input-fields/input-field';
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
 import {
   updateSimpleFormValue,
   triplesForPath,
 } from '@lblod/submission-form-helpers';
-import { NamedNode } from 'rdflib';
+import { NamedNode, Namespace } from 'rdflib';
+import { task, timeout } from 'ember-concurrency';
 
 export default class RdfInstanceSelectorComponent extends InputFieldComponent {
   inputId = 'input-' + guidFor(this);
+
+  @service store;
 
   @tracked selected = null;
   @tracked options = [];
@@ -17,24 +21,54 @@ export default class RdfInstanceSelectorComponent extends InputFieldComponent {
 
   constructor() {
     super(...arguments);
-    this.loadOptions();
+    this.load();
+  }
+
+  async load() {
+    await this.loadOptions();
     this.loadProvidedValue();
   }
 
-  loadOptions() {
-    // TODO hardcoded for now
-    const test1 = new NamedNode('http://example.org/test1');
-    const test2 = new NamedNode('http://example.org/test2');
-    this.options = [
-      {
-        subject: test1,
-        label: 'testLabel1',
+  async fetchInput() {
+    const formGraph = this.args.graphs.formGraph;
+    const FORM = new Namespace('http://lblod.data.gift/vocabularies/forms/');
+
+    const instanceLabelProperty = this.args.formStore.match(
+      this.args.field.uri,
+      FORM('instanceLabelProperty'),
+      undefined,
+      formGraph
+    )[0].object.value;
+    const instanceApiUrl = this.args.formStore.match(
+      this.args.field.uri,
+      FORM('instanceApiUrl'),
+      undefined,
+      formGraph
+    )[0].object.value;
+
+    const pageSize = 5;
+    const response = await fetch(`${instanceApiUrl}?page[size]=${pageSize}`, {
+      headers: {
+        Accept: 'application/vnd.api+json',
       },
-      {
-        subject: test2,
-        label: 'testLabel2',
-      },
-    ];
+    });
+    if (!response.ok) {
+      let error = new Error(response.statusText);
+      error.status = response.status;
+      throw error;
+    }
+    const result = await response.json();
+
+    const options = result.data.map((m) => {
+      const subject = new NamedNode(m.attributes['uri']);
+      return { subject: subject, label: m.attributes[instanceLabelProperty] };
+    });
+    return options;
+  }
+
+  async loadOptions() {
+    const instances = await this.fetchInput();
+    this.options = instances;
   }
 
   loadProvidedValue() {
@@ -67,4 +101,28 @@ export default class RdfInstanceSelectorComponent extends InputFieldComponent {
     this.hasBeenFocused = true;
     super.updateValidations();
   }
+
+  @task(function* (term) {
+    yield timeout(200);
+    // let url = `${instanceApiUrl}?page[size]=${pageSize}`;
+    // let url = `/fracties?page[size]=5?filter=${term}`;
+    let url = `/fracties?filter=${term}`;
+    return fetch(url, {
+      headers: {
+        Accept: 'application/vnd.api+json',
+      },
+    })
+      .then((resp) => resp.json())
+      .then((json) => json.data)
+      .then((data) =>
+        data.map((m) => {
+          const subject = new NamedNode(m.attributes['uri']);
+          return {
+            subject: subject,
+            label: m.attributes['naam'],
+          };
+        })
+      );
+  })
+  searchRepo;
 }
