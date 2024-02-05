130c4f1598508615877843b40584cf143e4a3c54
Ben Fidlers <ben.fidlers@qmino.com>
2024-02-05 12:05:38 +0100

    update routing leidinggevendenbeheer

--- app/router.js
+++ app/router.js
@@ -39,6 +39,25 @@ Router.map(function () {
     });
   });
 
+  this.route('leidinggevendenbeheer', function () {
+    this.route('bestuursfuncties', function () {
+      this.route(
+        'bestuursfunctie',
+        { path: '/:bestuursfunctie_id' },
+        function () {
+          this.route('contact-info');
+          this.route('functionarissen', function () {
+            this.route('edit', { path: '/:functionaris_id/edit' });
+            this.route('new-person');
+            this.route('new', function () {
+              this.route('periode', { path: '/:persoon_id/periode' });
+            });
+          });
+        }
+      );
+    });
+  });
+
   this.route('legacy', function () {
     this.route('mandatenbeheer', function () {
       this.route('mandatarissen', function () {
