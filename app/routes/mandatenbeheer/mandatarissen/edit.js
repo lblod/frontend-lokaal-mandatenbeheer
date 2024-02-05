32fb51ee722751dd611cd40b49ac3203313e519e
Ben Fidlers <ben.fidlers@qmino.com>
2024-02-05 11:36:26 +0100

    moved model logic from component to route

--- app/routes/mandatenbeheer/mandatarissen/edit.js
+++ app/routes/mandatenbeheer/mandatarissen/edit.js
@@ -8,11 +8,41 @@ export default class MandatenbeheerMandatarissenEditRoute extends Route {
   async model(params) {
     const parentModel = this.modelFor('mandatenbeheer');
     const persoon = await this.store.findRecord('persoon', params.id);
+    const mandatarissen = this.getMandatarissen(
+      parentModel.bestuursorganen,
+      persoon
+    );
 
     return RSVP.hash({
       bestuurseenheid: parentModel.bestuurseenheid,
       bestuursorganen: parentModel.bestuursorganen,
       persoon,
+      mandatarissen,
     });
+  }
+
+  async getMandatarissen(bestuursorganen, persoon) {
+    let bestuursorganenIds = bestuursorganen.map((o) => o.get('id'));
+    let queryParams = {
+      filter: {
+        'is-bestuurlijke-alias-van': {
+          id: persoon.id,
+        },
+        bekleedt: {
+          'bevat-in': {
+            id: bestuursorganenIds.join(','),
+          },
+        },
+      },
+      include: [
+        'is-bestuurlijke-alias-van',
+        'bekleedt.bestuursfunctie',
+        'beleidsdomein',
+        'heeft-lidmaatschap.binnen-fractie',
+      ].join(','),
+    };
+
+    let mandatarissen = await this.store.query('mandataris', queryParams);
+    return mandatarissen.slice();
   }
 }
