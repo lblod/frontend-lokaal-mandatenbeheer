a65ab4462c4c0d840296397e82bfc690d97acac7
Ben Fidlers <ben.fidlers@qmino.com>
2024-02-06 10:42:23 +0100

    remove comment

--- app/controllers/leidinggevendenbeheer/bestuursfunctie/functionarissen/edit.js
+++ app/controllers/leidinggevendenbeheer/bestuursfunctie/functionarissen/edit.js
@@ -5,7 +5,6 @@ import { action } from '@ember/object';
 export default class LeidinggevendenbeheerBestuursfunctieFunctionarissenEditController extends Controller {
   @service router;
 
-  // TODO make sure filled in data is reset after cancelling
   @action
   cancel() {
     this.router.transitionTo(
