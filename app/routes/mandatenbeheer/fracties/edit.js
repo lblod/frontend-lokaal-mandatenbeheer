2bbc02d7feaa77c4146e1a047a9764f32fb98a2a
Ruben Bottu <ruben.bottu@qmino.com>
2024-02-06 11:41:07 +0100

    Added fractie form ID to well-known-ids

--- app/routes/mandatenbeheer/fracties/edit.js
+++ app/routes/mandatenbeheer/fracties/edit.js
@@ -1,13 +1,13 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import { getForm } from 'frontend-lmb/utils/get-form';
+import { FRACTIE_FORM_ID } from 'frontend-lmb/utils/well-known-ids';
 
 export default class MandaatbeheerFractiesEditRoute extends Route {
   @service store;
 
   async model(params) {
-    const formId = 'fractie';
-    const form = await getForm(this.store, formId);
+    const form = await getForm(this.store, FRACTIE_FORM_ID);
 
     return { form, instanceId: params.id };
   }
