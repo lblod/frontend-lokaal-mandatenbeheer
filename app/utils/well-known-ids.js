b7189cf098b4ae8351d753777a277db64e3d9c5c
Ben Fidlers <ben.fidlers@qmino.com>
2024-02-07 16:11:24 +0100

    add edit aanstellingsperiode form

--- app/utils/well-known-ids.js
+++ app/utils/well-known-ids.js
@@ -3,3 +3,4 @@ export const FEMALE_ID = '5ab0e9b8a3b2ca7c5e000029';
 
 export const FRACTIE_FORM_ID = 'fractie';
 export const CONTACTINFO_FORM_ID = 'contactinfo';
+export const FUNCTIONARIS_EDIT_FORM_ID = 'functionaris-edit';
