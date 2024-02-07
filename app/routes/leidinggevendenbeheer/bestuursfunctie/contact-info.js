3e21217f37437e3ad38f118de5e78467f0eedfad
Ben Fidlers <ben.fidlers@qmino.com>
2024-02-07 09:07:44 +0100

    add contact info form id constant

--- app/routes/leidinggevendenbeheer/bestuursfunctie/contact-info.js
+++ app/routes/leidinggevendenbeheer/bestuursfunctie/contact-info.js
@@ -1,6 +1,7 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import { getForm } from 'frontend-lmb/utils/get-form';
+import { CONTACTINFO_FORM_ID } from 'frontend-lmb/utils/well-known-ids';
 
 export default class LeidinggevendenbeheerBestuursfunctiesBestuursfunctieContactInfoRoute extends Route {
   @service store;
@@ -20,7 +21,7 @@ export default class LeidinggevendenbeheerBestuursfunctiesBestuursfunctieContact
       await bestuursfunctie.save();
     }
 
-    const form = await getForm(this.store, 'contactinfo');
+    const form = await getForm(this.store, CONTACTINFO_FORM_ID);
 
     return {
       bestuurseenheid,
--- app/utils/well-known-ids.js
+++ app/utils/well-known-ids.js
@@ -1,3 +1,5 @@
 export const MALE_ID = '5ab0e9b8a3b2ca7c5e000028';
 export const FEMALE_ID = '5ab0e9b8a3b2ca7c5e000029';
+
 export const FRACTIE_FORM_ID = 'fractie';
+export const CONTACTINFO_FORM_ID = 'contactinfo';
