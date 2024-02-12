1c91a3a029b994053969a4690fd21dd2f386c26a
karel kremer <karel.kremer@redpencil.io>
2024-02-12 11:56:52 +0100

    remove unused prefixes

--- app/utils/bestuursorgaan-meta-ttl.js
+++ app/utils/bestuursorgaan-meta-ttl.js
@@ -5,9 +5,7 @@ export const getBestuursorgaanMetaTtl = (bestuursorgaan) => {
   const bestuursorgaanUri = bestuursorgaan.uri;
 
   return `
-    @prefix mandaat: <http://data.vlaanderen.be/ns/mandaat#> .
     @prefix ext: <http://mu.semte.ch/vocabularies/ext/> .
-    @prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
 
     ext:applicationContext ext:currentBestuursorgaan <${bestuursorgaanUri}> .
   `;
