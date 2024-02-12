c45b672d0a2dd071a30fd9cafc8e15a08e8712c3
Ben Fidlers <ben.fidlers@qmino.com>
2024-02-12 15:42:42 +0100

    copy mandataris table

---
+++ app/components/mandatenbeheer/mandataris/mandataris-table.js
@@ -0,0 +1,14 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { service } from '@ember/service';
+
+export default class MandatenbeheerMandatarisTableComponent extends Component {
+  @service router;
+
+  @action
+  async removeMandataris(mandataris) {
+    await mandataris.destroyRecord();
+
+    this.router.refresh('legacy.mandatenbeheer.mandatarissen');
+  }
+}
