44c471223418a13f591ab329b8007cc3e1fc8d21
Ben Fidlers <ben.fidlers@qmino.com>
2024-02-13 16:24:06 +0100

    extend rijksregister input field

--- app/components/rdf-input-fields/rijksregister-input.js
+++ app/components/rdf-input-fields/rijksregister-input.js
@@ -1,6 +1,41 @@
-import InputFieldComponent from '@lblod/ember-submission-form-fields/components/rdf-input-fields/input';
+import { action } from '@ember/object';
+import InputFieldComponent from '@lblod/ember-submission-form-fields/components/rdf-input-fields/input-field';
 import { guidFor } from '@ember/object/internals';
+import {
+  updateSimpleFormValue,
+  triplesForPath,
+} from '@lblod/submission-form-helpers';
 
 export default class RDFRijksRegisterInput extends InputFieldComponent {
   inputId = 'rrn-' + guidFor(this);
+
+  constructor() {
+    super(...arguments);
+    this.loadProvidedValue();
+  }
+
+  loadProvidedValue() {
+    const matches = triplesForPath(this.storeOptions);
+    if (matches.values.length > 0) {
+      this.nodeValue = matches.values[0];
+      this.value = matches.values[0].value;
+    }
+  }
+
+  @action
+  updateValue(event) {
+    if (event && typeof event.preventDefault === 'function')
+      event.preventDefault();
+    this.value = event.target.value.trim();
+    updateSimpleFormValue(this.storeOptions, this.value, this.nodeValue);
+
+    this.hasBeenFocused = true;
+    this.loadProvidedValue();
+
+    this.updateValidations();
+  }
+
+  updateValidations() {
+    // TODO some validation needs to happen here.
+  }
 }
