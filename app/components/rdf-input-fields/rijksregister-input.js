20343ecd560e8135be34505772e14fd4adc4d9a1
Ben Fidlers <ben.fidlers@qmino.com>
2024-02-14 08:21:06 +0100

    update rijksregister input to relation

--- app/components/rdf-input-fields/rijksregister-input.js
+++ app/components/rdf-input-fields/rijksregister-input.js
@@ -1,38 +1,82 @@
+import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 import InputFieldComponent from '@lblod/ember-submission-form-fields/components/rdf-input-fields/input-field';
 import { guidFor } from '@ember/object/internals';
 import {
   updateSimpleFormValue,
   triplesForPath,
 } from '@lblod/submission-form-helpers';
+import { isValidRijksregisternummer } from 'frontend-lmb/utils/rijksregisternummer';
 
 export default class RDFRijksRegisterInput extends InputFieldComponent {
   inputId = 'rrn-' + guidFor(this);
+
+  @service store;
+  @tracked value;
 
   constructor() {
     super(...arguments);
     this.loadProvidedValue();
   }
 
-  loadProvidedValue() {
+  async loadProvidedValue() {
     const matches = triplesForPath(this.storeOptions);
     if (matches.values.length > 0) {
-      this.nodeValue = matches.values[0];
-      this.value = matches.values[0].value;
+      const queryResult = await this.store.query('identificator', {
+        filter: { ':uri:': matches.values[0].value },
+      });
+      if (queryResult.length >= 1) {
+        const identificator = queryResult.at(0);
+        this.nodeValue = identificator.uri;
+        this.value = identificator.identificator;
+      }
     }
   }
 
   @action
-  updateValue(event) {
+  async updateValue(event) {
     if (event && typeof event.preventDefault === 'function')
       event.preventDefault();
-    this.value = event.target.value.trim();
+    const rijksregisternummer = event.target.value.trim();
+
+    // TODO ideally this should also check if the other fields are compatible with the rrn
+    // such as birthdate and sex, but this might be more trouble then it's worth.
+    if (!isValidRijksregisternummer(rijksregisternummer)) {
+      // Throw error here.
+      console.log('rijksregister nummer not valid');
+      return;
+    }
+    const identificator = await this.loadOrCreateRijksregister(
+      rijksregisternummer
+    );
+
+    this.value = identificator.uri;
+    this.nodeValue = identificator.identificator;
+
     updateSimpleFormValue(this.storeOptions, this.value, this.nodeValue);
 
     this.hasBeenFocused = true;
-    this.loadProvidedValue();
 
     this.updateValidations();
+  }
+
+  async loadOrCreateRijksregister(rrn) {
+    let identificator;
+    let queryResult = await this.store.query('identificator', {
+      filter: { ':exact:identificator': rrn },
+    });
+
+    if (queryResult.length >= 1) {
+      identificator = queryResult.at(0);
+    } else {
+      identificator = await this.store
+        .createRecord('identificator', {
+          identificator: rrn,
+        })
+        .save();
+    }
+    return identificator;
   }
 
   updateValidations() {
--- app/models/identificator.js
+++ app/models/identificator.js
@@ -2,4 +2,5 @@ import Model, { attr } from '@ember-data/model';
 
 export default class IdentificatorModel extends Model {
   @attr identificator;
+  @attr uri;
 }
