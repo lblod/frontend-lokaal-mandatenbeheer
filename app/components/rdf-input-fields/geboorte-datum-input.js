dfdfb4bee3ee444ac71e9455a7124a98f790321e
Ben Fidlers <ben.fidlers@qmino.com>
2024-02-14 13:15:51 +0100

    upgrade geboorte datum input field

--- app/components/rdf-input-fields/geboorte-datum-input.js
+++ app/components/rdf-input-fields/geboorte-datum-input.js
@@ -1,15 +1,58 @@
+import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 import { guidFor } from '@ember/object/internals';
-import SimpleInputFieldComponent from '@lblod/ember-submission-form-fields/components/rdf-input-fields/simple-value-input-field';
-import { XSD } from '@lblod/submission-form-helpers';
-import { literal } from 'rdflib';
+import InputFieldComponent from '@lblod/ember-submission-form-fields/components/rdf-input-fields/input-field';
+import { triplesForPath } from '@lblod/submission-form-helpers';
+import { replaceSingleFormValue } from 'frontend-lmb/utils/replaceSingleFormValue';
 
-export default class RDFGeboorteDatumInput extends SimpleInputFieldComponent {
+export default class RDFGeboorteDatumInput extends InputFieldComponent {
   inputId = 'birthdate-' + guidFor(this);
 
+  @service store;
+  @tracked geboortedatum;
+
+  constructor() {
+    super(...arguments);
+    this.loadProvidedValue();
+  }
+
+  async loadProvidedValue() {
+    const matches = triplesForPath(this.storeOptions);
+    if (matches.values.length > 0) {
+      const queryResult = await this.store.query('geboorte', {
+        filter: { ':uri:': matches.values[0].value },
+      });
+      if (queryResult.length >= 1) {
+        const geboorte = queryResult.at(0);
+        this.geboortedatum = geboorte.datum;
+      }
+    }
+  }
+
   @action
-  updateValue(isoDate) {
-    const newDate = isoDate ? literal(isoDate, XSD('date')) : null;
-    super.updateValue(newDate);
+  async updateValue(isoDate, date) {
+    const geboorte = await this.loadOrCreateGeboorte(isoDate, date);
+
+    replaceSingleFormValue(this.storeOptions, geboorte.uri);
+
+    this.hasBeenFocused = true;
+    this.updateValidations();
+  }
+
+  async loadOrCreateGeboorte(isoDate, date) {
+    let geboorte;
+    const queryResult = await this.store.query('geboorte', {
+      filter: { datum: isoDate },
+    });
+
+    if (queryResult.length >= 1) {
+      geboorte = queryResult.at(0);
+    } else {
+      geboorte = await this.store
+        .createRecord('geboorte', { datum: date })
+        .save();
+    }
+    return geboorte;
   }
 }
