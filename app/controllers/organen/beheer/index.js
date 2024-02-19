369ef70c437d87750564e493c4030e3bd1bb9fd8
Ben Fidlers <ben.fidlers@qmino.com>
2024-02-19 11:23:22 +0100

    fix sorting, pagination etc of bestuursorgaan table

--- app/controllers/organen/beheer/index.js
+++ app/controllers/organen/beheer/index.js
@@ -1,9 +1,15 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class OrganenBeheerIndexController extends Controller {
   @service router;
+
+  @tracked filter = '';
+  @tracked page = 0;
+  sort = 'naam';
+  size = 5;
 
   @action
   createNewOrgaan() {
--- app/routes/organen/beheer/index.js
+++ app/routes/organen/beheer/index.js
@@ -1,14 +1,51 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class OrganenbeheerIndexRoute extends Route {
   @service store;
 
-  async model() {
+  queryParams = {
+    filter: { refreshModel: true },
+    page: { refreshModel: true },
+    size: { refreshModel: true },
+    sort: { refreshModel: true },
+  };
+
+  async model(params) {
     const parentModel = this.modelFor('organen');
 
+    const options = this.getOptions(params, parentModel.bestuurseenheid);
+    const bestuursorganen = await this.store.query('bestuursorgaan', options);
+
     return {
-      bestuursorganen: parentModel.bestuursorganen,
+      bestuursorganen,
     };
+  }
+
+  getOptions(params, bestuurseenheid) {
+    const queryParams = {
+      sort: params.sort,
+      page: {
+        number: params.page,
+        size: params.size,
+      },
+      filter: {
+        bestuurseenheid: {
+          id: bestuurseenheid.id,
+        },
+      },
+    };
+
+    if (params.filter) {
+      queryParams['filter']['naam'] = params.filter;
+    }
+
+    return queryParams;
+  }
+
+  @action
+  reloadModel() {
+    this.refresh();
   }
 }
