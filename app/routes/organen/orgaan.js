48a97970ee6e349b0ce9c7165c9235a7ce2ee4e5
Ruben Bottu <ruben.bottu@qmino.com>
2024-02-19 12:36:17 +0100

    Added readonly show bestuursorgaan route

---
+++ app/routes/organen/orgaan.js
@@ -0,0 +1,26 @@
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import { getFormFrom } from 'frontend-lmb/utils/get-form';
+import { BESTUURSORGAAN_FORM_ID } from 'frontend-lmb/utils/well-known-ids';
+import RSVP from 'rsvp';
+
+export default class OrganenOrgaanRoute extends Route {
+  @service store;
+  async model(params) {
+    const bestuursorgaan = this.store.findRecord(
+      'bestuursorgaan',
+      params.orgaan_id,
+      {
+        include: 'classificatie,heeft-tijdsspecialisaties',
+      }
+    );
+
+    const formDefinition = getFormFrom(this.store, BESTUURSORGAAN_FORM_ID);
+
+    return RSVP.hash({
+      form: formDefinition,
+      instanceId: params.orgaan_id,
+      bestuursorgaan,
+    });
+  }
+}
