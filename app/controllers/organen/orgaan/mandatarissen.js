c138087815ecc0caa533b178d70d04fde3f6dadf
Ben Fidlers <ben.fidlers@qmino.com>
2024-02-21 15:33:53 +0100

    add search to orgaan mandataris page

--- app/controllers/organen/orgaan/mandatarissen.js
+++ app/controllers/organen/orgaan/mandatarissen.js
@@ -2,6 +2,8 @@ import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { restartableTask, timeout } from 'ember-concurrency';
+import { SEARCH_TIMEOUT } from 'frontend-lmb/utils/constants';
 
 export default class OrganenMandatarissenController extends Controller {
   @service router;
@@ -10,6 +12,15 @@ export default class OrganenMandatarissenController extends Controller {
   @tracked page = 0;
   sort = 'is-bestuurlijke-alias-van.achternaam';
   size = 20;
+
+  @tracked searchData;
+
+  @restartableTask
+  *search(searchData) {
+    yield timeout(SEARCH_TIMEOUT);
+    this.page = 0;
+    this.filter = searchData;
+  }
 
   @action
   selectPeriod(period) {
