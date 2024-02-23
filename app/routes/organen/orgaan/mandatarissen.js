ee5d9bd47d2bd798bed38cf0a51da795d39e0798
Ben Fidlers <ben.fidlers@qmino.com>
2024-02-23 09:57:37 +0100

    fix typo

--- app/routes/organen/orgaan/mandatarissen.js
+++ app/routes/organen/orgaan/mandatarissen.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
-export default class OrganenMandatarissenRout extends Route {
+export default class OrganenMandatarissenRoute extends Route {
   @service store;
 
   queryParams = {
