6486f094ee69020d4528e9de64be38257650438a
Ben Fidlers <ben.fidlers@qmino.com>
2024-02-23 09:26:49 +0100

    basic search page mandataris

---
+++ app/controllers/mandataris-search.js
@@ -0,0 +1,29 @@
+import Controller from '@ember/controller';
+import { restartableTask, timeout } from 'ember-concurrency';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { SEARCH_TIMEOUT } from 'frontend-lmb/utils/constants';
+
+export default class MandatarisSearchController extends Controller {
+  @service router;
+
+  @tracked filter = '';
+  @tracked page = 0;
+  sort = 'is-bestuurlijke-alias-van.achternaam';
+  size = 20;
+
+  @tracked searchData;
+
+  @restartableTask
+  *search(searchData) {
+    yield timeout(SEARCH_TIMEOUT);
+    this.page = 0;
+    this.filter = searchData;
+  }
+
+  @action
+  goToPersonPage() {
+    this.router.transitionTo('under-construction');
+  }
+}
---
+++ app/routes/mandataris-search.js
@@ -0,0 +1,46 @@
+import Route from '@ember/routing/route';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+
+export default class MandatarisSearchRoute extends Route {
+  @service store;
+
+  queryParams = {
+    filter: { refreshModel: true },
+    page: { refreshModel: true },
+    size: { refreshModel: true },
+    sort: { refreshModel: true },
+  };
+
+  async model(params) {
+    const options = this.getOptions(params);
+    const mandatarissen = await this.store.query('mandataris', options);
+
+    return {
+      mandatarissen,
+    };
+  }
+
+  getOptions(params) {
+    const queryParams = {
+      sort: params.sort,
+      page: {
+        number: params.page,
+        size: params.size,
+      },
+      filter: {
+        'is-bestuurlijke-alias-van': params.filter,
+      },
+      include: ['is-bestuurlijke-alias-van', 'bekleedt.bestuursfunctie'].join(
+        ','
+      ),
+    };
+
+    return queryParams;
+  }
+
+  @action
+  reloadModel() {
+    this.refresh();
+  }
+}
