646baacd711751b7220f9201f87c3215e1f03ff9
karel kremer <karel.kremer@redpencil.io>
2024-02-23 10:16:49 +0100

    allow edit of mandaten

--- app/routes/mandaat/edit.js
+++ app/routes/mandaat/edit.js
@@ -1,7 +1,18 @@
 import Route from '@ember/routing/route';
+import { getFormFrom } from 'frontend-lmb/utils/get-form';
+import { MANDAAT_FORM_ID } from 'frontend-lmb/utils/well-known-ids';
+import RSVP from 'rsvp';
+import { inject as service } from '@ember/service';
 
 export default class MandaatEditRoute extends Route {
-  model(params) {
-    console.log(`TODO implement fetching ${params.id}`);
+  @service store;
+
+  async model(params) {
+    const form = getFormFrom(this.store, MANDAAT_FORM_ID);
+    const mandaat = await this.store.findRecord('mandaat', params.id, {
+      include: 'bestuursfunctie',
+    });
+
+    return RSVP.hash({ form, instanceId: params.id, mandaat });
   }
 }
