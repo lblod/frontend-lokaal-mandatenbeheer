308ffddb416577d941d4099db2905181ef912c02
Ben Fidlers <ben.fidlers@qmino.com>
2024-02-26 16:23:47 +0100

    clean up unused code

--- app/controllers/mandatarissen/persoon.js
+++ app/controllers/mandatarissen/persoon.js
@@ -1,13 +1,9 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 
 export default class MandatarissenPersoonController extends Controller {
   @service router;
-  @service store;
-
-  @tracked createMode = false;
 
   @action
   createMandataris() {
