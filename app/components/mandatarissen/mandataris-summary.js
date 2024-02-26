a3354464736b3e5087bfca70f3c4a96b7b441d02
Ben Fidlers <ben.fidlers@qmino.com>
2024-02-26 15:52:42 +0100

    add new mandataris summary

---
+++ app/components/mandatarissen/mandataris-summary.js
@@ -0,0 +1,34 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+
+export default class MandatenbeheerMandatarisSummaryComponent extends Component {
+  @service router;
+
+  get rol() {
+    return this.args.mandataris.bekleedt.get('bestuursfunctie').get('label');
+  }
+  get status() {
+    return this.args.mandataris.status.get('label');
+  }
+  get fractie() {
+    return this.args.mandataris.heeftLidmaatschap.get('binnenFractie')
+      ? this.args.mandataris.heeftLidmaatschap.get('binnenFractie').get('naam')
+      : '';
+  }
+
+  get formattedBeleidsdomein() {
+    const beleidsdomeinenPromise = this.args.mandataris.beleidsdomein;
+    if (!beleidsdomeinenPromise.length) {
+      return [];
+    }
+    return beleidsdomeinenPromise.then((beleidsdomeinen) => {
+      return beleidsdomeinen.map((item) => item.label).join(', ');
+    });
+  }
+
+  @action
+  linkToDetailPage() {
+    this.router.transitionTo('under-construction');
+  }
+}
