be97c34f66c78869028d449c01697fdaf27d8f36
Ben Fidlers <ben.fidlers@qmino.com>
2024-02-28 11:27:02 +0100

    add modal and selector

--- app/controllers/mandatarissen/persoon.js
+++ app/controllers/mandatarissen/persoon.js
@@ -1,15 +1,29 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 
 export default class MandatarissenPersoonController extends Controller {
   @service router;
 
+  @tracked isModalOpen = false;
+  @tracked selectedBestuursorgaan = null;
+
+  @action
+  toggleModal() {
+    this.isModalOpen = !this.isModalOpen;
+  }
+
+  @action
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
   @action
   createMandataris() {
     this.router.transitionTo(
-      'organen.orgaan.mandatarissen',
-      this.model.bestuursorganen[0].id
+      'organen.orgaan.mandataris.new',
+      this.selectedBestuursorgaan.id
     );
   }
 }
