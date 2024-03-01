45873e1f68dc2aded801912c294fd1abc4484033
Ruben Bottu <ruben.bottu@qmino.com>
2024-03-01 11:05:02 +0100

    Added buildSourceTtl to orgaan.mandataris.new for isDraft

--- app/controllers/organen/orgaan/mandataris/new.js
+++ app/controllers/organen/orgaan/mandataris/new.js
@@ -28,6 +28,11 @@ export default class OrganenMandatarisNewController extends Controller {
   }
 
   @action
+  buildSourceTtl(instanceUri) {
+    return `<${instanceUri}> <http://mu.semte.ch/vocabularies/ext/isDraft> "true".`;
+  }
+
+  @action
   buildMetaTtl() {
     return getBestuursorgaanMetaTtl(this.model.currentBestuursorgaan);
   }
