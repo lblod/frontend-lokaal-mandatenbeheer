04d3572b61f937aa86c2d85be4b77fee843d2f52
Ben Fidlers <ben.fidlers@qmino.com>
2024-03-05 08:38:42 +0100

    update title in mandataris page to show all linked bestuursorganen

--- app/controllers/mandatarissen/mandataris.js
+++ app/controllers/mandatarissen/mandataris.js
@@ -5,6 +5,17 @@ import { inject as service } from '@ember/service';
 export default class MandatarissenMandatarisController extends Controller {
   @service router;
 
+  get bestuursorganenTitle() {
+    let displayText =
+      this.model.bestuursorganen[0].isTijdsspecialisatieVan.get('naam');
+    for (let i = 1; i < this.model.bestuursorganen.length; i++) {
+      displayText += ` - ${this.model.bestuursorganen[1].isTijdsspecialisatieVan.get(
+        'naam'
+      )}`;
+    }
+    return displayText;
+  }
+
   @action
   onMandatarisChanged(newMandataris) {
     this.router.transitionTo('mandatarissen.mandataris', newMandataris.id);
