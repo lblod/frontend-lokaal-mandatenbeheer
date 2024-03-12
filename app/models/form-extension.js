1812bc5d5b6257e2aec1d2705f27d018daee4f55
Ruben Bottu <ruben.bottu@qmino.com>
2024-03-12 13:28:25 +0100

    Fixed relation between form and form-extension

--- app/models/form-extension.js
+++ app/models/form-extension.js
@@ -3,9 +3,7 @@ import Model, { belongsTo } from '@ember-data/model';
 export default class FormExtensionModel extends Model {
   @belongsTo('form', {
     async: true,
-    inverse: 'form-extension',
-    polymorphic: true,
-    as: 'form-extension',
+    inverse: 'extensions',
   })
-  classificatie;
+  baseForm;
 }
