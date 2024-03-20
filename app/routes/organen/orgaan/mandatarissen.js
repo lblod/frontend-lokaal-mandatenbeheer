5d3694028fc3f8fd137cff910f66fd487b666ec3
karel kremer <karel.kremer@redpencil.io>
2024-03-20 17:22:05 +0100

    group mandataris instances in mandataris page of bestuursorgaan

--- app/routes/organen/orgaan/mandatarissen.js
+++ app/routes/organen/orgaan/mandatarissen.js
@@ -1,5 +1,6 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import moment from 'moment';
 
 export default class OrganenMandatarissenRoute extends Route {
   @service store;
@@ -23,8 +24,69 @@ export default class OrganenMandatarissenRoute extends Route {
     }
 
     return {
-      mandatarissen,
+      mandatarissen: await this.foldMandatarissen(mandatarissen),
     };
+  }
+
+  async foldMandatarissen(mandatarissen) {
+    const personMandaatData = {};
+    const folded = [];
+    await Promise.all(
+      mandatarissen.map(async (mandataris) => {
+        const personId = (await mandataris.isBestuurlijkeAliasVan).id;
+        const mandaatId = (await mandataris.bekleedt).id;
+        const key = `${personId}-${mandaatId}`;
+        const existing = personMandaatData[key];
+        if (existing) {
+          existing.foldedStart = moment.min(
+            moment(existing.foldedStart),
+            moment(mandataris.start)
+          );
+          if (!mandataris.einde || !existing.foldedEnd) {
+            existing.foldedEnd = null;
+          } else {
+            existing.foldedEnd = moment.max(
+              moment(existing.foldedEnd),
+              moment(mandataris.einde)
+            );
+          }
+          if (moment(mandataris.einde).isSame(existing.foldedEnd)) {
+            // keep the one with the oldest end date
+            existing.mandataris = mandataris;
+          }
+          const fractie = mandataris.get(
+            'heeftLidmaatschap.binnenFractie.naam'
+          );
+          if (fractie && !existing.foldedFracties.includes(fractie)) {
+            existing.foldedFracties.push(fractie);
+          }
+        } else {
+          const fractie = mandataris.get(
+            'heeftLidmaatschap.binnenFractie.naam'
+          );
+          let fracties = [];
+          if (fractie) {
+            fracties = [fractie];
+          }
+          const firstOccurrence = {
+            foldedStart: mandataris.start,
+            foldedEnd: mandataris.einde,
+            mandataris,
+            foldedFracties: fracties,
+          };
+          personMandaatData[key] = firstOccurrence;
+          folded.push(firstOccurrence);
+        }
+      })
+    );
+    return folded.map((entry) => {
+      return {
+        mandataris: entry.mandataris,
+        foldedStart: entry.foldedStart,
+        foldedEnd: entry.foldedEnd,
+        foldedFracties: entry.foldedFracties.join(', '),
+      };
+    });
   }
 
   getOptions(params, bestuursOrgaan) {
@@ -41,9 +103,11 @@ export default class OrganenMandatarissenRoute extends Route {
           },
         },
       },
-      include: ['is-bestuurlijke-alias-van', 'bekleedt.bestuursfunctie'].join(
-        ','
-      ),
+      include: [
+        'is-bestuurlijke-alias-van',
+        'bekleedt.bestuursfunctie',
+        'heeft-lidmaatschap.binnen-fractie',
+      ].join(','),
     };
 
     if (params.filter) {
