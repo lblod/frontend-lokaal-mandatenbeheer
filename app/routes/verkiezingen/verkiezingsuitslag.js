6c25dc43734d205ab862728f62f03ea1a83243a0
Ruben Bottu <ruben.bottu@qmino.com>
2024-03-21 15:37:14 +0100

    Add verkiezingsuitslag route and template

--- app/routes/verkiezingen/verkiezingsuitslag.js
+++ app/routes/verkiezingen/verkiezingsuitslag.js
@@ -1,3 +1,53 @@
 import Route from '@ember/routing/route';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
-export default class VerkiezingenVerkiezingsuitslagRoute extends Route {}
+export default class VerkiezingenVerkiezingsuitslagRoute extends Route {
+  @service store;
+
+  queryParams = {
+    filter: { refreshModel: true },
+    page: { refreshModel: true },
+    size: { refreshModel: true },
+    sort: { refreshModel: true },
+  };
+
+  getOptions(params) {
+    const options = {
+      sort: params.sort,
+      page: {
+        number: params.page,
+        size: params.size,
+      },
+      'filter[kandidatenlijst][verkiezing][bestuursorgaan-in-tijd][id]':
+        params.bestuursorgaan_in_tijd_id,
+      'filter[:has:persoon]': 'true',
+      include: ['kandidatenlijst', 'persoon'].join(','),
+    };
+    if (params.filter && params.filter.length > 0) {
+      options.filter = params.filter;
+    }
+    return options;
+  }
+
+  async model(params) {
+    const options = this.getOptions(params);
+    const verkiezingsresultaten = await this.store.query(
+      'verkiezingsresultaat',
+      options
+    );
+    return { verkiezingsresultaten };
+  }
+
+  setupController(controller) {
+    super.setupController(...arguments);
+    controller.searchData = this.paramsFor('verkiezingen.verkiezingsuitslag')[
+      'filter'
+    ];
+  }
+
+  @action
+  reloadModel() {
+    this.refresh();
+  }
+}
