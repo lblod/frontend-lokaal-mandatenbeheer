9aee2bad3d972b77fc2d264bca7cf1679fa16e47
Ben Fidlers <ben.fidlers@qmino.com>
2024-03-25 11:18:23 +0100

    add back formbeheer route

---
+++ app/routes/formbeheer.js
@@ -0,0 +1,23 @@
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+
+export default class FormbeheerRoute extends Route {
+  @service session;
+  @service currentSession;
+  @service router;
+
+  beforeModel(transition) {
+    this.session.requireAuthentication(transition, 'login');
+  }
+
+  async model() {
+    const response = await fetch('/form-content/forms');
+    if (!response.ok) {
+      let error = new Error(response.statusText);
+      error.status = response.status;
+      throw error;
+    }
+    const form = await response.json();
+    return form.formDirectories;
+  }
+}
