c8f32712dcccdeab6b3dac4d0fc351f9b6f244bd
Ben Fidlers <ben.fidlers@qmino.com>
2024-03-26 14:00:36 +0100

    remove unused import

--- app/routes/organen/fracties.js
+++ app/routes/organen/fracties.js
@@ -1,7 +1,6 @@
 import Route from '@ember/routing/route';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-import moment from 'moment';
 import {
   getBestuursPeriods,
   getSelectedBestuursorgaanWithPeriods,
