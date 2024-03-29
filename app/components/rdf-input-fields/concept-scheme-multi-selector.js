77419fd8184f87a066bd6445a81ae0a4d11da5df
Ben Fidlers <ben.fidlers@qmino.com>
2024-03-29 10:51:43 +0100

    add concept scheme multi selector without meta ttl

---
+++ app/components/rdf-input-fields/concept-scheme-multi-selector.js
@@ -0,0 +1,109 @@
+import { action } from '@ember/object';
+import { guidFor } from '@ember/object/internals';
+import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
+import {
+  triplesForPath,
+  updateSimpleFormValue,
+} from '@lblod/submission-form-helpers';
+import InputFieldComponent from '@lblod/ember-submission-form-fields/components/rdf-input-fields/input-field';
+import { restartableTask, timeout } from 'ember-concurrency';
+import { NamedNode } from 'rdflib';
+import { hasValidFieldOptions } from '@lblod/ember-submission-form-fields/utils/has-valid-field-options';
+import { SEARCH_TIMEOUT } from 'frontend-lmb/utils/constants';
+
+export default class RdfInputFieldsConceptSchemeMultiSelectorComponent extends InputFieldComponent {
+  inputId = 'select-' + guidFor(this);
+  @service store;
+
+  @tracked selected = null;
+  @tracked options = [];
+  @tracked searchEnabled = true;
+  conceptScheme;
+
+  constructor() {
+    super(...arguments);
+
+    this.load();
+  }
+
+  async load() {
+    await this.loadOptions();
+    await this.loadProvidedValue();
+  }
+
+  async loadOptions() {
+    const fieldOptions = this.args.field.options;
+
+    if (!hasValidFieldOptions(this.args.field, ['conceptScheme'])) {
+      return;
+    }
+
+    this.conceptScheme = fieldOptions.conceptScheme;
+
+    if (fieldOptions.searchEnabled !== undefined) {
+      this.searchEnabled = fieldOptions.searchEnabled;
+    }
+
+    this.options = await this.fetchOptions();
+  }
+
+  async loadProvidedValue() {
+    if (this.isValid) {
+      const matches = triplesForPath(this.storeOptions, true).values;
+      this.selected = this.options.filter((opt) =>
+        matches.find((m) => m.equals(opt.subject))
+      );
+    }
+  }
+
+  @action
+  updateSelection(options) {
+    this.selected = options;
+
+    // Retrieve options in store
+    const matches = triplesForPath(this.storeOptions, true).values;
+    const matchingOptions = matches.filter((m) =>
+      this.options.find((opt) => m.equals(opt.subject))
+    );
+
+    // Cleanup old value(s) in the store
+    matchingOptions
+      .filter((m) => !options.find((opt) => m.equals(opt.subject)))
+      .forEach((m) => updateSimpleFormValue(this.storeOptions, undefined, m));
+
+    // Insert new value in the store
+    options
+      .filter((opt) => !matchingOptions.find((m) => opt.subject.equals(m)))
+      .forEach((option) =>
+        updateSimpleFormValue(this.storeOptions, option.subject)
+      );
+
+    this.hasBeenFocused = true;
+    super.updateValidations();
+  }
+
+  async fetchOptions(searchData) {
+    const queryParams = {
+      filter: {
+        label: searchData,
+        'concept-schemes': {
+          ':uri:': this.conceptScheme,
+        },
+      },
+      sort: 'label',
+    };
+    if (searchData) {
+      queryParams['filter']['label'] = searchData;
+    }
+    const response = await this.store.query('concept', queryParams);
+    return await response.map((m) => {
+      const subject = new NamedNode(m.uri);
+      return { subject: subject, label: m.label };
+    });
+  }
+
+  search = restartableTask(async (term) => {
+    return await this.fetchOptions(term);
+  });
+}
--- app/utils/register-form-fields.js
+++ app/utils/register-form-fields.js
@@ -10,6 +10,7 @@ import RDFGeboorteDatumInput from 'frontend-lmb/components/rdf-input-fields/gebo
 import RDFArchivedInput from 'frontend-lmb/components/rdf-input-fields/archived-input';
 import RDFAMandatarisReplacementSelector from 'frontend-lmb/components/rdf-input-fields/mandataris-replacement-selector';
 import RDFConceptSchemeSelectorComponent from 'frontend-lmb/components/rdf-input-fields/concept-scheme-selector';
+import RDFConceptSchemeMultiSelectorComponent from 'frontend-lmb/components/rdf-input-fields/concept-scheme-multi-selector';
 
 export const registerCustomFormFields = () => {
   registerFormFields([
@@ -59,6 +60,11 @@ export const registerCustomFormFields = () => {
       displayType:
         'http://lblod.data.gift/display-types/conceptSchemeSelectorWithoutMeta',
       edit: RDFConceptSchemeSelectorComponent,
+    },
+    {
+      displayType:
+        'http://lblod.data.gift/display-types/conceptSchemeMultiSelectorWithoutMeta',
+      edit: RDFConceptSchemeMultiSelectorComponent,
     },
   ]);
 };
