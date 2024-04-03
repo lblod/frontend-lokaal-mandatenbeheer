7aded627f938f2211ab370b0ebafad90648ceb25
Ruben Bottu <ruben.bottu@qmino.com>
2024-04-03 15:10:54 +0200

    Remove geboorte datum and rijksregister input components

--- app/utils/register-form-fields.js
+++ app/utils/register-form-fields.js
@@ -5,8 +5,6 @@ import RDFAddressSelectorComponent from 'frontend-lmb/components/rdf-input-field
 import RDFPersonSelectorComponent from 'frontend-lmb/components/rdf-input-fields/person-selector';
 import RDFMandatarisFractieSelectorComponent from 'frontend-lmb/components/rdf-input-fields/mandataris-fractie-selector';
 import RDFMandatarisMandaatSelectorComponent from 'frontend-lmb/components/rdf-input-fields/mandataris-mandaat-selector';
-import RDFRijksRegisterInput from 'frontend-lmb/components/rdf-input-fields/rijksregister-input';
-import RDFGeboorteDatumInput from 'frontend-lmb/components/rdf-input-fields/geboorte-datum-input';
 import RDFArchivedInput from 'frontend-lmb/components/rdf-input-fields/archived-input';
 import RDFAMandatarisReplacementSelector from 'frontend-lmb/components/rdf-input-fields/mandataris-replacement-selector';
 import RDFConceptSchemeSelectorComponent from 'frontend-lmb/components/rdf-input-fields/concept-scheme-selector';
@@ -39,14 +37,6 @@ export const registerCustomFormFields = () => {
       displayType:
         'http://lblod.data.gift/display-types/mandatarisMandaatSelector',
       edit: RDFMandatarisMandaatSelectorComponent,
-    },
-    {
-      displayType: 'http://lblod.data.gift/display-types/rijksregisterInput',
-      edit: RDFRijksRegisterInput,
-    },
-    {
-      displayType: 'http://lblod.data.gift/display-types/geboorteDatumInput',
-      edit: RDFGeboorteDatumInput,
     },
     {
       displayType: 'http://lblod.data.gift/display-types/archivedInput',
