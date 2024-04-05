08f907b90854883de2da1584a82258df5f76f898
Ruben Bottu <ruben.bottu@qmino.com>
2024-04-05 14:52:47 +0200

    Re-add simplified rijksregister-input

---
+++ app/components/rdf-input-fields/rijksregister-input.js
@@ -0,0 +1,43 @@
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import InputFieldComponent from '@lblod/ember-submission-form-fields/components/rdf-input-fields/input-field';
+import { guidFor } from '@ember/object/internals';
+import { triplesForPath } from '@lblod/submission-form-helpers';
+import { replaceSingleFormValue } from 'frontend-lmb/utils/replaceSingleFormValue';
+
+export default class RDFRijksRegisterInput extends InputFieldComponent {
+  inputId = 'rrn-' + guidFor(this);
+
+  @service store;
+  @tracked rijksregisternummer;
+
+  constructor() {
+    super(...arguments);
+    this.loadProvidedValue();
+  }
+
+  async loadProvidedValue() {
+    const matches = triplesForPath(this.storeOptions);
+
+    if (matches.values.length > 0) {
+      this.rijksregisternummer = matches.values[0].value;
+    }
+  }
+
+  @action
+  async updateValue(event) {
+    if (event && typeof event.preventDefault === 'function') {
+      event.preventDefault();
+    }
+    const rijksregisternummer = event.target.value.trim();
+    this.rijksregisternummer = rijksregisternummer;
+
+    replaceSingleFormValue(
+      this.storeOptions,
+      rijksregisternummer ? rijksregisternummer : null
+    );
+
+    this.hasBeenFocused = true;
+  }
+}
--- app/utils/register-form-fields.js
+++ app/utils/register-form-fields.js
@@ -5,6 +5,7 @@ import RDFAddressSelectorComponent from 'frontend-lmb/components/rdf-input-field
 import RDFPersonSelectorComponent from 'frontend-lmb/components/rdf-input-fields/person-selector';
 import RDFMandatarisFractieSelectorComponent from 'frontend-lmb/components/rdf-input-fields/mandataris-fractie-selector';
 import RDFMandatarisMandaatSelectorComponent from 'frontend-lmb/components/rdf-input-fields/mandataris-mandaat-selector';
+import RDFRijksRegisterInput from 'frontend-lmb/components/rdf-input-fields/rijksregister-input';
 import RDFArchivedInput from 'frontend-lmb/components/rdf-input-fields/archived-input';
 import RDFAMandatarisReplacementSelector from 'frontend-lmb/components/rdf-input-fields/mandataris-replacement-selector';
 import RDFConceptSchemeSelectorComponent from 'frontend-lmb/components/rdf-input-fields/concept-scheme-selector';
@@ -41,6 +42,10 @@ export const registerCustomFormFields = () => {
     {
       displayType: 'http://lblod.data.gift/display-types/archivedInput',
       edit: RDFArchivedInput,
+    },
+    {
+      displayType: 'http://lblod.data.gift/display-types/rijksregisterInput',
+      edit: RDFRijksRegisterInput,
     },
     {
       displayType: 'http://lblod.data.gift/display-types/mandatarisReplacement',
