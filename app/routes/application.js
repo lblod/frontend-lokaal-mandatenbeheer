8c57c2d252cd67b6c98ca94189093900363098a4
Ruben Bottu <ruben.bottu@qmino.com>
2024-04-05 16:07:10 +0200

    Register rijksregisternummer validation and bump form helpers

--- app/routes/application.js
+++ app/routes/application.js
@@ -5,6 +5,7 @@ import ENV from 'frontend-lmb/config/environment';
 import 'moment';
 import 'moment-timezone';
 import { registerCustomFormFields } from 'frontend-lmb/utils/register-form-fields';
+import { registerCustomValidations } from 'frontend-lmb/utils/register-custom-validations';
 
 export default class ApplicationRoute extends Route {
   @service currentSession;
@@ -29,6 +30,7 @@ export default class ApplicationRoute extends Route {
 
     this.startAnalytics();
     registerCustomFormFields();
+    registerCustomValidations();
 
     return this._loadCurrentSession();
   }
