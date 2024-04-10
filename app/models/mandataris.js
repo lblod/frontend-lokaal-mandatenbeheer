1ee4ee94ff0bb143df03809e1fdce772bc13e105
Ruben Bottu <ruben.bottu@qmino.com>
2024-04-10 14:46:39 +0200

    Get unique vervanger mandatarissen

--- app/models/mandataris.js
+++ app/models/mandataris.js
@@ -111,3 +111,25 @@ export async function getUniqueBestuursorganen(mandataris) {
 
   return Array.from(bestuursorganen);
 }
+
+export const getUniqueVervangers = async (mandataris) => {
+  const duplicateVervangers = await mandataris.vervangerVan;
+  const vervangers = new Map();
+
+  for (let vervanger of duplicateVervangers) {
+    const persoon = await vervanger.isBestuurlijkeAliasVan;
+    if (!vervangers.has(persoon.id)) {
+      vervangers.set(persoon.id, vervanger);
+    } else {
+      const existingVervanger = vervangers.get(persoon.id);
+      const existingEinde = existingVervanger.einde;
+      const newEinde = vervanger.einde;
+
+      // Keep the most recent vervanger
+      if (!newEinde || (existingEinde && newEinde > existingEinde)) {
+        vervangers.set(persoon.id, vervanger);
+      }
+    }
+  }
+  return Array.from(vervangers.values());
+};
