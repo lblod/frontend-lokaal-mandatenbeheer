cd56d078b62234afb9e7417f74012a2b1703d1ff
Ruben Bottu <ruben.bottu@qmino.com>
2024-04-10 17:18:44 +0200

    Remove role model

--- app/models/post.js
+++ app/models/post.js
@@ -1,12 +1,6 @@
-import Model, { belongsTo, hasMany } from '@ember-data/model';
+import Model, { hasMany } from '@ember-data/model';
 
 export default class PostModel extends Model {
-  @belongsTo('role', {
-    async: true,
-    inverse: null,
-  })
-  role;
-
   @hasMany('agent-in-position', {
     async: true,
     inverse: 'post',
