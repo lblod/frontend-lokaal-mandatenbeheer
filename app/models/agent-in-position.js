e4b608e9f33b572a64eef6024507d232bf2987f5
Ruben Bottu <ruben.bottu@qmino.com>
2024-04-10 18:35:17 +0200

    Remove post model

--- app/models/agent-in-position.js
+++ app/models/agent-in-position.js
@@ -4,14 +4,6 @@ export default class AgentInPositionModel extends Model {
   @attr('date') agentStartDate;
   @attr('date') agentEndDate;
 
-  @belongsTo('post', {
-    async: true,
-    inverse: 'agentsInPosition',
-    polymorphic: true,
-    as: 'agent-in-position',
-  })
-  post;
-
   @belongsTo('persoon', { async: true, inverse: null }) person;
 
   @belongsTo('persoon', {
