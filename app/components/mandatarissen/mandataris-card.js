2059e397561ee21288b7bac8715abf0fd4283ecd
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-04-11 09:00:53 +0200

    LMB-238: set back mandataris card as it was on master

--- app/components/mandatarissen/mandataris-card.js
+++ app/components/mandatarissen/mandataris-card.js
@@ -1,11 +1,9 @@
 import Component from '@glimmer/component';
 
-import { restartableTask } from 'ember-concurrency';
-import { tracked } from '@glimmer/tracking';
-
 export default class MandatarisCardComponent extends Component {
-  @tracked bestuursOrgaanNames;
-
+  get rol() {
+    return this.args.mandataris.bekleedt.get('bestuursfunctie').get('label');
+  }
   get status() {
     return this.args.mandataris.status.get('label');
   }
@@ -24,42 +22,4 @@ export default class MandatarisCardComponent extends Component {
       return beleidsdomeinen.map((item) => item.label).join(', ');
     });
   }
-
-  get rolWithBestuurOrgaanNames() {
-    const rol = this.args.mandataris.bekleedt
-      .get('bestuursfunctie')
-      .get('label');
-
-    if (!this.bestuursOrgaanNames || this.bestuursOrgaanNames == '') {
-      return rol;
-    }
-    return `${rol} - ${this.bestuursOrgaanNames}`;
-  }
-
-  setBestuursOrganenForMandataris = restartableTask(async () => {
-    this.bestuursOrgaanNames = '';
-    const names = [];
-    const bestuursOrganenInDeTijd =
-      await this.args.mandataris.get('bekleedt.bevatIn');
-
-    if (!bestuursOrganenInDeTijd) {
-      return '';
-    }
-
-    for (const orgaan of bestuursOrganenInDeTijd) {
-      if (!orgaan) {
-        return null;
-      }
-
-      const bestuursOrgaan = await orgaan.get('isTijdsspecialisatieVan');
-
-      if (!bestuursOrgaan) {
-        continue;
-      }
-
-      names.push(bestuursOrgaan.naam);
-    }
-
-    this.bestuursOrgaanNames = names.join(', ');
-  });
 }
