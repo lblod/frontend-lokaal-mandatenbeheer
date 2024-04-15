822ba20b4799d6467618b4ac76efde656c97b98e
Ruben Bottu <ruben.bottu@qmino.com>
2024-04-16 01:58:41 +0200

    Add isBekrachtigd getter to mandataris-publication-status

--- app/models/mandataris-publication-status.js
+++ app/models/mandataris-publication-status.js
@@ -1,8 +1,13 @@
 import Model, { attr } from '@ember-data/model';
+import { MANDATARIS_BEKRACHTIGD_STATE } from 'frontend-lmb/utils/well-known-uris';
 
 export default class MandatarisPublicationStatusModel extends Model {
   @attr uri;
   @attr label;
+
+  get isBekrachtigd() {
+    return this.uri === MANDATARIS_BEKRACHTIGD_STATE;
+  }
 
   rdfaBindings = {
     class: 'http://www.w3.org/2004/02/skos/core#Concept',
