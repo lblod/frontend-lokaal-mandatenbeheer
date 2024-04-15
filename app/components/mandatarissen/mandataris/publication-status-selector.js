7e8303fda7fe8ac82f3376f97d3e7f079162ba3e
Ruben Bottu <ruben.bottu@qmino.com>
2024-04-15 23:35:12 +0200

    Add publication status selector component

---
+++ app/components/mandatarissen/mandataris/publication-status-selector.js
@@ -0,0 +1,17 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class MandatarissenMandatarisPublicationStatusSelectorComponent extends Component {
+  @tracked _options;
+
+  constructor() {
+    super(...arguments);
+    this._options = this.args.options?.sortBy('label') || [];
+  }
+
+  @action
+  async selectStatus(status) {
+    await this.args.onSelect(status);
+  }
+}
