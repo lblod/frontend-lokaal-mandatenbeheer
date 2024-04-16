bf1611e564e0093ee6b439e1d0cc908bdb95889b
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-04-16 08:25:31 +0200

    LMB-370: set default datetime modified in frasctie model

--- app/models/fractie.js
+++ app/models/fractie.js
@@ -4,7 +4,12 @@ export default class FractieModel extends Model {
   @attr uri;
   @attr naam;
   @attr('uri-set') generatedFrom;
-  @attr('datetime') modified;
+  @attr('datetime', {
+    defaultValue() {
+      return new Date();
+    },
+  })
+  modified;
 
   @belongsTo('fractietype', {
     async: true,
