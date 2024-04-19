28d90d4444e452eb7ff6d68999f4f04e41239d56
Ben Fidlers <ben.fidlers@qmino.com>
2024-04-19 07:38:27 +0200

    add search component

--- app/controllers/formbeheer/form/instances.js
+++ app/controllers/formbeheer/form/instances.js
@@ -2,6 +2,8 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
+import { task, timeout } from 'ember-concurrency';
+import { SEARCH_TIMEOUT } from 'frontend-lmb/utils/constants';
 
 export default class FormInstancesController extends Controller {
   queryParams = ['page', 'size', 'sort', 'filter'];
@@ -24,4 +26,10 @@ export default class FormInstancesController extends Controller {
   async onRemoveInstance() {
     this.router.refresh();
   }
+
+  search = task({ restartable: true }, async (searchData) => {
+    await timeout(SEARCH_TIMEOUT);
+    this.page = 0;
+    this.filter = searchData;
+  });
 }
