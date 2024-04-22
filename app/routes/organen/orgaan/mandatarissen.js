c503f749898c77607d5acd17697bb19f5e262a02
Ben Fidlers <ben.fidlers@qmino.com>
2024-04-22 09:22:54 +0200

    bugfix folded mandatarissen mandataris table

--- app/routes/organen/orgaan/mandatarissen.js
+++ app/routes/organen/orgaan/mandatarissen.js
@@ -112,8 +112,11 @@ export default class OrganenMandatarissenRoute extends Route {
               moment(mandataris.einde)
             );
           }
-          if (moment(mandataris.einde).isSame(existing.foldedEnd)) {
-            // keep the one with the oldest end date
+          if (
+            moment(mandataris.einde).isSame(existing.foldedEnd) ||
+            !mandataris.einde
+          ) {
+            // keep the one with the oldest end date (if it is null, we assume this is the oldest as well)
             existing.mandataris = mandataris;
           }
           const fractie = mandataris.get(
