27ff564f2961bf778bf04560b36ba48e64685f0b
Ben Fidlers <ben.fidlers@qmino.com>
2024-04-23 20:25:31 +0200

    move some bestuursperiod logic to service

--- app/routes/organen/fracties.js
+++ app/routes/organen/fracties.js
@@ -1,16 +1,13 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 import { action } from '@ember/object';
-import {
-  getBestuursPeriods,
-  getSelectedBestuursorgaanWithPeriods,
-} from 'frontend-lmb/utils/bestuursperioden';
 import { getFormFrom } from 'frontend-lmb/utils/get-form';
 import { FRACTIE_FORM_ID } from 'frontend-lmb/utils/well-known-ids';
 import { FRACTIETYPE_SAMENWERKINGSVERBAND } from 'frontend-lmb/utils/well-known-uris';
 
 export default class FractiesRoute extends Route {
   @service store;
+  @service tijdsspecialisaties;
 
   queryParams = {
     filter: { refreshModel: true },
@@ -24,10 +21,11 @@ export default class FractiesRoute extends Route {
   async model(params) {
     const parentModel = this.modelFor('organen');
 
-    const organenWithPeriods = await this.fetchBestuursOrganen(
-      parentModel.bestuursorganen,
-      params
-    );
+    const organenWithPeriods =
+      await this.tijdsspecialisaties.fetchBestuursOrganenWithTijdsperiods(
+        parentModel.bestuursorganen,
+        params
+      );
 
     const bestuursorganenIds = await Promise.all(
       organenWithPeriods.bestuursorganen.map(async (o) => {
@@ -59,44 +57,6 @@ export default class FractiesRoute extends Route {
       fracties,
       bestuurseenheid: parentModel.bestuurseenheid,
       ...organenWithPeriods,
-    };
-  }
-
-  async fetchBestuursOrganen(organen, params) {
-    let selectedPeriod, bestuursPeriods;
-
-    const selectedBestuursOrganen = await Promise.all(
-      organen.map(async (bestuursorgaan) => {
-        const tijdsspecialisaties =
-          await bestuursorgaan.heeftTijdsspecialisaties;
-
-        let currentBestuursorgaan;
-        if (tijdsspecialisaties.length != 0) {
-          const result = getSelectedBestuursorgaanWithPeriods(
-            tijdsspecialisaties,
-            {
-              startDate: params.startDate,
-              endDate: params.endDate,
-            }
-          );
-
-          currentBestuursorgaan = result.bestuursorgaan;
-
-          if (!selectedPeriod) {
-            selectedPeriod = {
-              startDate: result.startDate,
-              endDate: result.endDate,
-            };
-            bestuursPeriods = getBestuursPeriods(tijdsspecialisaties);
-          }
-          return currentBestuursorgaan;
-        }
-      })
-    );
-    return {
-      bestuursPeriods,
-      selectedPeriod,
-      bestuursorganen: selectedBestuursOrganen,
     };
   }
 
