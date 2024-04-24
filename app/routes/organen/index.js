30c3bc4e838a0fae247943253b6e42a891957635
Ben Fidlers <ben.fidlers@qmino.com>
2024-04-24 10:33:41 +0200

    actual fix filter no bestuursperiod selected

--- app/routes/organen/index.js
+++ app/routes/organen/index.js
@@ -51,8 +51,8 @@ export default class OrganenIndexRoute extends Route {
         const tijdsspecialisaties = await orgaan.heeftTijdsspecialisaties;
         const time = await tijdsspecialisaties.some((b) => {
           return (
-            !params.startDate ||
-            moment(b.bindingStart).format('YYYY-MM-DD') == params.startDate
+            moment(b.bindingStart).format('YYYY-MM-DD') ==
+            organenWithPeriods.selectedPeriod.startDate
           );
         });
         return { bool: some && time, orgaan };
