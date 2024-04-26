a6e5cb43150fef1396ad3c0c63516f35c21bcc74
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-04-26 11:00:00 +0200

    LMB-388: get mandatarissen from bestuursorgaan

--- app/routes/verkiezingen/verkiezingsuitslag/prepare.js
+++ app/routes/verkiezingen/verkiezingsuitslag/prepare.js
@@ -1,9 +1,57 @@
 import Route from '@ember/routing/route';
 
+import { service } from '@ember/service';
+
 export default class PrepareInstallatievergaderingRoute extends Route {
-  async model() {
+  @service store;
+
+  async model(params) {
+    const verkiezingsuitslag = this.modelFor('verkiezingen.verkiezingsuitslag');
+    const bestuursorgaan =
+      await verkiezingsuitslag.installatievergadering.bestuursorgaanInTijd;
+
+    let mandatarissen;
+    if (bestuursorgaan) {
+      mandatarissen = await this.store.query(
+        'mandataris',
+        this.getOptions(params, bestuursorgaan)
+      );
+      console.log(mandatarissen);
+    }
+
     return {
-      ...this.modelFor('verkiezingen.verkiezingsuitslag'),
+      ...verkiezingsuitslag,
+      mandatarissen: mandatarissen,
     };
+  }
+
+  getOptions(params, bestuursOrgaan) {
+    const NO_PAGINATION = {
+      number: 0,
+      size: 999,
+    };
+    const queryParams = {
+      sort: params.sort,
+      page: NO_PAGINATION,
+      filter: {
+        bekleedt: {
+          'bevat-in': {
+            id: bestuursOrgaan.id,
+          },
+        },
+      },
+      include: [
+        'is-bestuurlijke-alias-van',
+        'bekleedt.bestuursfunctie',
+        'heeft-lidmaatschap.binnen-fractie',
+        'status',
+      ].join(','),
+    };
+
+    if (params.filter) {
+      queryParams['filter']['is-bestuurlijke-alias-van'] = params.filter;
+    }
+
+    return queryParams;
   }
 }
