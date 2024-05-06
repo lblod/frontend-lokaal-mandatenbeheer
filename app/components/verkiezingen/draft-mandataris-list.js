1b2f353a4b33b1a343a935412fd210d6dea25649
Ben Fidlers <ben.fidlers@qmino.com>
2024-05-06 19:33:29 +0200

    use call to mandataris service for deleting mandatarissen

--- app/components/verkiezingen/draft-mandataris-list.js
+++ app/components/verkiezingen/draft-mandataris-list.js
@@ -1,10 +1,14 @@
 import Component from '@glimmer/component';
 
+import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import { orderMandatarissenByRangorde } from 'frontend-lmb/utils/rangorde';
+import { JSON_API_TYPE } from 'frontend-lmb/utils/constants';
 
 export default class DraftMandatarisListComponent extends Component {
+  @service toaster;
+
   @tracked isModalOpen = false;
   @tracked mandataris;
   @tracked editBeleidsdomeinen;
@@ -68,6 +72,23 @@ export default class DraftMandatarisListComponent extends Component {
 
   @action
   async removeMandataris(mandataris) {
-    await mandataris.destroyRecord();
+    const result = await fetch(
+      `/mandataris-api/mandatarissen/${mandataris.id}`,
+      {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': JSON_API_TYPE,
+        },
+      }
+    );
+
+    if (result.ok) {
+      const succesMessage = 'Mandataris succesvol verwijderd.';
+      this.toaster.success(succesMessage, 'Succes', { timeOut: 5000 });
+    } else {
+      const errorMessage =
+        'Er ging iets mis bij het verwijderen van de mandataris. Probeer het later opnieuw.';
+      this.toaster.error(errorMessage, 'Error');
+    }
   }
 }
