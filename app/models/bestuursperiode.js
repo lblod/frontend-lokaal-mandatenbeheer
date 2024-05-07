8aae7eb26c8e232fbd5e32447e1a9e13c636120c
Ben Fidlers <ben.fidlers@qmino.com>
2024-05-07 15:09:59 +0200

    specify attributes as number

--- app/models/bestuursperiode.js
+++ app/models/bestuursperiode.js
@@ -2,8 +2,8 @@ import { attr, hasMany } from '@ember-data/model';
 import ConceptModel from './concept';
 
 export default class BeleidsdomeinCodeModel extends ConceptModel {
-  @attr start;
-  @attr einde;
+  @attr('number') start;
+  @attr('number') einde;
 
   @hasMany('bestuursorgaan', {
     async: true,
