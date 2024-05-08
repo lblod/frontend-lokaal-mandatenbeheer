bcd557c967127f1397cfe3f1aa9de22537bd870d
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-05-08 09:00:39 +0200

    LMB-420: remove the prepare settings method with the loader

--- app/controllers/settings.js
+++ app/controllers/settings.js
@@ -1,9 +1,8 @@
 import Controller from '@ember/controller';
 
-import { restartableTask, timeout } from 'ember-concurrency';
+import { restartableTask } from 'ember-concurrency';
 import { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
-import { SEARCH_TIMEOUT } from 'frontend-lmb/utils/constants';
 
 export default class SettingsController extends Controller {
   @service currentSession;
@@ -13,17 +12,10 @@ export default class SettingsController extends Controller {
     return { email: this.bestuurseenheidEmail };
   }
 
-  prepareSettings = restartableTask(async () => {
-    await timeout(SEARCH_TIMEOUT);
-    console.log(`model`, this.model);
-    console.log(`model.bestuurseenheid`, this.model.bestuurseenheid);
-  });
-
   saveBestuurseenheidSettings = restartableTask(async () => {});
   cancelBestuurseenheidSettings = restartableTask(async () => {});
 
   get bestuurseenheidLabel() {
-    console.log(this.currentSession);
-    return `${this.currentSession.groupClassification.label} ${this.currentSession.group.naam}`;
+    return this.currentSession.group.naam;
   }
 }
