488b7d3f2d175155c9a167f5dadef5d38cc47468
Ben Fidlers <ben.fidlers@qmino.com>
2024-05-10 11:27:11 +0200

    re-add models we still need

--- app/routes/organen/orgaan.js
+++ app/routes/organen/orgaan.js
@@ -2,6 +2,8 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import { queryRecord } from 'frontend-lmb/utils/query-record';
 import RSVP from 'rsvp';
+import { getFormFrom } from 'frontend-lmb/utils/get-form';
+import { BESTUURSORGAAN_FORM_ID } from 'frontend-lmb/utils/well-known-ids';
 
 export default class OrganenOrgaanRoute extends Route {
   @service store;
@@ -41,7 +43,14 @@ export default class OrganenOrgaanRoute extends Route {
       }
     );
 
+    const bestuursorgaanFormDefinition = getFormFrom(
+      this.store,
+      BESTUURSORGAAN_FORM_ID
+    );
+
     return RSVP.hash({
+      bestuursorgaanFormDefinition,
+      instanceId: bestuursorgaanId,
       bestuursorgaan,
       bestuursPeriods,
       selectedPeriod,
