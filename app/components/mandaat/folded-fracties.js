33c71a56ba61e3eba31414ea3217e57ab2ca5507
Ruben Bottu <ruben.bottu@qmino.com>
2024-05-15 17:31:32 +0200

    Add folded-fracties component

---
+++ app/components/mandaat/folded-fracties.js
@@ -0,0 +1,75 @@
+import Component from '@glimmer/component';
+import { service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import { toUserReadableListing } from 'frontend-lmb/utils/to-user-readable-listing';
+import moment from 'moment';
+
+export default class MandaatFoldedFractiesComponent extends Component {
+  @service store;
+
+  @tracked fractiesText = '';
+
+  get persoon() {
+    return this.args.persoon;
+  }
+
+  constructor(...args) {
+    super(...args);
+    this.load();
+  }
+
+  async load() {
+    const mandatarissen = await this.getMandatarissen(this.persoon);
+
+    const { currentFractie, allFracties } =
+      await this.getFoldedFracties(mandatarissen);
+
+    this.fractiesText = this.fractiesToString(currentFractie, allFracties);
+  }
+
+  async getMandatarissen(persoon) {
+    return await this.store.query('mandataris', {
+      filter: {
+        'is-bestuurlijke-alias-van': {
+          ':id:': persoon.id,
+        },
+      },
+      include: 'heeft-lidmaatschap.binnen-fractie',
+    });
+  }
+
+  async getFoldedFracties(mandatarissen) {
+    let latestMandataris = null;
+    const allFracties = new Set();
+
+    await Promise.all(
+      mandatarissen.map(async (mandataris) => {
+        const fractie = mandataris.get('heeftLidmaatschap.binnenFractie.naam');
+        allFracties.add(fractie);
+
+        if (
+          !latestMandataris ||
+          moment(mandataris.einde).isAfter(latestMandataris.einde) ||
+          !mandataris.einde
+        ) {
+          latestMandataris = mandataris;
+        }
+      })
+    );
+
+    const currentFractie = latestMandataris.get(
+      'heeftLidmaatschap.binnenFractie.naam'
+    );
+
+    return { currentFractie, allFracties: Array.from(allFracties) };
+  }
+
+  fractiesToString(currentFractie, allFracties) {
+    const otherFracties = allFracties
+      .filter((f) => f != currentFractie && f) // TODO the "!=" and "&& f" are confusing
+      .toSorted((a, b) => a.localeCompare(b));
+    return otherFracties.length
+      ? `${currentFractie} (${toUserReadableListing(otherFracties)})`
+      : currentFractie;
+  }
+}
