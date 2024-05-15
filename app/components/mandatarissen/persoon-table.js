0521ef4971e112380777f6b2495788acf5bd8312
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-05-15 10:50:04 +0200

    LMB-425: only show fracties of active mandatarissen

--- app/components/mandatarissen/persoon-table.js
+++ app/components/mandatarissen/persoon-table.js
@@ -5,12 +5,16 @@ export default class MandatarissenPersoonTable extends Component {
     const mandatarissen = await persoon.isAangesteldAls;
     const fractieLabels = await Promise.all(
       mandatarissen.map(async (mandataris) => {
+        if (!mandataris.isActive) {
+          return null;
+        }
+
         const isLidVan = await mandataris.heeftLidmaatschap;
         const binnenFractie = await isLidVan.binnenFractie;
         return binnenFractie ? binnenFractie.naam : null;
       })
     );
 
-    return [...new Set(fractieLabels)].join(', ');
+    return [...new Set(fractieLabels.filter((label) => label))].join(', ');
   }
 }
