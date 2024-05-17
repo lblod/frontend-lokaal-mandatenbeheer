7bebc969dd800f7c4798947fe9fcf2375e7221a4
Ben Fidlers <ben.fidlers@qmino.com>
2024-05-17 16:01:03 +0200

    add bewerk mandataris info form

--- app/components/mandatarissen/mandataris-extra-info-card.js
+++ app/components/mandatarissen/mandataris-extra-info-card.js
@@ -1,6 +1,13 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { service } from '@ember/service';
 
 export default class MandatarisExtraInfoCardComponent extends Component {
+  @service router;
+
+  @tracked isModalActive = false;
+
   get formattedBeleidsdomein() {
     const beleidsdomeinenPromise = this.args.mandataris.beleidsdomein;
     if (!beleidsdomeinenPromise.length) {
@@ -9,5 +16,16 @@ export default class MandatarisExtraInfoCardComponent extends Component {
     return beleidsdomeinenPromise.then((beleidsdomeinen) => {
       return beleidsdomeinen.map((item) => item.label).join(', ');
     });
+  }
+
+  @action
+  toggleModal() {
+    this.isModalActive = !this.isModalActive;
+  }
+
+  @action
+  onSave() {
+    this.isModalActive = !this.isModalActive;
+    setTimeout(() => this.router.refresh(), 1000);
   }
 }
