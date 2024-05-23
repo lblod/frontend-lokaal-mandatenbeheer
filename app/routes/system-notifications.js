7fcaffd618b8a05ecbd48446414988ab6785c024
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-05-23 08:43:30 +0200

    LMB-359: get all the system notifications of the user

--- app/routes/system-notifications.js
+++ app/routes/system-notifications.js
@@ -1,3 +1,18 @@
 import Route from '@ember/routing/route';
 
-export default class SystemNotificationsRoute extends Route {}
+import { service } from '@ember/service';
+
+export default class SystemNotificationsRoute extends Route {
+  @service currentSession;
+  @service store;
+
+  async model() {
+    const notifications = await this.store.query('system-notification', {
+      'filter[gebruiker][:id:]': this.currentSession.user.id,
+    });
+
+    return {
+      notifications: notifications ?? [],
+    };
+  }
+}
