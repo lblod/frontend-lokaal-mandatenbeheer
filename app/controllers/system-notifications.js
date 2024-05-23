1dfba34c17f40f4f1e3ff25548f403482b6c5a32
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-05-23 10:22:00 +0200

    LMB-359: add sorting to the table

--- app/controllers/system-notifications.js
+++ app/controllers/system-notifications.js
@@ -1,9 +1,14 @@
 import Controller from '@ember/controller';
 
+import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { restartableTask } from 'ember-concurrency';
 
 export default class SystemNotificationsController extends Controller {
+  queryParams = ['created-at', 'page', 'size'];
+
+  @tracked sort = 'created-at';
+
   toggleDate = restartableTask(async (notification, modelAttribute) => {
     if (notification[modelAttribute]) {
       notification[modelAttribute] = null;
@@ -16,7 +21,7 @@ export default class SystemNotificationsController extends Controller {
   @action
   readAtText(notification) {
     if (notification.readAt) {
-      return 'Markeer als niet gelezen'; // euh idk
+      return 'Markeer als niet gelezen';
     }
     return 'Markeer als gelezen';
   }
