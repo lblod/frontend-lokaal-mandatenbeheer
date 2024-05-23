f81e468d811488867bd89478f28aef7d5d24b27b
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-05-23 09:18:51 +0200

    LMB-359: toggle the datetime on read and archived
    Update the model and update the text depending on what value the property has

---
+++ app/controllers/system-notifications.js
@@ -0,0 +1,31 @@
+import Controller from '@ember/controller';
+
+import { action } from '@ember/object';
+import { restartableTask } from 'ember-concurrency';
+
+export default class SystemNotificationsController extends Controller {
+  toggleDate = restartableTask(async (notification, modelAttribute) => {
+    if (notification[modelAttribute]) {
+      notification[modelAttribute] = null;
+    } else {
+      notification[modelAttribute] = new Date();
+    }
+    notification.save();
+  });
+
+  @action
+  readAtText(notification) {
+    if (notification.readAt) {
+      return 'Markeer als niet gelezen'; // euh idk
+    }
+    return 'Markeer als gelezen';
+  }
+
+  @action
+  archivedAtText(notification) {
+    if (notification.archivedAt) {
+      return 'Terugzetten uit archief';
+    }
+    return 'Archiveer';
+  }
+}
