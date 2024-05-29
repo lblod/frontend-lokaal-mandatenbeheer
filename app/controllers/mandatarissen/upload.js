4818611a1eb25cc4e2f409badec9c1db5834c3fa
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-05-29 16:15:23 +0200

    LMB-305: add the details of the upload in tables

--- app/controllers/mandatarissen/upload.js
+++ app/controllers/mandatarissen/upload.js
@@ -1,25 +1,63 @@
 import Controller from '@ember/controller';
 
+import { tracked } from '@glimmer/tracking';
 import { task } from 'ember-concurrency';
 
 export default class MandatarissenUploadController extends Controller {
+  @tracked isFileUploaded;
+  @tracked isDetailExpanded;
+  @tracked uploadedFile;
+  @tracked fileDetails;
+
   uploadFile = task(async (uploadFile) => {
-    console.log(`uploadFile`, uploadFile);
-    const response = await uploadFile.upload(
-      '/mandataris-api/mandatarissen/upload-csv',
-      {
-        contentType: 'multipart/form-data',
-      }
-    );
-    // const uploadState: CsvUploadState = {
-    //   errors: [],
-    //   warnings: [],
-    //   personsCreated: 0,
-    //   mandatarissenCreated: 0,
-    //   beleidsdomeinenCreated: 0,
-    //   beleidsDomeinMapping: {},
-    // };
-    const jsonResponse = await response.json();
-    console.log({ jsonResponse });
+    this.isFileUploaded = false;
+    this.fileDetails = null;
+    try {
+      const response = await uploadFile.upload(
+        '/mandataris-api/mandatarissen/upload-csv',
+        {
+          contentType: 'multipart/form-data',
+        }
+      );
+
+      this.fileDetails = await response.json();
+      this.uploadedFile = uploadFile;
+      this.isFileUploaded = true;
+    } catch (error) {
+      this.isFileUploaded = false;
+      this.fileDetails = null;
+    }
   });
+
+  get file() {
+    const pills = [];
+
+    if (this.fileDetails.errors.length >= 1) {
+      pills.push({
+        label: 'error',
+        skin: 'error',
+        icon: 'cross',
+      });
+    }
+    if (this.fileDetails.warnings.length >= 1) {
+      pills.push({
+        label: 'warning',
+        skin: 'warning',
+        icon: 'alert-triangle',
+      });
+    }
+
+    return {
+      name: this.uploadedFile.name,
+      status: this.fileDetails.status,
+      hasWarnings: this.fileDetails.warnings.length >= 1,
+      hasErrors: this.fileDetails.errors.length >= 1,
+      errors: this.fileDetails.errors,
+      warnings: this.fileDetails.warnings,
+      pills: pills,
+      createdPersons: this.fileDetails.personsCreated,
+      createdMandatarissen: this.fileDetails.mandatarissenCreated,
+      createdBeleidsdomeinen: this.fileDetails.beleidsdomeinenCreated,
+    };
+  }
 }
