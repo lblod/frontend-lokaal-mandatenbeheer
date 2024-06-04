8ffa39069244516c35975c3e92448796f7b8bc45
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-06-04 11:42:20 +0200

    LMB-147: cannot fill in a negative count of mandaat houders

--- app/components/mandatenbeheer/bestuursorgaan-mandaten.js
+++ app/components/mandatenbeheer/bestuursorgaan-mandaten.js
@@ -16,6 +16,7 @@ export default class MandatenbeheerBestuursorgaanMandatenComponent extends Compo
 
   @tracked editMandaat = null;
   @tracked aantalHouders;
+  @tracked errorMessageAantalHouders;
 
   constructor() {
     super(...arguments);
@@ -83,7 +84,15 @@ export default class MandatenbeheerBestuursorgaanMandatenComponent extends Compo
     if (event && typeof event.preventDefault === 'function') {
       event.preventDefault();
     }
-    this.aantalHouders = event.target.value;
+    const aantal = event.target.value;
+
+    if (aantal && !this.isPositiveNumber(aantal)) {
+      this.errorMessageAantalHouders = 'Dit moet een positief getal zijn.';
+    } else {
+      this.errorMessageAantalHouders = null;
+    }
+
+    this.aantalHouders = aantal;
   }
   @action
   async saveMandaat() {
@@ -96,5 +105,10 @@ export default class MandatenbeheerBestuursorgaanMandatenComponent extends Compo
   cancelEditMandaat() {
     this.aantalHouders = null;
     this.editMandaat = false;
+  }
+
+  @action
+  isPositiveNumber(value) {
+    return value && Number(value) >= 0;
   }
 }
