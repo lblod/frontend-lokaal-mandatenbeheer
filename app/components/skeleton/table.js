250ad3185f570adb6a3835917f7a06f62e19880a
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-06-13 16:51:06 +0200

    LMB-133: simplify logic of table skeleton

--- app/components/skeleton/table.js
+++ app/components/skeleton/table.js
@@ -1,15 +1,10 @@
 import Component from '@glimmer/component';
 
-import { tracked } from '@glimmer/tracking';
-
 export default class SkeletonTableComponent extends Component {
-  @tracked columns;
-  @tracked rows;
-
-  constructor() {
-    super(...arguments);
-
-    this.columns = new Array(this.args.columns ?? 1);
-    this.rows = new Array(this.args.rows ?? 1);
+  get columns() {
+    return new Array(this.args.columns ?? 1);
+  }
+  get rows() {
+    return new Array(this.args.rows ?? 1);
   }
 }
