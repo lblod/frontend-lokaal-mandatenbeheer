6295abf1d4516a7d1833c2585f21245dbef7f21f
Ben Fidlers <ben.fidlers@qmino.com>
2024-06-26 16:42:12 +0200

    move creation beeindigd state to migration

--- app/services/mandataris-status.js
+++ app/services/mandataris-status.js
@@ -2,6 +2,7 @@ import Service from '@ember/service';
 
 import { tracked } from '@glimmer/tracking';
 import { service } from '@ember/service';
+import { MANDATARIS_BEEINDIGD_STATE } from 'frontend-lmb/utils/well-known-uris';
 
 export default class MandatarisStatusService extends Service {
   @tracked statuses = [];
@@ -10,16 +11,12 @@ export default class MandatarisStatusService extends Service {
 
   endedState = null;
 
-  constructor() {
-    super(...arguments);
-    this.endedState = this.store.createRecord('mandataris-status-code', {
-      label: 'Beëindigd',
-    });
-  }
-
   async loadStatusOptions() {
     if (this.statuses.length === 0) {
       this.statuses = await this.store.findAll('mandataris-status-code');
+      this.endedState = this.statuses.find((status) => {
+        return status.uri === MANDATARIS_BEEINDIGD_STATE;
+      });
     }
   }
 }
