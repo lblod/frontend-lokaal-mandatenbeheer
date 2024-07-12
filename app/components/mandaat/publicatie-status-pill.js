d59ddbb0a8fd3f8f6b48a8fa7af545fe786986d3
Ben Fidlers <ben.fidlers@qmino.com>
2024-07-12 14:27:52 +0200

    small bugfix show besluit in mandataris

--- app/components/mandaat/publicatie-status-pill.js
+++ app/components/mandaat/publicatie-status-pill.js
@@ -1,24 +1,9 @@
 import Component from '@glimmer/component';
 
-import { MANDATARIS_BEKRACHTIGD_PUBLICATION_STATE } from 'frontend-lmb/utils/well-known-uris';
-import { action } from '@ember/object';
-
-import { tracked } from '@glimmer/tracking';
-
 export default class MandaatPublicatieStatusPillComponent extends Component {
-  @tracked isMandatarisBekrachtigd;
-
-  @action
-  async setIsMandatarisBekrachtigd() {
-    const status = await this.args.mandataris.publicationStatus;
-    if (!status || !status.label) {
-      return true;
-    }
-
-    if (status.uri === MANDATARIS_BEKRACHTIGD_PUBLICATION_STATE) {
-      return true;
-    }
-
-    return false;
+  get isMandatarisBekrachtigd() {
+    return this.args.mandataris.get('publicationStatus').label
+      ? this.args.mandataris.get('publicationStatus').isBekrachtigd
+      : true;
   }
 }
