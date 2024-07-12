c3a12e8fe0a741e9daa57dbe4cad4e55c69f6c59
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-07-12 12:49:05 +0200

    fix: publicationstatus is a promise on mandataris

--- app/components/mandaat/publicatie-status-pill.js
+++ app/components/mandaat/publicatie-status-pill.js
@@ -1,17 +1,21 @@
 import Component from '@glimmer/component';
 
 import { MANDATARIS_BEKRACHTIGD_PUBLICATION_STATE } from 'frontend-lmb/utils/well-known-uris';
+import { action } from '@ember/object';
+
+import { tracked } from '@glimmer/tracking';
 
 export default class MandaatPublicatieStatusPillComponent extends Component {
-  get isMandatarisBekrachtigd() {
-    if (!this.args.mandataris.publicationStatus.label) {
+  @tracked isMandatarisBekrachtigd;
+
+  @action
+  async setIsMandatarisBekrachtigd() {
+    const status = await this.args.mandataris.publicationStatus;
+    if (!status || !status.label) {
       return true;
     }
 
-    if (
-      this.args.mandataris.publicationStatus.uri ===
-      MANDATARIS_BEKRACHTIGD_PUBLICATION_STATE
-    ) {
+    if (status.uri === MANDATARIS_BEKRACHTIGD_PUBLICATION_STATE) {
       return true;
     }
 
