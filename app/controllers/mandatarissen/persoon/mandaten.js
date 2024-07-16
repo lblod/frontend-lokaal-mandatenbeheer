386ea9475171ce82b6e1ca9b85442f7ce9354ec1
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-07-16 16:21:45 +0200

    LMB-517: you can only become onafahnkelijk when not all activemandatarissen are already onafhankelijk

--- app/controllers/mandatarissen/persoon/mandaten.js
+++ app/controllers/mandatarissen/persoon/mandaten.js
@@ -5,12 +5,14 @@ import { tracked } from '@glimmer/tracking';
 import { service } from '@ember/service';
 
 import { task } from 'ember-concurrency';
+import moment from 'moment';
 
 export default class MandatarissenPersoonMandatenController extends Controller {
   @service router;
 
   queryParams = ['activeOnly'];
 
+  @tracked isBecomeOnafhankelijkDisabled = false;
   @tracked isModalOpen = false;
   @tracked selectedBestuursorgaan = null;
   @tracked activeOnly = true;
@@ -41,6 +43,48 @@ export default class MandatarissenPersoonMandatenController extends Controller {
   toggleActiveOnly() {
     this.activeOnly = !this.activeOnly;
   }
+
+  isFractieOfFoldedMandatarisOnafhankelijk = task(async (foldedMandataris) => {
+    const lid = await foldedMandataris.mandataris.heeftLidmaatschap;
+    if (!lid) {
+      return true;
+    }
+
+    const fractie = await lid.binnenFractie;
+    if (fractie) {
+      const type = await fractie.fractietype;
+      return type ? type.isOnafhankelijk : false;
+    }
+
+    return false;
+  });
+
+  isDateInbetweenStartAndEnd(foldedMandataris) {
+    const now = moment();
+    return (
+      moment(foldedMandataris.foldedStart).isBefore(now) &&
+      (!foldedMandataris.foldedEnd ||
+        moment(foldedMandataris.foldedEnd).isAfter(now))
+    );
+  }
+
+  checkFracties = task(async (foldedMandatarissen) => {
+    let notAllOnafhankelijk = true;
+    for (const fold of foldedMandatarissen) {
+      if (!this.isDateInbetweenStartAndEnd || !notAllOnafhankelijk) {
+        continue;
+      }
+
+      const isOnafhankelijk =
+        await this.isFractieOfFoldedMandatarisOnafhankelijk.perform(fold);
+      if (!isOnafhankelijk) {
+        notAllOnafhankelijk = false;
+        continue;
+      }
+    }
+
+    this.isBecomeOnafhankelijkDisabled = notAllOnafhankelijk;
+  });
 
   wordtOnafhankelijk = task(async () => {
     console.log(`Wordt onafhankelijk`);
