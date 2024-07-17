ff815fffcd4846c098114c124e1447af3b396859
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-07-17 16:24:01 +0200

    LMB-130: add feature helper

---
+++ tests/integration/helpers/is-feature-enabled-test.js
@@ -0,0 +1,17 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'frontend-lmb/tests/helpers';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Helper | is-feature-enabled', function (hooks) {
+  setupRenderingTest(hooks);
+
+  // TODO: Replace this with your real tests.
+  test('it renders', async function (assert) {
+    this.set('inputValue', '1234');
+
+    await render(hbs`{{is-feature-enabled this.inputValue}}`);
+
+    assert.dom(this.element).hasText('1234');
+  });
+});
