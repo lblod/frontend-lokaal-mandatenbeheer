e4c808ca50941f6899115ba6d0ef252924c5d503
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-07-26 10:14:00 +0200

    LMB-616: fractie repository is an object instead of a class

--- app/repositories/fractie.js
+++ app/repositories/fractie.js
@@ -1,31 +1,33 @@
 import { API, STATUS_CODE } from 'frontend-lmb/utils/constants';
 
-export default class FractieRepository {
-  async createOnafhankelijkeFractie(
-    bestuursorganenInTijdUris,
-    bestuurseenheidUri
-  ) {
-    const response = await fetch(
-      `${API.MANDATARIS_SERVICE}/fracties/onafhankelijke-fractie`,
-      {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          bestuurseenheidUri: bestuurseenheidUri,
-          bestuursorgaanUrisInTijd: bestuursorganenInTijdUris,
-        }),
-      }
-    );
+export const fractieRepository = {
+  createOnafhankelijkeFractie,
+};
 
-    const jsonReponse = await response.json();
-
-    if (response.status !== STATUS_CODE.CREATED) {
-      throw new Error(jsonReponse.message);
+async function createOnafhankelijkeFractie(
+  bestuursorganenInTijdUris,
+  bestuurseenheidUri
+) {
+  const response = await fetch(
+    `${API.MANDATARIS_SERVICE}/fracties/onafhankelijke-fractie`,
+    {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        bestuurseenheidUri: bestuurseenheidUri,
+        bestuursorgaanUrisInTijd: bestuursorganenInTijdUris,
+      }),
     }
+  );
 
-    return jsonReponse.uri;
+  const jsonReponse = await response.json();
+
+  if (response.status !== STATUS_CODE.CREATED) {
+    throw new Error(jsonReponse.message);
   }
+
+  return jsonReponse.uri;
 }
