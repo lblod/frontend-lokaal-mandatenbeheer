5d78680d4fc0667d975a89aa425ecbeaec3a5c74
Ben Fidlers <ben.fidlers@qmino.com>
2024-07-30 07:05:16 +0200

    Add modal to create duplicate mandataris

--- app/controllers/organen/orgaan/mandatarissen.js
+++ app/controllers/organen/orgaan/mandatarissen.js
@@ -7,10 +7,12 @@ import { SEARCH_TIMEOUT } from 'frontend-lmb/utils/constants';
 import { getBestuursorganenMetaTtl } from 'frontend-lmb/utils/form-context/bestuursorgaan-meta-ttl';
 import { buildNewMandatarisSourceTtl } from 'frontend-lmb/utils/build-new-mandataris-source-ttl';
 import { syncNewMandatarisMembership } from 'frontend-lmb/utils/sync-new-mandataris-membership';
+import { showErrorToast, showSuccessToast } from 'frontend-lmb/utils/toasts';
 
 export default class OrganenMandatarissenController extends Controller {
   @service router;
   @service store;
+  @service toaster;
 
   @tracked filter = '';
   @tracked page = 0;
@@ -19,6 +21,11 @@ export default class OrganenMandatarissenController extends Controller {
   // we are folding the mandataris instances, so just pick a very high number here and hope our government is reasonable about the
   // number of mandatarisses that can exist
   size = 900000;
+
+  @tracked doubleMandateTitle = '';
+  @tracked doubleMandateText = '';
+  @tracked doubleMandateModal = false;
+  newestMandatarisId = null;
 
   search = task({ restartable: true }, async (searchData) => {
     await timeout(SEARCH_TIMEOUT);
@@ -34,8 +41,11 @@ export default class OrganenMandatarissenController extends Controller {
   @action
   async onCreate({ instanceTtl, instanceId }) {
     await syncNewMandatarisMembership(this.store, instanceTtl, instanceId);
-    setTimeout(() => this.router.refresh(), 1000);
     this.isCreatingMandataris = false;
+    this.newestMandatarisId = instanceId;
+    await this.checkDoubleMandataris();
+    this.router.refresh();
+    // setTimeout(() => this.router.refresh(), 1000);
   }
 
   @action
@@ -50,5 +60,55 @@ export default class OrganenMandatarissenController extends Controller {
       instanceUri,
       this.person
     );
+  }
+
+  @action
+  toggleDoubleMandateModal() {
+    this.doubleMandateModal = !this.doubleMandateModal;
+  }
+
+  @action
+  async checkDoubleMandataris() {
+    const response = await fetch(
+      `/mandataris-api/mandatarissen/${this.newestMandatarisId}/check-possible-double`
+    );
+    const jsonReponse = await response.json();
+
+    if (response.status !== 200) {
+      console.error(jsonReponse.message);
+      throw jsonReponse.message;
+    }
+
+    if (!jsonReponse.duplicateMandate || jsonReponse.hasDouble) {
+      return;
+    }
+
+    this.doubleMandateModal = true;
+    const currentMandate = jsonReponse.currentMandate;
+    const duplicateMandate = jsonReponse.duplicateMandate;
+    this.doubleMandateTitle = `Aanmaken mandaat ${duplicateMandate}`;
+    this.doubleMandateText = `
+    U heeft zonet een mandataris met het mandaat ${currentMandate} aangemaakt.
+    Normaliter heeft een mandataris met dit mandaat ook een corresponderend mandaat als ${duplicateMandate}.
+    Wenst u dit mandaat aan te maken?`;
+  }
+
+  @action
+  async createDoubleMandataris() {
+    const response = await fetch(
+      `/mandataris-api/mandatarissen/${this.newestMandatarisId}/create-linked-mandataris`,
+      { method: 'POST' }
+    );
+    const jsonReponse = await response.json();
+
+    if (response.status !== 201) {
+      console.error(jsonReponse.message);
+      showErrorToast(this.toaster, jsonReponse.message);
+    }
+    showSuccessToast(
+      this.toaster,
+      `Mandataris ${jsonReponse.createdMandate} werd succesvol aangemaakt.`
+    );
+    this.toggleDoubleMandateModal();
   }
 }
