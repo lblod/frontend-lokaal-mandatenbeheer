62edded4f47207b00d0eb7f1a2c20a6c7ee8431e
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-07-30 16:23:18 +0200

    LMB-617: update state current fractie of person is set by our service

--- app/components/mandatarissen/update-state.js
+++ app/components/mandatarissen/update-state.js
@@ -13,6 +13,7 @@ import {
   burgemeesterOnlyStates,
   notBurgemeesterStates,
 } from 'frontend-lmb/utils/well-known-uris';
+import { mandatarisRepository } from 'frontend-lmb/repositories/mandataris';
 
 export default class MandatarissenUpdateState extends Component {
   @tracked newStatus = null;
@@ -183,18 +184,15 @@ export default class MandatarissenUpdateState extends Component {
       newMandataris,
       this.selectedFractie
     );
-
-    const personOfMandataris = await newMandataris.isBestuurlijkeAliasVan;
-    personOfMandataris.fractie = this.selectedFractie;
-    personOfMandataris.save();
+    await mandatarisRepository.updateCurrentFractieForPerson(newMandataris.id);
 
     return newMandataris;
   }
 
-  endMandataris() {
+  async endMandataris() {
     this.args.mandataris.einde = this.date;
 
-    return this.args.mandataris.save();
+    return await this.args.mandataris.save();
   }
 
   onStateChanged(newMandataris) {
