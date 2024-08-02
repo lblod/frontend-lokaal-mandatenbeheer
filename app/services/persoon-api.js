a9fab6abd5fc0e0191e5d58291cd2ca3a5a2e625
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-08-02 11:46:11 +0200

    LMB-617: create persoon api service

---
+++ app/services/persoon-api.js
@@ -0,0 +1,53 @@
+import Service from '@ember/service';
+
+import { service } from '@ember/service';
+
+import { API, STATUS_CODE } from 'frontend-lmb/utils/constants';
+
+export default class PersoonApiService extends Service {
+  @service store;
+
+  async getMandatarisFracties(persoonId, bestuursperiodeId) {
+    const response = await fetch(
+      `${API.MANDATARIS_SERVICE}/personen/${persoonId}/bestuursperiode/${bestuursperiodeId}/fracties`
+    );
+    const jsonReponse = await response.json();
+
+    if (response.status !== STATUS_CODE.OK) {
+      console.error(jsonReponse.message);
+      throw {
+        status: response.status,
+        message: jsonReponse.message,
+      };
+    }
+
+    return await this.store.query('fractie', {
+      'filter[:id:]': jsonReponse.fracties.join(','),
+    });
+  }
+
+  async getCurrentFractie(persoonId, bestuursperiodeId) {
+    const response = await fetch(
+      `${API.MANDATARIS_SERVICE}/personen/${persoonId}/bestuursperiode/${bestuursperiodeId}/current-fractie`
+    );
+    const jsonReponse = await response.json();
+
+    if (response.status !== STATUS_CODE.OK) {
+      console.error(jsonReponse.message);
+      throw {
+        status: response.status,
+        message: jsonReponse.message,
+      };
+    }
+
+    if (!jsonReponse.fractie) {
+      return null;
+    }
+
+    return (
+      await this.store.query('fractie', {
+        'filter[:uri:]': jsonReponse.fractie,
+      })
+    ).at(0);
+  }
+}
