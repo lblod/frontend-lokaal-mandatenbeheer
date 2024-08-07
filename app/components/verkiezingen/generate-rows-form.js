35de9085094f605fd1e42186513b2c5989ac7c52
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-08-07 16:30:55 +0200

    LMB-473: validate the input of the rows

--- app/components/verkiezingen/generate-rows-form.js
+++ app/components/verkiezingen/generate-rows-form.js
@@ -2,6 +2,7 @@ import Component from '@glimmer/component';
 
 import { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 import { task, restartableTask, timeout } from 'ember-concurrency';
 import { INPUT_DEBOUNCE } from 'frontend-lmb/utils/constants';
@@ -12,6 +13,8 @@ export default class GenerateRowsFormComponent extends Component {
   @tracked mandaatOptions;
   @tracked selectedMandaat;
   @tracked rowsToGenerate;
+  @tracked rowWarnings = [];
+  @tracked lengthExistingMandaten = 0;
 
   constructor() {
     super(...arguments);
@@ -33,17 +36,76 @@ export default class GenerateRowsFormComponent extends Component {
     );
   });
 
+  @action
+  async selectMandaat(mandaatOption) {
+    this.selectedMandaat = mandaatOption;
+    await this.checkPossibleMandatenToGenerate.perform();
+  }
+
+  checkPossibleMandatenToGenerate = restartableTask(async () => {
+    this.rowWarnings = [];
+    this.lengthExistingMandaten = (
+      await this.store.query('mandataris', {
+        page: {
+          size: 9999,
+        },
+        'filter[bekleedt][:id:]': this.selectedMandaat.parent.id,
+        'filter[bekleedt][bevat-in][:id:]': this.args.bestuursorgaan.id,
+      })
+    ).length;
+
+    if (
+      this.selectedMandaat.parent.maxAantalHouders &&
+      this.lengthExistingMandaten >=
+        this.selectedMandaat.parent.maxAantalHouders
+    ) {
+      this.rowWarnings.push(
+        `Je hebt het maximum aantal houders voor dit mandaat bereikt.`
+      );
+    }
+    if (
+      this.selectedMandaat.parent.minAantalHouders &&
+      this.lengthExistingMandaten < this.selectedMandaat.parent.minAantalHouders
+    ) {
+      const minimumToCreate =
+        this.selectedMandaat.parent.minAantalHouders -
+        this.lengthExistingMandaten;
+      this.rowWarnings.push(
+        `Je moet nog minimum ${minimumToCreate} mandaten aanmaken voor dit bestuursorgaan.`
+      );
+    }
+
+    if (this.selectedMandaat.parent.maxAantalHouders) {
+      this.rowsToGenerate =
+        this.selectedMandaat.parent.maxAantalHouders -
+        this.lengthExistingMandaten;
+    } else {
+      this.rowsToGenerate = 1;
+    }
+  });
+
   validateRows = restartableTask(async (event) => {
     await timeout(INPUT_DEBOUNCE);
     const inputValue = parseInt(event.target?.value);
     if (isNaN(inputValue)) {
       this.rowsToGenerate = null;
-      this.rowsError = 'Geef een positief getal in.';
+      this.rowWarnings.push('Geef een positief getal in.');
       return;
+    }
+    if (
+      inputValue + this.lengthExistingMandaten >
+      this.selectedMandaat.parent.maxAantalHouders
+    ) {
+      const toMuch =
+        inputValue -
+        this.selectedMandaat.parent.maxAantalHouders +
+        this.lengthExistingMandaten;
+      this.rowWarnings.push(
+        `Je gaat meer mandaten aanmaken dan er gewenst is ${toMuch} teveel.`
+      );
     }
 
     this.rowsToGenerate = inputValue;
-    console.log(`rowsToGenerate:`, this.rowsToGenerate);
   });
 
   onConfigReady = restartableTask(async () => {
