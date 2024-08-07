0491c9a2fd8b2fe2f9d2c41cf8727782ad751c11
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-08-07 15:01:13 +0200

    LMB-473: show the madnaten for the bestuursorgaan the generate is opened

--- app/components/verkiezingen/generate-rows-form.js
+++ app/components/verkiezingen/generate-rows-form.js
@@ -8,9 +8,8 @@ import { task, restartableTask } from 'ember-concurrency';
 export default class GenerateRowsFormComponent extends Component {
   @service store;
 
-  @tracked formInfo;
-  @tracked showErrors;
-  @tracked isFormValid;
+  @tracked mandaatOptions;
+  @tracked selectedMandaat;
 
   constructor() {
     super(...arguments);
@@ -18,7 +17,19 @@ export default class GenerateRowsFormComponent extends Component {
     this.initForm.perform();
   }
 
-  initForm = task(async () => {});
+  initForm = task(async () => {
+    const mandaten = await this.args.bestuursorgaan.bevat;
+    this.mandaatOptions = await Promise.all(
+      mandaten.map(async (mandaat) => {
+        const bestuursfunctie = await mandaat.bestuursfunctie;
+
+        return {
+          parent: mandaat,
+          label: bestuursfunctie.label,
+        };
+      })
+    );
+  });
 
   onConfigReady = restartableTask(async () => {
     this.args.onConfigReceived({
