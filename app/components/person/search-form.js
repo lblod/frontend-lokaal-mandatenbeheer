7e18f984133ce5b1fb322cee0505312186c42528
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-08-22 15:54:13 +0200

    LMB-552: remove unused getter in search-form

--- app/components/person/search-form.js
+++ app/components/person/search-form.js
@@ -159,8 +159,4 @@ export default class SharedPersoonPersoonSearchFormComponent extends Component {
       });
     }
   }
-
-  get searchElectedPerson() {
-    return this.args.searchElected;
-  }
 }
