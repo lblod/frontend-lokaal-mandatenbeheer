49f06fba9304e2afad24a8f634bf958b247bee0a
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-09-10 14:37:37 +0200

    LMB-630: add getter for mandataris status code 'isVerhinderd'

--- app/models/mandataris-status-code.js
+++ app/models/mandataris-status-code.js
@@ -3,4 +3,8 @@ import Model, { attr } from '@ember-data/model';
 export default class MandatarisStatusCodeModel extends Model {
   @attr label;
   @attr uri;
+
+  get isVerhinderd() {
+    return this.label && this.label.toLowerCase() === 'verhinderd';
+  }
 }
