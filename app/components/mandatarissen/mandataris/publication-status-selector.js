5b3f13ce4fec7cfe973bca1011a88259d789ea21
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-09-17 10:09:31 +0200

    LMB-689: create the form in ember for setting the decision link

--- app/components/mandatarissen/mandataris/publication-status-selector.js
+++ app/components/mandatarissen/mandataris/publication-status-selector.js
@@ -4,19 +4,28 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import { service } from '@ember/service';
 
+import { restartableTask, task } from 'ember-concurrency';
+
 export default class MandatarissenMandatarisPublicationStatusSelectorComponent extends Component {
   @service store;
 
   @tracked options = [];
   @tracked isDisabled = false;
   @tracked showLinkToDecisionModal;
+  @tracked selectedType;
+  @tracked selectedDecisionPredicate;
+  @tracked linkToDecision;
+  @tracked isLinkToPage;
+  @tracked selectedPublicationStatus;
 
   get mandataris() {
     return this.args.mandataris;
   }
 
   get publicationStatus() {
-    return this.mandataris.publicationStatus;
+    return this.selectedPublicationStatus
+      ? this.selectedPublicationStatus
+      : this.mandataris.publicationStatus;
   }
 
   get sortedOptions() {
@@ -45,10 +54,10 @@ export default class MandatarissenMandatarisPublicationStatusSelectorComponent e
 
   @action
   async onUpdate(publicationStatus) {
-    console.log(`publicationStatus`, publicationStatus);
+    this.selectedPublicationStatus = publicationStatus;
     if (publicationStatus.isBekrachtigd) {
-      this.showLinkToDecisionModal = true;
       this.isDisabled = true;
+      this.showLinkToDecisionModal = true;
     } else {
       await this.setStatus(publicationStatus);
     }
@@ -61,5 +70,58 @@ export default class MandatarissenMandatarisPublicationStatusSelectorComponent e
     if (this.args.onUpdate) {
       this.args.onUpdate();
     }
+  }
+
+  @action
+  setType(selectedType) {
+    this.selectedType = selectedType;
+    this.isLinkToPage = selectedType.id === 3;
+  }
+
+  @action
+  setDecisionPredicate(predicate) {
+    this.selectedDecisionPredicate = predicate;
+  }
+
+  @action
+  async cancelAddDecision() {
+    this.selectedPublicationStatus = await this.mandataris.publicationStatus;
+    this.showLinkToDecisionModal = false;
+    this.args.onUpdate();
+  }
+
+  isValidUri(inputValue) {
+    if (!inputValue) {
+      return false;
+    }
+
+    return inputValue.trim() !== '';
+  }
+
+  setLinkTodecision = restartableTask(async (event) => {
+    this.linkToDecision = event.target?.value;
+  });
+
+  addDecisionToMandataris = task(async () => {
+    // Todo: look for decision URI if it exists and link it to the mandataris
+  });
+
+  get canSaveLinkToDecision() {
+    return this.isValidUri(this.linkToDecision);
+  }
+
+  get typeOptions() {
+    return [
+      { id: 1, label: 'Artikel' },
+      { id: 2, label: 'Besluit' },
+      { id: 3, label: 'Link naar pagina' },
+    ];
+  }
+
+  get predicateOptions() {
+    return [
+      { id: 1, label: 'bekrachtigtAanstellingVan' },
+      { id: 2, label: 'bekrachtigtOntslagVan' },
+    ];
   }
 }
