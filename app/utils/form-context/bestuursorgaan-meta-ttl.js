735a1c4fed2d2c9b7f66c40e0ae686a4c029cc97
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-09-24 10:38:20 +0200

    lmb-700: limit the start and endate input in to the bestuursorgaan periode

--- app/utils/form-context/bestuursorgaan-meta-ttl.js
+++ app/utils/form-context/bestuursorgaan-meta-ttl.js
@@ -1,4 +1,5 @@
-import { EXT } from 'frontend-lmb/rdf/namespaces';
+import { EXT, LMB } from 'frontend-lmb/rdf/namespaces';
+import { NULL_DATE } from 'frontend-lmb/utils/constants';
 
 // Expects bestuursorgaan in de tijd
 export const getBestuursorganenMetaTtl = (bestuursorgaan) => {
@@ -12,10 +13,22 @@ export const getBestuursorganenMetaTtl = (bestuursorgaan) => {
     })
     .join(', ');
 
+  const period = getBestuursperiodeForBestuursorganen(bestuursorgaan);
+
   return `
     @prefix ext: <http://mu.semte.ch/vocabularies/ext/> .
+    @prefix lmb: <${LMB.value}> .
 
     ext:applicationContext ext:currentBestuursorgaan ${bestuursorgaanUris} .
+
+    ${
+      period
+        ? `
+        ext:applicationContext lmb:periodStart """${period.startDate}""";
+                               lmb:periodEnd """${period.endDate}""".
+        `
+        : ''
+    }
   `;
 };
 
@@ -29,4 +42,37 @@ export const loadBestuursorgaanUrisFromContext = (storeOptions) => {
   );
 
   return bestuursorgaanUri?.map((node) => node.object.value);
+};
+
+export const loadBestuursorgaanPeriodFromContext = (storeOptions) => {
+  const startDate = storeOptions.store.any(
+    EXT('applicationContext'),
+    LMB('periodStart'),
+    null,
+    storeOptions.metaGraph
+  );
+  const endDate = storeOptions.store.any(
+    EXT('applicationContext'),
+    LMB('periodEnd'),
+    null,
+    storeOptions.metaGraph
+  );
+
+  return {
+    startDate: new Date(startDate.value),
+    endDate: new Date(endDate.value),
+  };
+};
+
+const getBestuursperiodeForBestuursorganen = (bestuursorganen) => {
+  if (bestuursorganen.length === 1) {
+    const bestuursorgaan = bestuursorganen.at(0);
+
+    return {
+      startDate: bestuursorgaan.bindingStart,
+      endDate: bestuursorgaan.bindingEinde ?? NULL_DATE,
+    };
+  }
+
+  return null;
 };
