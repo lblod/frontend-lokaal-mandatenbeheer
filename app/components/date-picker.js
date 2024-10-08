cf771e04858cfce0af378019cfe277c7d7d53854
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-10-08 11:34:01 +0200

    lmb-856: add an error message to the input depending on min and max date

--- app/components/date-picker.js
+++ app/components/date-picker.js
@@ -10,16 +10,14 @@ export default class DatepickerComponent extends Component {
   elementId = `date-${guidFor(this)}`;
 
   @tracked dateInputString;
+  @tracked errorMessage;
 
   constructor() {
     super(...arguments);
 
     if (this.args.value) {
-      this.dateInputString = moment(this.bestuursorgaanEndDate).format(
-        'DD-MM-YYYY'
-      );
-
-      console.log(`startValue`, this.dateInputString);
+      this.dateInputString = moment(this.args.value).format('DD-MM-YYYY');
+      this.processDate(this.args.value);
     }
   }
 
@@ -27,13 +25,59 @@ export default class DatepickerComponent extends Component {
   onChange(event) {
     const inputValue = event.target?.value;
     this.dateInputString = inputValue;
-    const date = new Date(this.dateInputString);
-    if (this.isValidDate(date) && this.args.onChange) {
-      this.args.onChange(date);
-    }
+    this.processDate(new Date(this.dateInputString));
   }
 
   isValidDate(date) {
     return date instanceof Date && !isNaN(date);
+  }
+
+  isDateInRange(date, min, max) {
+    date.setHours(0, 0, 0, 0);
+    if (!min && !max) {
+      return true;
+    }
+
+    if (min && date.getTime() < min.getTime()) {
+      return false;
+    }
+    if (max && date.getTime() > max.getTime()) {
+      return false;
+    }
+
+    return true;
+  }
+
+  processDate(date) {
+    if (!this.isValidDate(date)) {
+      return;
+    }
+
+    if (this.args.onChange) {
+      this.args.onChange(date);
+    }
+
+    let minDateTime = null;
+    let maxDateTime = null;
+    if (this.args.from) {
+      minDateTime = new Date(this.args.from);
+      minDateTime.setHours(0, 0, 0, 0);
+    }
+    if (this.args.to) {
+      maxDateTime = new Date(this.args.to);
+      maxDateTime.setHours(0, 0, 0, 0);
+    }
+
+    if (!this.isDateInRange(date, minDateTime, maxDateTime)) {
+      const stringMinDate = minDateTime
+        ? moment(minDateTime).format('DD-MM-YYYY')
+        : null;
+      const stringMaxDate = maxDateTime
+        ? moment(maxDateTime).format('DD-MM-YYYY')
+        : null;
+      this.errorMessage = `Kies een datum tussen ${stringMinDate || 'een moment in het verleden'} en ${stringMaxDate || 'een moment in de toekomst'}.`;
+    } else {
+      this.errorMessage = null;
+    }
   }
 }
