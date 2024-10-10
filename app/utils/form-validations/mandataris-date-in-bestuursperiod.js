8c07c49c659d3c3608ba86ab06e3456da0feec0d
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-10-10 19:32:58 +0200

    lmb-856: reuse the logic from the date input to see if the date is in range

--- app/utils/form-validations/mandataris-date-in-bestuursperiod.js
+++ app/utils/form-validations/mandataris-date-in-bestuursperiod.js
@@ -1,6 +1,7 @@
 import moment from 'moment';
 import { NULL_DATE } from '../constants';
 import { loadBestuursorgaanPeriodFromContext } from '../form-context/application-context-meta-ttl';
+import { isDateInRange } from 'frontend-lmb/components/date-input';
 
 export const isValidMandatarisDate = ([dateLiteral], options) => {
   if (!dateLiteral) {
@@ -9,18 +10,11 @@ export const isValidMandatarisDate = ([dateLiteral], options) => {
 
   const date = new Date(dateLiteral.value);
   const period = loadBestuursorgaanPeriodFromContext(options);
+  let maxDate = period.endDate;
 
-  if (moment(period.endDate).isSame(NULL_DATE)) {
-    if (moment(date).isSameOrAfter(period.startDate)) {
-      return true;
-    }
-    return false;
+  if (moment(period.endDate).isSame(moment(NULL_DATE))) {
+    maxDate = null;
   }
 
-  return moment(date).isBetween(
-    moment(period.startDate),
-    moment(period.endDate),
-    'day',
-    '[]'
-  );
+  return isDateInRange(date, period.startDate, maxDate);
 };
