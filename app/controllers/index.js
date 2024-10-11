5b573b0115ea94308f4d5a663bd41314f14494a8
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-10-11 13:56:40 +0200

    lmb-812: move the can show legislatuur module to the router

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,22 +1,8 @@
 import Controller from '@ember/controller';
 
 import { service } from '@ember/service';
-import { tracked } from '@glimmer/tracking';
 
 export default class IndexController extends Controller {
   @service currentSession;
   @service features;
-
-  @tracked showLegislatuurModule;
-
-  constructor() {
-    super(...arguments);
-
-    this.showModules();
-  }
-
-  async showModules() {
-    this.showLegislatuurModule =
-      await this.currentSession.showLegislatuurModule();
-  }
 }
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,9 +3,16 @@ import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 
 export default class IndexRoute extends Route {
+  @service currentSession;
   @service session;
 
   async beforeModel(transition) {
     this.session.requireAuthentication(transition, 'login');
+  }
+
+  async model() {
+    return {
+      showLegislatuurModule: await this.currentSession.showLegislatuurModule(),
+    };
   }
 }
