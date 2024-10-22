77e27e2a54fcfba53bcd31afecc8c5ef189dc45e
Ben Fidlers <ben.fidlers@qmino.com>
2024-10-22 15:55:22 +0200

    add table for bekrachtigingen

--- app/controllers/organen/orgaan/bulk-bekrachtiging.js
+++ app/controllers/organen/orgaan/bulk-bekrachtiging.js
@@ -1,3 +1,10 @@
 import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
 
-export default class BulkBekrachtigingController extends Controller {}
+export default class BulkBekrachtigingController extends Controller {
+  queryParams = ['size', 'page', 'sort'];
+
+  @tracked size = 900000;
+  @tracked page = 0;
+  @tracked sort = 'is-bestuurlijke-alias-van.achternaam';
+}
--- app/routes/organen/orgaan/bulk-bekrachtiging.js
+++ app/routes/organen/orgaan/bulk-bekrachtiging.js
@@ -1,13 +1,63 @@
 import Route from '@ember/routing/route';
 
+import { service } from '@ember/service';
+
+import { foldMandatarisses } from 'frontend-lmb/utils/fold-mandatarisses';
+
 export default class BulkBekrachtigingRoute extends Route {
-  async model() {
+  @service store;
+  @service installatievergadering;
+
+  queryParams = {
+    page: { refreshModel: true },
+    size: { refreshModel: true },
+    sort: { refreshModel: true },
+  };
+
+  async model(params) {
     const parentModel = this.modelFor('organen.orgaan');
     const currentBestuursorgaan = await parentModel.currentBestuursorgaan;
 
+    let mandatarissen;
+    if (currentBestuursorgaan) {
+      const options = this.getOptions(params, currentBestuursorgaan);
+
+      mandatarissen = await this.store.query('mandataris', options);
+    }
+    const folded = await foldMandatarisses(params, mandatarissen);
+
     return {
+      mandatarissen: folded,
       bestuursorgaan: parentModel.bestuursorgaan,
+      selectedBestuursperiode: parentModel.selectedBestuursperiode,
       currentBestuursorgaan: currentBestuursorgaan,
     };
+  }
+
+  getOptions(params, bestuursOrgaan) {
+    const queryParams = {
+      sort: params.sort,
+      page: {
+        number: params.page,
+        size: params.size,
+      },
+      filter: {
+        bekleedt: {
+          'bevat-in': {
+            id: bestuursOrgaan.id,
+          },
+        },
+        ':has:is-bestuurlijke-alias-van': true,
+      },
+      include: [
+        'is-bestuurlijke-alias-van',
+        'bekleedt.bestuursfunctie',
+        'heeft-lidmaatschap.binnen-fractie',
+        'status',
+        'publication-status',
+      ].join(','),
+    };
+
+    return queryParams;
   }
 }
