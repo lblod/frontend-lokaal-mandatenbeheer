be7127464db2ebc73bff6095c04cd1f4ee0a242d
Ben Fidlers <ben.fidlers@qmino.com>
2024-10-22 16:05:13 +0200

    add checkbox

--- app/controllers/organen/orgaan/bulk-bekrachtiging.js
+++ app/controllers/organen/orgaan/bulk-bekrachtiging.js
@@ -1,5 +1,6 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class BulkBekrachtigingController extends Controller {
   queryParams = ['size', 'page', 'sort'];
@@ -7,4 +8,8 @@ export default class BulkBekrachtigingController extends Controller {
   @tracked size = 900000;
   @tracked page = 0;
   @tracked sort = 'is-bestuurlijke-alias-van.achternaam';
+
+  @action checkBox(mandataris) {
+    console.log(mandataris);
+  }
 }
