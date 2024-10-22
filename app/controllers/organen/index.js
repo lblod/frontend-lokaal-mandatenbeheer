a4c295d81dedde17176d164424633440ad75eeb7
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-10-22 11:59:17 +0200

    lmb-599: remove extra check for rr serial digits

--- app/controllers/organen/index.js
+++ app/controllers/organen/index.js
@@ -8,8 +8,6 @@ import {
 } from 'frontend-lmb/utils/well-known-ids';
 import moment from 'moment';
 
-import moment from 'moment';
-
 export default class OrganenIndexController extends Controller {
   queryParams = ['sort', 'activeOrgans', 'selectedTypes', 'bestuursperiode'];
   @service store;
