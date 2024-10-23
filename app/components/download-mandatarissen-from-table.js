2e286d1cd1f7b07cdd215dbbbc4e524d236e890a
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-10-23 14:42:10 +0200

    lmb-796: add the bestuursorgaan  to the filter + retrieve boi from bestuursorgaan

--- app/components/download-mandatarissen-from-table.js
+++ app/components/download-mandatarissen-from-table.js
@@ -8,9 +8,28 @@ export default class DownloadMandatarissenFromTableComponent extends Component {
   @service('mandataris-api') mandatarisApi;
 
   download = task(async () => {
+    const boiId = await this.getBestuursorgaanInTijdForPeriod();
+
     await this.mandatarisApi.downloadAsCsv({
       bestuursperiodeId: this.args.bestuursperiode?.id,
       activeOnly: this.args.activeOnly,
+      bestuursorgaanId: boiId,
     });
   });
+
+  async getBestuursorgaanInTijdForPeriod() {
+    const bestuursorganenInTijdFromPeriod =
+      (await this.args.bestuursperiode.heeftBestuursorganenInTijd) ?? [];
+    const bestuursorganenInTijd =
+      (await this.args.bestuursorgaan?.heeftTijdsspecialisaties) ?? [];
+
+    const fromPeriodIds = bestuursorganenInTijdFromPeriod.map((boi) => boi.id);
+    const boiIds = bestuursorganenInTijd.map((boi) => boi.id);
+    const boiInPeriod = boiIds.filter((id) => fromPeriodIds.includes(id));
+    if (boiInPeriod.length >= 1) {
+      return boiInPeriod.at(0);
+    }
+
+    return null;
+  }
 }
