ed7f3f008b2f1949bc972df82d245cfef7971f76
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-10-23 13:53:51 +0200

    lmb-796: create download mandataris table content component

---
+++ app/components/download-mandatarissen-from-table.js
@@ -0,0 +1,15 @@
+import Component from '@glimmer/component';
+
+import { service } from '@ember/service';
+
+import { task } from 'ember-concurrency';
+
+export default class DownloadMandatarissenFromTableComponent extends Component {
+  @service('mandataris-api') mandatarisApi;
+
+  download = task(async () => {
+    await this.mandatarisApi.downloadAsCsv({
+      bestuursperiodeId: this.args.bestuursperiode?.id,
+    });
+  });
+}
