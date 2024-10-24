09bee80b1a80c1478d94c6f643eac22293d543f9
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-10-24 11:46:00 +0200

    lmb-796: add a modal with some info before donwlaoding the mandataris data

--- app/components/download-mandatarissen-from-table.js
+++ app/components/download-mandatarissen-from-table.js
@@ -3,9 +3,12 @@ import Component from '@glimmer/component';
 import { service } from '@ember/service';
 
 import { task } from 'ember-concurrency';
+import { tracked } from '@glimmer/tracking/';
 
 export default class DownloadMandatarissenFromTableComponent extends Component {
   @service('mandataris-api') mandatarisApi;
+
+  @tracked isDownloadModalOpen;
 
   download = task(async () => {
     let boiId = null;
@@ -19,6 +22,7 @@ export default class DownloadMandatarissenFromTableComponent extends Component {
       bestuursorgaanId: boiId,
       sort: this.args.sort,
     });
+    this.isDownloadModalOpen = false;
   });
 
   async getBestuursorgaanInTijdForPeriod() {
@@ -35,5 +39,26 @@ export default class DownloadMandatarissenFromTableComponent extends Component {
     }
 
     return null;
+  }
+
+  get activefilters() {
+    return [
+      {
+        isActive: this.args.bestuursperiode,
+        label: `${this.args.bestuursperiode.start}-${this.args.bestuursperiode.einde ?? 'heden'}`,
+      },
+      {
+        isActive: this.args.bestuursorgaan && this.args.bestuursorgaan.naam,
+        label: this.args.bestuursorgaan.naam,
+      },
+      {
+        isActive: this.args.activeOnly,
+        label: 'Actieve mandatarissen',
+      },
+      {
+        isActive: this.args.sort,
+        label: 'Column sorting',
+      },
+    ];
   }
 }
