68af689c6697b3dc68555b6fc9b21dc26db3df5e
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-10-25 13:44:05 +0200

    lmb-796: remove the modal as we will be able to apply all filters on the data

--- app/components/download-mandatarissen-from-table.js
+++ app/components/download-mandatarissen-from-table.js
@@ -3,12 +3,9 @@ import Component from '@glimmer/component';
 import { service } from '@ember/service';
 
 import { task } from 'ember-concurrency';
-import { tracked } from '@glimmer/tracking/';
 
 export default class DownloadMandatarissenFromTableComponent extends Component {
   @service('mandataris-api') mandatarisApi;
-
-  @tracked isDownloadModalOpen;
 
   download = task(async () => {
     let boiId = null;
@@ -25,7 +22,6 @@ export default class DownloadMandatarissenFromTableComponent extends Component {
       bestuursFunctieCodeIds: this.bestuursFunctieCodeIds ?? [],
       sort: this.args.sort,
     });
-    this.isDownloadModalOpen = false;
   });
 
   async getBestuursorgaanInTijdForPeriod() {
@@ -54,26 +50,5 @@ export default class DownloadMandatarissenFromTableComponent extends Component {
 
   get bestuursFunctieCodeIds() {
     return this.args.bestuursFunctieCodes?.map((code) => code.id);
-  }
-
-  get activefilters() {
-    return [
-      {
-        isActive: this.args.bestuursperiode,
-        label: `${this.args.bestuursperiode.start}-${this.args.bestuursperiode.einde ?? 'heden'}`,
-      },
-      {
-        isActive: this.args.bestuursorgaan && this.args.bestuursorgaan.naam,
-        label: this.args.bestuursorgaan?.naam ?? '',
-      },
-      {
-        isActive: this.args.activeOnly,
-        label: 'Actieve mandatarissen',
-      },
-      {
-        isActive: this.args.sort,
-        label: 'Gesorteerd op kolom',
-      },
-    ];
   }
 }
