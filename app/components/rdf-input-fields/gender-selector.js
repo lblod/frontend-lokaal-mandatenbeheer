a8084d363114b8f18c037425ba13a1505e3f0921
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-11-04 15:39:01 +0100

    lmb-973: fillin the gender male/female form the rijksregisternumber

---
+++ app/components/rdf-input-fields/gender-selector.js
@@ -0,0 +1,68 @@
+import { service } from '@ember/service';
+
+import { triplesForPath } from '@lblod/submission-form-helpers';
+import { task, timeout } from 'ember-concurrency';
+import { NamedNode } from 'rdflib';
+
+import { isPredicateInObserverChange } from 'frontend-lmb/utils/is-predicate-in-observer-change';
+import { INPUT_DEBOUNCE, PERSON_PREDICATE } from 'frontend-lmb/utils/constants';
+import {
+  isBiologicalMale,
+  isGenderKnown,
+  isValidRijksregisternummer,
+} from 'frontend-lmb/utils/form-validations/rijksregisternummer';
+import { SKOS } from 'frontend-lmb/rdf/namespaces';
+import RdfInputFieldsConceptSchemeSelectorComponent from './concept-scheme-selector';
+
+export default class RDFGenderSelector extends RdfInputFieldsConceptSchemeSelectorComponent {
+  @service store;
+
+  constructor() {
+    super(...arguments);
+
+    this.storeOptions.store.registerObserver(async (formChange) => {
+      const mustTrigger = isPredicateInObserverChange(
+        formChange,
+        new NamedNode(PERSON_PREDICATE.identifier)
+      );
+      if (mustTrigger && !this.checkForAutomaticFillIn.isRunning) {
+        await this.checkForAutomaticFillIn.perform();
+      }
+    });
+  }
+
+  checkForAutomaticFillIn = task(async () => {
+    await timeout(INPUT_DEBOUNCE);
+    const identifier = this.storeOptions.store.any(
+      this.storeOptions.sourceNode,
+      new NamedNode(PERSON_PREDICATE.identifier),
+      undefined,
+      this.storeOptions.sourceGraph
+    );
+    const rrnString = this.storeOptions.store.any(
+      identifier,
+      SKOS('notation'),
+      undefined,
+      this.storeOptions.sourceGraph
+    )?.value;
+    if (isValidRijksregisternummer(rrnString) && isGenderKnown(rrnString)) {
+      const male = {
+        subject: new NamedNode(
+          'http://publications.europa.eu/resource/authority/human-sex/FEMALE'
+        ),
+        label: 'Mannelijk',
+      };
+      const woman = {
+        subject: new NamedNode(
+          'http://publications.europa.eu/resource/authority/human-sex/MALE'
+        ),
+        label: 'Vrouwelijk',
+      };
+      this.selected = isBiologicalMale(rrnString) ? male : woman;
+    }
+  });
+
+  get title() {
+    return this.args.field?.label || 'Geslacht';
+  }
+}
