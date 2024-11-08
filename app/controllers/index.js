eabff613373d463ea65c413d1ba24cbe8d113504
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-11-08 09:30:19 +0100

    lmb-1010: only show the admin panel card when not impersonating

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -5,6 +5,7 @@ import { service } from '@ember/service';
 export default class IndexController extends Controller {
   @service currentSession;
   @service features;
+  @service impersonation;
 
   get showLegislatuurModule() {
     return this.currentSession.showLegislatuurModule;
