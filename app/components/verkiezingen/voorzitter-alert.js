f3dd83f3d12a5f9697f4b76b7bf4f77bb0bb286d
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-12-16 16:29:28 +0100

    lmb-858: voorzitter alert is also in the alert group

--- app/components/verkiezingen/voorzitter-alert.js
+++ app/components/verkiezingen/voorzitter-alert.js
@@ -1,15 +1,21 @@
 import Component from '@glimmer/component';
 
+import { action } from '@ember/object';
+
+import { consume } from 'ember-provide-consume-context';
+
 export default class VerkiezingenVoorzitterAlertComponent extends Component {
+  @consume('alert-group') alerts;
+
   get errorMessage() {
     if (this.args.mandatarissen.length === 0) {
-      return '';
+      return null;
     }
     if (
       this.args.mandatarissen.length === 1 &&
       this.args.mandatarissen.at(0).isVoorzitter
     ) {
-      return '';
+      return null;
     }
     const hasVoorzitter = this.args.mandatarissen.some(
       (mandataris) =>
@@ -18,6 +24,23 @@ export default class VerkiezingenVoorzitterAlertComponent extends Component {
     if (!hasVoorzitter) {
       return `Je hebt nog geen voorzitter voor het orgaan ${this.args.bestuursorgaanIT.get('isTijdsspecialisatieVan').get('naam')} aangeduid. Voeg hiervoor een nieuwe mandataris met het mandaat voorzitter toe.`;
     }
-    return '';
+    return null;
+  }
+
+  @action
+  onUpdate() {
+    const exists = this.alerts.findBy('id', this.errorMessageId);
+    if (exists) {
+      this.alerts.removeObject(exists);
+    }
+
+    if (!this.errorMessage) {
+      return;
+    }
+
+    this.alerts.pushObject({
+      id: this.errorMessageId,
+      message: this.errorMessage,
+    });
   }
 }
