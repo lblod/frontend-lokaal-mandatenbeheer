2d978becb7cfa08f1f74df37dfee962ea30a6b70
karel kremer <karel.kremer@redpencil.io>
2024-12-19 14:31:44 +0100

    allow adding and removing custom fields, hide functionality behind feature flag

--- app/components/generate-custom-field-button.js
+++ app/components/generate-custom-field-button.js
@@ -1,9 +1,60 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import { JSON_API_TYPE } from 'frontend-lmb/utils/constants';
 
 export default class GenerateCustomFieldButtonComponent extends Component {
+  @service formReplacements;
+  @tracked showModal = false;
+  @tracked loading = false;
+  @tracked fieldName = '';
+
+  get invalidName() {
+    return !this.fieldName || this.fieldName.trim().length < 1;
+  }
+
+  get disabled() {
+    return this.invalidName;
+  }
+
   @action
-  onAddField() {
-    console.log(`add clicked ${this.args.form.id}`);
+  async onAddField() {
+    this.showModal = true;
+    this.fieldName = '';
+  }
+
+  @action
+  closeModal() {
+    this.showModal = false;
+  }
+
+  @action async onSaveField() {
+    this.loading = true;
+    const result = await fetch(`/form-content/${this.args.form.id}/fields`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': JSON_API_TYPE,
+      },
+      body: JSON.stringify({
+        displayType:
+          'http://lblod.data.gift/display-types/lmb/custom-string-input',
+        order: 9000,
+        name: this.fieldName,
+      }),
+    });
+
+    const body = await result.json();
+    const newFormId = body.id;
+    this.formReplacements.setReplacement(this.args.form.id, newFormId);
+    if (this.args.onUpdate) {
+      this.args.onUpdate();
+    }
+    this.loading = false;
+    this.showModal = false;
+  }
+
+  @action updateName(event) {
+    this.fieldName = event.target.value;
   }
 }
--- tests/unit/services/form-replacements-test.js
+++ tests/unit/services/form-replacements-test.js
@@ -1,12 +1,12 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'frontend-lmb/tests/helpers';
 
-module('Unit | Service | form-repository', function (hooks) {
+module('Unit | Service | form-replacements', function (hooks) {
   setupTest(hooks);
 
   // TODO: Replace this with your real tests.
   test('it exists', function (assert) {
-    let service = this.owner.lookup('service:form-repository');
+    let service = this.owner.lookup('service:form-replacements');
     assert.ok(service);
   });
 });
