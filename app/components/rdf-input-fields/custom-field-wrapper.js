08f5dcebe46092170129d84a687f5c8962d9de28
karel kremer <karel.kremer@redpencil.io>
2024-12-19 14:31:44 +0100

    allow adding and removing custom fields, hide functionality behind feature flag

--- app/components/rdf-input-fields/custom-field-wrapper.js
+++ app/components/rdf-input-fields/custom-field-wrapper.js
@@ -1,15 +1,33 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import { JSON_API_TYPE } from 'frontend-lmb/utils/constants';
+import { consume } from 'ember-provide-consume-context';
 
 export default class RdfInputFieldsCustomFieldWrapperComponent extends Component {
+  @service formReplacements;
+  @consume('on-form-update') onUpdate;
+
+  @tracked removing = false;
   get title() {
     return this.args.field?.label;
   }
 
   @action
-  onRemove() {
-    console.log(
-      `remove clicked on ${this.args.field.uri} of form ${this.args.form}`
-    );
+  async onRemove() {
+    this.removing = true;
+    await fetch(`/form-content/fields`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': JSON_API_TYPE,
+      },
+      body: JSON.stringify({
+        fieldUri: this.args.field.uri.value,
+        formUri: this.args.form.uri,
+      }),
+    });
+    this.onUpdate();
+    this.removed = true;
   }
 }
