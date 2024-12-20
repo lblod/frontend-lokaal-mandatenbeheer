641d4ab0f531f51b5712819b9a7477f92ac29327
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-12-20 14:31:25 +0100

    lmb-892: directly use currentSession service in template instead of the getter

--- app/components/shared/main-menu.js
+++ app/components/shared/main-menu.js
@@ -4,18 +4,4 @@ import { service } from '@ember/service';
 
 export default class SharedMainMenuComponent extends Component {
   @service currentSession;
-  @service features;
-  @service impersonation;
-
-  get showLegislatuurModule() {
-    return this.currentSession.showLegislatuurModule;
-  }
-
-  get showCommonModules() {
-    if (!this.currentSession.isAdmin) {
-      return true;
-    }
-
-    return this.impersonation.isImpersonating;
-  }
 }
--- app/components/shared/modules.js
+++ app/components/shared/modules.js
@@ -4,8 +4,4 @@ import { service } from '@ember/service';
 
 export default class SharedModulesComponent extends Component {
   @service currentSession;
-
-  get showLegislatuurModule() {
-    return this.currentSession.showLegislatuurModule;
-  }
 }
