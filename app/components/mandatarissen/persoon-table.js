cd6a8ecc75e7d464f54484a2d134ebbac2594530
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-12-20 11:43:07 +0100

    lmb-414: when closing the dropdown the items are removed from the row

--- app/components/mandatarissen/persoon-table.js
+++ app/components/mandatarissen/persoon-table.js
@@ -27,31 +27,49 @@ export default class MandatarissenPersoonTable extends Component {
       'filter[bekleedt][bevat-in][is-tijdsspecialisatie-van][:has-no:original-bestuurseenheid]': true,
       include: ['bekleedt', 'bekleedt.bestuursfunctie'].join(','),
     });
-    const foldedMandatarissen = await foldMandatarisses(null, mandatarissen);
-    const subRows = await Promise.all(
-      foldedMandatarissen.map(async (foldedMandataris) => {
-        const mandataris = foldedMandataris.mandataris;
-        const mandaat = await mandataris.bekleedt;
-        const bestuursfunctie = await mandaat.bestuursfunctie;
-        return {
-          type: this.displayType.subRow,
-          data: {
-            orgaan: null,
-            mandaat: {
-              label: bestuursfunctie.label,
-              routeModelIds: [persoonId, mandataris.id],
+    if (this.isClickedObjectOpen(clickedObject)) {
+      const idsOfRowsToRemove = (
+        await foldMandatarisses(null, mandatarissen)
+      ).map((fold) => `${persoonId}-${fold.mandataris.id}`);
+      const rowsToRemove = this.tableRows.filter((row) =>
+        idsOfRowsToRemove.includes(row.data.id)
+      );
+      const clickedIndex = this.tableRows.indexOf(clickedObject);
+      delete clickedObject.iconSubRowOpen;
+      const clickedReplacement = {
+        iconSubRowOpen: 'nav-down',
+        ...clickedObject,
+      };
+      this.tableRows.replace(clickedIndex, 1, [clickedReplacement]);
+      this.tableRows.removeObjects(rowsToRemove);
+    } else {
+      const foldedMandatarissen = await foldMandatarisses(null, mandatarissen);
+      const subRows = await Promise.all(
+        foldedMandatarissen.map(async (foldedMandataris) => {
+          const mandataris = foldedMandataris.mandataris;
+          const mandaat = await mandataris.bekleedt;
+          const bestuursfunctie = await mandaat.bestuursfunctie;
+          return {
+            type: this.displayType.subRow,
+            data: {
+              id: `${persoonId}-${mandataris.id}`,
+              orgaan: null,
+              mandaat: {
+                label: bestuursfunctie.label,
+                routeModelIds: [persoonId, mandataris.id],
+              },
             },
-          },
-        };
-      })
-    );
-    const clickedIndex = this.tableRows.indexOf(clickedObject);
-    delete clickedObject.iconSubRowOpen;
-    const clickedReplacement = {
-      iconSubRowOpen: 'nav-up',
-      ...clickedObject,
-    };
-    this.tableRows.replace(clickedIndex, 1, [clickedReplacement, ...subRows]);
+          };
+        })
+      );
+      const clickedIndex = this.tableRows.indexOf(clickedObject);
+      delete clickedObject.iconSubRowOpen;
+      const clickedReplacement = {
+        iconSubRowOpen: 'nav-up',
+        ...clickedObject,
+      };
+      this.tableRows.replace(clickedIndex, 1, [clickedReplacement, ...subRows]);
+    }
   });
 
   setupTableRows = restartableTask(async () => {
@@ -65,4 +83,8 @@ export default class MandatarissenPersoonTable extends Component {
     });
     this.tableRows.pushObjects(withModelType);
   });
+
+  isClickedObjectOpen(clickedObject) {
+    return clickedObject.iconSubRowOpen === 'nav-up';
+  }
 }
