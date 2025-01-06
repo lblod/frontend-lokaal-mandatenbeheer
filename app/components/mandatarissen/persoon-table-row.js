4f3af1d083c6bce1c9f27ffc5831bdf87a99577b
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-01-06 09:48:49 +0100

    lmb-414: show the nav icon for opening and closing the subrows

--- app/components/mandatarissen/persoon-table-row.js
+++ app/components/mandatarissen/persoon-table-row.js
@@ -1,7 +1,21 @@
 import Component from '@glimmer/component';
 
+import { tracked } from '@glimmer/tracking';
+
+import { restartableTask } from 'ember-concurrency';
+
 export default class MandatarissenPersoonTableRowComponent extends Component {
+  @tracked isSubRowOpen;
+
+  getSubRowData = restartableTask(async () => {
+    this.isSubRowOpen = !this.isSubRowOpen;
+  });
+
   get persoonDetailRoute() {
     return 'mandatarissen.persoon.mandaten';
+  }
+
+  get iconSubRowOpen() {
+    return this.isSubRowOpen ? 'nav-down' : 'nav-up';
   }
 }
