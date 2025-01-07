94396fb345a687a30b13d33ca569437a155bb249
karel kremer <karel.kremer@redpencil.io>
2025-01-07 16:05:24 +0100

    review fixes

--- app/components/generate-custom-field-button.js
+++ app/components/generate-custom-field-button.js
@@ -3,9 +3,12 @@ import { action } from '@ember/object';
 import { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { JSON_API_TYPE } from 'frontend-lmb/utils/constants';
+import { showErrorToast } from 'frontend-lmb/utils/toasts';
 
 export default class GenerateCustomFieldButtonComponent extends Component {
   @service formReplacements;
+  @service toaster;
+
   @tracked showModal = false;
   @tracked loading = false;
   @tracked fieldName = '';
@@ -30,25 +33,32 @@ export default class GenerateCustomFieldButtonComponent extends Component {
   }
 
   @action async onSaveField() {
-    this.loading = true;
-    const result = await fetch(`/form-content/${this.args.form.id}/fields`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': JSON_API_TYPE,
-      },
-      body: JSON.stringify({
-        displayType:
-          'http://lblod.data.gift/display-types/lmb/custom-string-input',
-        order: 9000,
-        name: this.fieldName,
-      }),
-    });
+    try {
+      this.loading = true;
+      const result = await fetch(`/form-content/${this.args.form.id}/fields`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': JSON_API_TYPE,
+        },
+        body: JSON.stringify({
+          displayType:
+            'http://lblod.data.gift/display-types/lmb/custom-string-input',
+          order: 9000,
+          name: this.fieldName,
+        }),
+      });
 
-    const body = await result.json();
-    const newFormId = body.id;
-    this.formReplacements.setReplacement(this.args.form.id, newFormId);
-    if (this.args.onUpdate) {
-      this.args.onUpdate();
+      const body = await result.json();
+      const newFormId = body.id;
+      this.formReplacements.setReplacement(this.args.form.id, newFormId);
+      if (this.args.onUpdate) {
+        this.args.onUpdate();
+      }
+    } catch (error) {
+      showErrorToast(
+        this.toaster,
+        'Er ging iets mis bij het opslaan van het veld.'
+      );
     }
     this.loading = false;
     this.showModal = false;
