c4bd7e36bf7ac1fb329f7e9133e636d9f5e5cd0a
karel kremer <karel.kremer@redpencil.io>
2025-01-07 13:53:01 +0100

    allow using a library field when adding a new field to the form

--- app/components/generate-custom-field-button.js
+++ app/components/generate-custom-field-button.js
@@ -5,13 +5,31 @@ import { tracked } from '@glimmer/tracking';
 import { JSON_API_TYPE } from 'frontend-lmb/utils/constants';
 import { showErrorToast } from 'frontend-lmb/utils/toasts';
 
+import { TEXT_CUSTOM_DISPLAY_TYPE_ID } from 'frontend-lmb/utils/well-known-ids';
+
 export default class GenerateCustomFieldButtonComponent extends Component {
   @service formReplacements;
+  @service store;
   @service toaster;
 
   @tracked showModal = false;
   @tracked loading = false;
   @tracked fieldName = '';
+  customFieldEntry = this.store.createRecord('library-entry', {
+    name: 'Eigen veld',
+  });
+  @tracked selectedLibraryFieldType = this.customFieldEntry;
+  @tracked selectedDisplayType = null;
+
+  constructor() {
+    super(...arguments);
+    this.displayTypes.then((displayTypes) => {
+      this.selectedDisplayType = displayTypes.findBy(
+        'id',
+        TEXT_CUSTOM_DISPLAY_TYPE_ID
+      );
+    });
+  }
 
   get invalidName() {
     return !this.fieldName || this.fieldName.trim().length < 1;
@@ -19,6 +37,20 @@ export default class GenerateCustomFieldButtonComponent extends Component {
 
   get disabled() {
     return this.invalidName;
+  }
+
+  get libraryFieldOptions() {
+    return this.store
+      .findAll('library-entry', { include: 'display-type' })
+      .then((entries) => {
+        return entries.sortBy('id').reverse();
+      });
+  }
+
+  get displayTypes() {
+    return this.store.findAll('display-type').then((entries) => {
+      return entries.sortBy('label');
+    });
   }
 
   @action
@@ -32,17 +64,32 @@ export default class GenerateCustomFieldButtonComponent extends Component {
     this.showModal = false;
   }
 
+  @action
+  selectLibraryFieldType(libraryEntry) {
+    this.selectedLibraryFieldType = libraryEntry;
+    this.displayTypes.then((types) => {
+      this.selectedDisplayType =
+        types.findBy('uri', libraryEntry.get('displayType.uri')) ||
+        types.findBy('id', TEXT_CUSTOM_DISPLAY_TYPE_ID);
+    });
+  }
+
+  @action
+  selectDisplayType(displayType) {
+    this.selectedDisplayType = displayType;
+  }
+
   @action async onSaveField() {
+    this.loading = true;
     try {
-      this.loading = true;
       const result = await fetch(`/form-content/${this.args.form.id}/fields`, {
         method: 'POST',
         headers: {
           'Content-Type': JSON_API_TYPE,
         },
         body: JSON.stringify({
-          displayType:
-            'http://lblod.data.gift/display-types/lmb/custom-string-input',
+          displayType: this.selectedDisplayType.uri,
+          libraryEntryUri: this.selectedLibraryFieldType.uri,
           order: 9000,
           name: this.fieldName,
         }),
