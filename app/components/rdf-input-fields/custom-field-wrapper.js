dfc6c263b3c33ed9676938431b0c3794e1a9d485
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-01-09 10:25:59 +0100

    lmb-182: add modal with version one form for editing the form

--- app/components/rdf-input-fields/custom-field-wrapper.js
+++ app/components/rdf-input-fields/custom-field-wrapper.js
@@ -1,17 +1,34 @@
 import Component from '@glimmer/component';
+
 import { action } from '@ember/object';
 import { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
+
 import { JSON_API_TYPE } from 'frontend-lmb/utils/constants';
+
 import { consume } from 'ember-provide-consume-context';
+import { task } from 'ember-concurrency';
+import { showSuccessToast } from 'frontend-lmb/utils/toasts';
 
 export default class RdfInputFieldsCustomFieldWrapperComponent extends Component {
+  @service toaster;
   @service formReplacements;
   @consume('on-form-update') onUpdate;
 
   @tracked removing = false;
+  @tracked showEditFieldModal;
+
+  // Field properties
+  @tracked name;
+  @tracked type;
+  @tracked order;
+
   get title() {
     return this.args.field?.label;
+  }
+
+  get canSaveChanges() {
+    return this.name && this.type && this.order;
   }
 
   @action
@@ -30,4 +47,21 @@ export default class RdfInputFieldsCustomFieldWrapperComponent extends Component
     this.onUpdate();
     this.removed = true;
   }
+
+  @action
+  closeEditFieldModal() {
+    this.showEditFieldModal = false;
+  }
+
+  @action
+  updateField(event) {
+    const { name, value } = event.target;
+    this[name] = value;
+  }
+
+  saveChanges = task(async () => {
+    // TODO:
+    this.showEditFieldModal = false;
+    showSuccessToast(this.toaster, 'Het veld werd succesvol aangepast.');
+  });
 }
