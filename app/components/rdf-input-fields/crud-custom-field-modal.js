2045d0d068ffccdb6460df6096d82c748640cd26
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-01-10 12:32:37 +0100

    lmb-182: show the library entry dropdown

--- app/components/rdf-input-fields/crud-custom-field-modal.js
+++ app/components/rdf-input-fields/crud-custom-field-modal.js
@@ -2,16 +2,44 @@ import Component from '@glimmer/component';
 
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { service } from '@ember/service';
 
 import { task } from 'ember-concurrency';
+import { ForkingStore } from '@lblod/ember-submission-form-fields';
+import { consume } from 'ember-provide-consume-context';
 
-import { JSON_API_TYPE } from 'frontend-lmb/utils/constants';
+import { JSON_API_TYPE, SOURCE_GRAPH } from 'frontend-lmb/utils/constants';
+import { PROV } from 'frontend-lmb/rdf/namespaces';
+import { TEXT_CUSTOM_DISPLAY_TYPE_ID } from 'frontend-lmb/utils/well-known-ids';
 
 export default class RdfInputFieldCrudCustomFieldModalComponent extends Component {
+  @consume('form-definition') formDefinition;
+
+  @service store;
+
   @tracked isRemovingField;
+
+  customFieldEntry = this.store.createRecord('library-entry', {
+    name: 'Eigen veld',
+  });
 
   // Field properties
   @tracked fieldName;
+  @tracked libraryFieldType = this.customFieldEntry;
+  @tracked displayType;
+
+  constructor() {
+    super(...arguments);
+
+    if (this.args.isCreating) {
+      this.displayTypes.then((displayTypes) => {
+        this.displayType = displayTypes.findBy(
+          'id',
+          TEXT_CUSTOM_DISPLAY_TYPE_ID
+        );
+      });
+    }
+  }
 
   @action
   updateField(event) {
@@ -20,6 +48,16 @@ export default class RdfInputFieldCrudCustomFieldModalComponent extends Componen
   }
 
   saveChanges = task(async () => {});
+
+  @action
+  selectLibraryFieldType(libraryEntry) {
+    this.libraryFieldType = libraryEntry;
+    this.displayTypes.then((types) => {
+      this.displayType =
+        types.findBy('uri', libraryEntry.get('displayType.uri')) ||
+        types.findBy('id', TEXT_CUSTOM_DISPLAY_TYPE_ID);
+    });
+  }
 
   async removeField() {
     await fetch(`/form-content/fields`, {
@@ -47,6 +85,41 @@ export default class RdfInputFieldCrudCustomFieldModalComponent extends Componen
     if (this.args.onCloseModal) {
       this.args.onCloseModal();
     }
+  }
+
+  get displayTypes() {
+    return this.store.findAll('display-type').then((entries) => {
+      return entries.sortBy('label');
+    });
+  }
+
+  get libraryFieldOptions() {
+    const forkingStore = new ForkingStore();
+    forkingStore.parse(
+      this.formDefinition.formTtl,
+      SOURCE_GRAPH,
+      'text/turtle'
+    );
+
+    const alreadyUsedLibraryEntries = forkingStore
+      .match(null, PROV('wasDerivedFrom'), null, SOURCE_GRAPH)
+      .map((triple) => triple.object.value);
+
+    return this.store
+      .findAll('library-entry', { include: 'display-type' })
+      .then((entries) => {
+        return [
+          this.customFieldEntry,
+          ...entries
+            .sortBy('id')
+            .reverse()
+            .filter((entry) => {
+              return (
+                entry.uri && alreadyUsedLibraryEntries.indexOf(entry.uri) < 0
+              );
+            }),
+        ];
+      });
   }
 
   get canSaveChanges() {
