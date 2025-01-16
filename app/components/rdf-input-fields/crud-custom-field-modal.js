6dd277300df4ceb1d53de961fce13573f032b585
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-01-16 09:48:09 +0100

    lmb-181: also show the required button when creating a field

--- app/components/rdf-input-fields/crud-custom-field-modal.js
+++ app/components/rdf-input-fields/crud-custom-field-modal.js
@@ -102,6 +102,7 @@ export default class RdfInputFieldCrudCustomFieldModalComponent extends Componen
             displayType: this.displayType.uri,
             libraryEntryUri: this.libraryFieldType.uri,
             name: this.fieldName,
+            isRequired: !!this.isFieldRequired,
           }),
         }
       );
