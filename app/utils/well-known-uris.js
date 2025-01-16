c1fc34fc620c4aeaebf5acb2b67a4bbb594c592f
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-01-16 10:45:35 +0100

    lmb-181: do not show the required toggle for adres types

--- app/utils/well-known-uris.js
+++ app/utils/well-known-uris.js
@@ -101,6 +101,8 @@ export const KANDIDATENLIJST_KAMER_VOLKSVERTEGENWOORDIGING =
 
 export const TEXT_CUSTOM_DISPLAY_TYPE =
   'http://lblod.data.gift/display-types/lmb/custom-string-input';
+export const ADRES_CUSTOM_DISPLAY_TYPE =
+  'http://lblod.data.gift/display-types/lmb/custom-address-input';
 
 export const LIBRARY_ENTREES = [
   'http://data.lblod.info/id/lmb/form-library-entries/095d1329-a9fa-4d72-8c49-eb52aaf893de', // Verblijfsplaats
