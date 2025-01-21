6feee0c9c5bcda2b91fe41ea2d35adb4e38a3881
brechtvdv <br5cht@hotmail.com>
2025-01-21 14:14:24 +0100

    Show validation results for latest report

--- app/controllers/report.js
+++ app/controllers/report.js
@@ -4,10 +4,5 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 export default class ReportController extends Controller {
-  @tracked selectedReport;
-
-  @action onSelectReport(report) {
-    console.log(report);
-    this.selectedReport = report;
-  }
+  
 }
