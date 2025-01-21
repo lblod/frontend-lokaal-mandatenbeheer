a2917a978e4a28b80426dd9d8551a89380018e61
brechtvdv <br5cht@hotmail.com>
2025-01-21 16:17:40 +0100

    Add dynamic LinkTo to focus node route

--- app/controllers/report.js
+++ app/controllers/report.js
@@ -1,8 +1,11 @@
 import Controller from '@ember/controller';
 
 import { action } from '@ember/object';
+import { service } from '@ember/service';
 
 export default class ReportController extends Controller {
+  @service store;
+
   get targetClasses() {
     return Array.from(this.model.resultsByTargetClass.keys());
   }
@@ -15,5 +18,32 @@ export default class ReportController extends Controller {
   @action
   lengthOfResultsByTargetClass(targetClass) {
     return this.filterResultsByTargetClass(targetClass).length;
+  }
+
+  @action
+  async getFocusNodeContext(focusNode, targetClass) {
+    const mapTargetClassToRoute = {
+      'http://data.vlaanderen.be/ns/mandaat#Mandataris': {
+        route: 'mandatarissen.mandataris',
+        model: 'mandataris',
+      },
+    };
+    return {
+      route: mapTargetClassToRoute[targetClass].route,
+      modelId: await this.searchModelIdForFocusNode(
+        mapTargetClassToRoute[targetClass].model,
+        focusNode
+      ),
+    };
+  }
+
+  async searchModelIdForFocusNode(modelName, focusNode) {
+    return (
+      await this.store.query(modelName, {
+        filter: {
+          ':uri:': focusNode,
+        },
+      })
+    )?.at(0)?.id;
   }
 }
