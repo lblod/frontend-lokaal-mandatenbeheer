1dd6efcdf66881828d19709614ae2718b02628d4
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-01-27 15:00:28 +0100

    v4.12.2...v5.12.0

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -40,9 +40,12 @@ module.exports = function (defaults) {
   return require('@embroider/compat').compatBuild(app, Webpack, {
     staticAddonTestSupportTrees: true,
     staticAddonTrees: true,
-    staticHelpers: true,
-    staticModifiers: true,
-    staticComponents: true,
-    // splitAtRoutes: ['route.name'], // can also be a RegExp
+    staticInvokables: true,
+    staticEmberSource: true,
+    skipBabel: [
+      {
+        package: 'qunit',
+      },
+    ],
   });
 };
