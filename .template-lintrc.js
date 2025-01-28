ae8baff3f80e8ab77c25a496b115391bdcdcc335
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-01-28 11:33:32 +0100

    lmb-997: fix the override did not work for did-insert & did-update?

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -3,18 +3,14 @@
 module.exports = {
   plugins: ['ember-template-lint-plugin-prettier'],
   extends: ['recommended', 'ember-template-lint-plugin-prettier:recommended'],
+  rules: {
+    'no-at-ember-render-modifiers': false, // Makes it possible to use {{did-insert}} && {{did-update}} modifiers in template
+  },
   overrides: [
     {
       files: ['tests/**/*-test.*'],
       rules: {
         'require-input-label': false,
-      },
-    },
-    {
-      // TODO: lmb-1331
-      files: ['app/templates/**/*.hbs'],
-      rules: {
-        'no-at-ember-render-modifiers': false, // Makes it possible to use {{did-insert}} && {{did-update}} modifiers in template
       },
     },
   ],
