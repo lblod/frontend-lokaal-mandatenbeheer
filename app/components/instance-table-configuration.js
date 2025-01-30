ad7a98309b154f4fb4db884321b0ef725fbace2a
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-01-30 16:56:40 +0100

    lmb-1228: can select and unselect the column labels in the configuration

--- app/components/instance-table-configuration.js
+++ app/components/instance-table-configuration.js
@@ -1,3 +1,36 @@
 import Component from '@glimmer/component';
 
-export default class InstanceTableConfiguration extends Component {}
+import { A } from '@ember/array';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+export default class InstanceTableConfiguration extends Component {
+  @tracked labels = A([]);
+
+  constructor() {
+    super(...arguments);
+
+    const labelsWithIsSelectedProperty = this.args.labels.map((label) => {
+      return {
+        ...label,
+        isSelected: false,
+      };
+    });
+
+    this.labels.clear();
+    this.labels.push(...labelsWithIsSelectedProperty);
+  }
+
+  @action
+  toggleLabel(label) {
+    const selectedState = label.isSelected;
+    this.labels.removeObject(label);
+
+    delete label.isSelected;
+    this.labels.push({ ...label, isSelected: !selectedState });
+  }
+
+  get sortedLabels() {
+    return this.labels.sort((a, b) => a.name.localeCompare(b.name));
+  }
+}
