36bf3193a36c9d9134838f61caad7eb3b0745797
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-02-07 09:35:58 +0100

    lmb-1232: make it possible to delete a custom codelist

--- app/controllers/codelijsten/id.js
+++ app/controllers/codelijsten/id.js
@@ -1,7 +1,42 @@
 import Controller from '@ember/controller';
 
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { service } from '@ember/service';
+
+import { showSuccessToast } from 'frontend-lmb/utils/toasts';
+
 export default class CodelijstenIdController extends Controller {
+  @service router;
+  @service toaster;
+
+  @tracked isDeleting;
+
   get isReadOnly() {
     return this.model.codelijst?.readOnly;
+  }
+
+  @action
+  async deleteCodelist() {
+    this.isDeleting = true;
+    await this.deleteConcepts();
+    this.model.codelijst.deleteRecord();
+    this.model.codelijst.save();
+    showSuccessToast(
+      this.toaster,
+      'Codelijst succesvol verwijderd',
+      'Codelijst'
+    );
+    this.router.transitionTo('codelijsten.overzicht');
+    this.isDeleting = false;
+  }
+
+  async deleteConcepts() {
+    await Promise.all(
+      this.model.concepten.map(async (concept) => {
+        await concept.deleteRecord();
+        return concept.save();
+      })
+    );
   }
 }
