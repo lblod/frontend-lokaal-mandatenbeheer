2f529cf7d406c6518c9110a97b205e4a98579c8b
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-02-07 15:21:42 +0100

    lmb-1232: move the .id route to detail/view

--- app/routes/codelijsten/detail.js
+++ app/routes/codelijsten/detail.js
@@ -4,16 +4,14 @@ import { service } from '@ember/service';
 
 import { queryRecord } from 'frontend-lmb/utils/query-record';
 
-export default class CodelijstenIdRoute extends Route {
+export default class CodelijstenDetailRoute extends Route {
   @service session;
   @service store;
-
-  queryParams = {
-    sort: { refreshModel: true },
-  };
+  @service router;
 
   beforeModel(transition) {
     this.session.requireAuthentication(transition, 'login');
+    this.router.replaceWith('codelijsten.detail.view', transition.to.params.id);
   }
 
   async model(params) {
