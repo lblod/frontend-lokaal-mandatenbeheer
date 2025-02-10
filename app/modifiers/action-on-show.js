cc387fd2171e7700251078991a2ff347d65779ca
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-02-10 16:21:56 +0100

    lmb-1232: fix undefined method

--- app/modifiers/action-on-show.js
+++ app/modifiers/action-on-show.js
@@ -2,6 +2,8 @@ import { modifier } from 'ember-modifier';
 
 export default modifier((element, methods) => {
   const method = methods.length ? methods[0] : null;
-
-  return method() ?? null;
+  if (typeof method == 'function') {
+    return method();
+  }
+  return;
 });
