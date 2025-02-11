f27aaa0da791c6aa41a3e0c93860f4d309c1406a
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-02-11 10:11:51 +0100

    lmb-1232: cleanup codelijst NEW route

--- app/routes/codelijsten/new.js
+++ app/routes/codelijsten/new.js
@@ -1,3 +1,21 @@
 import Route from '@ember/routing/route';
 
-export default class CodelijstenNewRoute extends Route {}
+import { service } from '@ember/service';
+
+export default class CodelijstenNewRoute extends Route {
+  @service store;
+
+  setupController(controller) {
+    super.setupController(...arguments);
+    this.store
+      .peekAll('concept-scheme')
+      .filter((cs) => !cs.id)
+      .map((cs) => cs.rollbackAttributes());
+    this.store
+      .peekAll('concept')
+      .filter((c) => !c.id)
+      .map((c) => c.rollbackAttributes());
+
+    controller.concepten.clear();
+  }
+}
