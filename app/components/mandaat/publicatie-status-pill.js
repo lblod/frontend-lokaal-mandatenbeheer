a6ab202503f8725a4a33be9e68a97603ede044e6
karel kremer <karel.kremer@redpencil.io>
2025-02-11 10:01:22 +0100

    fix manueel bekrachtigd links

--- app/components/mandaat/publicatie-status-pill.js
+++ app/components/mandaat/publicatie-status-pill.js
@@ -1,8 +1,18 @@
 import Component from '@glimmer/component';
 
 import { effectiefIsLastPublicationStatus } from 'frontend-lmb/utils/effectief-is-last-publication-status';
+import { isValidUri } from 'frontend-lmb/utils/is-valid-uri';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { service } from '@ember/service';
+import { showErrorToast, showSuccessToast } from 'frontend-lmb/utils/toasts';
 
 export default class MandaatPublicatieStatusPillComponent extends Component {
+  @service toaster;
+
+  @tracked newLink;
+  @tracked showEditLinkModal;
+
   get isMandatarisBekrachtigd() {
     return this.status ? this.status.get('isBekrachtigd') : true;
   }
@@ -35,5 +45,49 @@ export default class MandaatPublicatieStatusPillComponent extends Component {
 
   get skin() {
     return this.getSkinForPill(this.status);
+  }
+
+  get invalidLink() {
+    return (
+      !this.args.mandataris.linkToBesluit ||
+      !isValidUri(this.args.mandataris.linkToBesluit)
+    );
+  }
+
+  @action
+  updateLink() {
+    this.args.mandataris.linkToBesluit = this.newLink;
+    this.args.mandataris
+      .save()
+      .then(() => {
+        showSuccessToast(
+          this.toaster,
+          'De link naar het besluit werd aangepast.'
+        );
+      })
+      .catch((e) => {
+        console.error(e);
+        showErrorToast(
+          this.toaster,
+          'Er ging iets mis bij het aanpassen van de link naar het besluit.'
+        );
+      });
+    this.showEditLinkModal = false;
+  }
+
+  @action
+  editLink() {
+    this.newLink = this.args.mandataris.linkToBesluit;
+    this.showEditLinkModal = true;
+  }
+
+  @action
+  onEditLink(e) {
+    this.newLink = e.target.value;
+  }
+
+  @action
+  closeModal() {
+    this.showEditLinkModal = false;
   }
 }
