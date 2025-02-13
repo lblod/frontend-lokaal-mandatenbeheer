ed4c70b5d051c537d589887a9f17d839d875bb2f
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-02-13 13:44:47 +0100

    lmb-1313: mandataris can successfully be destroyed

--- app/components/mandatarissen/delete-modal.js
+++ app/components/mandatarissen/delete-modal.js
@@ -4,7 +4,10 @@ import { action } from '@ember/object';
 import { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 
+import { timeout } from 'ember-concurrency';
+
 import { showSuccessToast } from 'frontend-lmb/utils/toasts';
+import { RESOURCE_CACHE_TIMEOUT } from 'frontend-lmb/utils/constants';
 
 export default class MandatarissenDeleteModal extends Component {
   @service router;
@@ -15,16 +18,16 @@ export default class MandatarissenDeleteModal extends Component {
   @action
   async delete() {
     this.isDeleting = true;
-    await this.args.mandataris.deleteRecord();
+    this.args.mandataris.deleteRecord();
+    await this.args.mandataris.save();
+    await timeout(RESOURCE_CACHE_TIMEOUT);
     this.isDeleting = false;
+    this.args.onClose();
     showSuccessToast(
       this.toaster,
       'Mandataris succesvol verwijderd',
       'Mandataris'
     );
-    if (this.args.afterDeleteRoute) {
-      await this.args.mandataris.save();
-      this.router.transitionTo(this.args.afterDeleteRoute);
-    }
+    this.router.transitionTo(this.args.afterDeleteRoute);
   }
 }
