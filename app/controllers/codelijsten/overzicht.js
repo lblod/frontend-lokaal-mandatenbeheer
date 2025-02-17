f36ffb4f5ff222aa812038c7b601c53e392caf5c
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-02-17 15:56:39 +0100

    lmb-1232: make the page size also tracked

--- app/controllers/codelijsten/overzicht.js
+++ app/controllers/codelijsten/overzicht.js
@@ -5,7 +5,8 @@ import { tracked } from '@glimmer/tracking';
 
 export default class CodelijstenOverzichtController extends Controller {
   @tracked filter;
-  @tracked page;
+  @tracked page = 0;
+  @tracked size = 20;
 
   @action
   search(event) {
