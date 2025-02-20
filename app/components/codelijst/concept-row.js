f99a65650666fcc2c9fd2dbd52c39a30c91ddd2f
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-02-20 13:20:07 +0100

    lmb-1232: label must be 2 characters long at minimum

--- app/components/codelijst/concept-row.js
+++ app/components/codelijst/concept-row.js
@@ -7,32 +7,43 @@ import { tracked } from '@glimmer/tracking';
 export default class CodelijstConceptRow extends Component {
   @service store;
 
-  @tracked concept;
+  @tracked label;
 
   @tracked isEditing;
+  @tracked errorMessage =
+    'Label voor een concept moet minstens 2 karakters lang zijn.';
 
   constructor() {
     super(...arguments);
     if (this.args.concept) {
-      this.concept = this.args.concept;
+      this.label = this.args.concept.label;
     }
   }
 
   @action
   updateConceptLabel(event) {
-    this.concept.label = event.target?.value;
+    this.label = event.target?.value;
   }
 
   @action
   async saveChanges() {
+    if (!this.canSaveLabel) {
+      return;
+    }
+
+    this.errorMessage = null;
     this.isEditing = false;
+    this.concept.label = this.label?.trim();
     await this.concept.save();
   }
 
   @action
   async discardChanges() {
     this.isEditing = false;
-    this.args.concept.rollbackAttributes();
-    this.concept = this.args.concept;
+    this.label = this.args.concept.label ?? 'Optie';
+  }
+
+  get canSaveLabel() {
+    return this.label && this.label.trim().length >= 2;
   }
 }
