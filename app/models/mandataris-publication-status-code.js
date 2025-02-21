e49d0700822d6de91f44344c103203910aa3d493
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-02-21 15:30:04 +0100

    lmb-1407: add a displayLabel getter to the publication status model

--- app/models/mandataris-publication-status-code.js
+++ app/models/mandataris-publication-status-code.js
@@ -1,6 +1,10 @@
 import Model, { attr } from '@ember-data/model';
 
-import { MANDATARIS_BEKRACHTIGD_PUBLICATION_STATE } from 'frontend-lmb/utils/well-known-uris';
+import {
+  MANDATARIS_BEKRACHTIGD_PUBLICATION_STATE,
+  MANDATARIS_DRAFT_PUBLICATION_STATE,
+  MANDATARIS_EFFECTIEF_PUBLICATION_STATE,
+} from 'frontend-lmb/utils/well-known-uris';
 
 export default class MandatarisPublicationStatusCodeModel extends Model {
   @attr uri;
@@ -9,5 +13,15 @@ export default class MandatarisPublicationStatusCodeModel extends Model {
 
   get isBekrachtigd() {
     return this.uri === MANDATARIS_BEKRACHTIGD_PUBLICATION_STATE;
+  }
+
+  get displayLabel() {
+    const statusMapping = {
+      [MANDATARIS_DRAFT_PUBLICATION_STATE]: 'Draft',
+      [MANDATARIS_EFFECTIEF_PUBLICATION_STATE]: 'Niet bekrachtigd',
+      [MANDATARIS_BEKRACHTIGD_PUBLICATION_STATE]: 'Bekrachtigd',
+    };
+
+    return statusMapping[this.uri] ?? 'Niet beschikbaar';
   }
 }
