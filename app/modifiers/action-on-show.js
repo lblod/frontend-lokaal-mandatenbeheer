05be88d62c251cb9a3b1927a1d8b531f1203b32c
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-02-21 08:47:12 +0100

    lmb-1232: remove the on action show modifier

--- app/modifiers/action-on-show.js
+++
@@ -1,9 +0,0 @@
-import { modifier } from 'ember-modifier';
-
-export default modifier((element, methods) => {
-  const method = methods.length ? methods[0] : null;
-  if (typeof method == 'function') {
-    return method();
-  }
-  return;
-});
