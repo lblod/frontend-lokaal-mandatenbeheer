3d5b7b0eb8685f98dbed9f25e970da5dcadf56de
brechtvdv <br5cht@hotmail.com>
2025-02-24 15:39:36 +0100

    Add unit test for ValidatieTable component

--- tests/integration/components/validatie-table-test.js
+++ tests/integration/components/validatie-table-test.js
@@ -2,23 +2,81 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'frontend-lmb/tests/helpers';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import Service from '@ember/service';
+
+// Stub validatie service
+class ValidatieStub extends Service {
+  latestValidationReport = {};
+}
 
 module('Integration | Component | validatie-table', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  let store, mandataris;
 
-    await render(hbs`<ValidatieTable />`);
+  hooks.beforeEach(function () {
+    // Registering the stubbed validatie service
+    this.owner.register('service:validatie', ValidatieStub);
+    this.set('validatie', this.owner.lookup('service:validatie'));
 
-    assert.dom().hasText('');
+    // Setting up store and creating a mandataris record as instance to test
+    store = this.owner.lookup('service:store');
+    mandataris = store.createRecord('mandataris', {
+      rangorde: 1,
+      start: new Date(2022, 0, 1), // January 1, 2022
+      uri: 'http://example.com/mandataris/1',
+      linkToBesluit: 'http://example.com/besluit/1',
+      modified: new Date(),
+      effectiefAt: new Date(),
+    });
+    this.set('instance', mandataris);
 
-    // Template block usage:
-    await render(hbs`<ValidatieTable>
-  template block text
-</ValidatieTable>`);
+    // Enable shacl-report feature service
+    this.set('features', this.owner.lookup('service:features'));
+    this.set('features.isEnabled', function (feature) {
+      if (feature === 'shacl-report') {
+        return true;
+      }
+      return false;
+    });
+  });
 
-    assert.dom().hasText('template block text');
+  test('mandataris instance exists', function (assert) {
+    assert.ok(this.instance, 'Mandataris instance is created');
+  });
+
+  test('it does not render AuDataTable when instance does not have results', async function (assert) {
+    this.set('validatie.getResultsByInstance', async function () {
+      return [];
+    });
+
+    await render(hbs`<ValidatieTable @instance={{this.instance}} />`);
+
+    assert
+      .dom('[data-test-validatie-table]')
+      .doesNotExist(
+        'ValidatieTable component is not rendered when results are empty'
+      );
+  });
+
+  test('it does render AuDataTable when instance has validation results', async function (assert) {
+    // Stubbing the validatie service to return validation results
+    this.set('validatie.getResultsByInstance', async function () {
+      return [
+        {
+          focusNode: 'http://example.com/mandataris/1',
+          resultMessage: 'Validation error occurred',
+          targetClassOfFocusNode:
+            'http://data.vlaanderen.be/ns/mandaat#Mandataris',
+          focusNodeId: '123',
+        },
+      ];
+    });
+
+    await render(hbs`<ValidatieTable @instance={{this.instance}} />`);
+
+    assert
+      .dom('[data-test-validatie-table]')
+      .exists('ValidatieTable component is rendered when results are present');
   });
 });
