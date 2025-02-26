fc05f7720ecf40603f25349bfad079e3bfb74316
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-02-26 16:16:29 +0100

    lmb-1232: show detailed text of the to be deleted codelist

--- app/services/concept-scheme-api.js
+++ app/services/concept-scheme-api.js
@@ -2,6 +2,50 @@ import Service from '@ember/service';
 import { API, JSON_API_TYPE, STATUS_CODE } from 'frontend-lmb/utils/constants';
 
 export default class ConceptSchemeApiService extends Service {
+  async conceptSchemeHasImplementations(conceptSchemeId) {
+    if (!conceptSchemeId) {
+      return;
+    }
+    const response = await fetch(
+      `${API.CONCEPT_SCHEME_SERVICE}/concept-scheme/${conceptSchemeId}/has-implementations`
+    );
+
+    const jsonResponse = await response.json();
+
+    if (response.status !== STATUS_CODE.OK) {
+      console.error(jsonResponse.message);
+      throw {
+        status: response.status,
+        message: jsonResponse.message,
+      };
+    }
+
+    return jsonResponse;
+  }
+
+  async deleteConceptSchemeAndTheirConcepts(conceptScheme) {
+    if (!conceptScheme || !conceptScheme.id) {
+      return;
+    }
+    const response = await fetch(
+      `${API.CONCEPT_SCHEME_SERVICE}/concept-scheme/${conceptScheme.id}`,
+      {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': JSON_API_TYPE,
+        },
+      }
+    );
+    if (response.status !== STATUS_CODE.NO_CONTENT) {
+      const jsonResponse = await response.json();
+      console.error(jsonResponse.message);
+      throw {
+        status: response.status,
+        message: jsonResponse.message,
+      };
+    }
+  }
+
   async conceptHasImplementations(conceptId) {
     if (!conceptId) {
       return;
@@ -23,7 +67,7 @@ export default class ConceptSchemeApiService extends Service {
     return jsonResponse;
   }
 
-  async deleteConceptsTheirImplementation(concepts) {
+  async deleteConceptsAndTheirImplementations(concepts) {
     const response = await fetch(`${API.CONCEPT_SCHEME_SERVICE}/concept/ids`, {
       method: 'DELETE',
       headers: {
@@ -34,16 +78,13 @@ export default class ConceptSchemeApiService extends Service {
       }),
     });
 
-    const jsonResponse = await response.json();
-
-    if (response.status !== STATUS_CODE.OK) {
+    if (response.status !== STATUS_CODE.NO_CONTENT) {
+      const jsonResponse = await response.json();
       console.error(jsonResponse.message);
       throw {
         status: response.status,
         message: jsonResponse.message,
       };
     }
-
-    return jsonResponse;
   }
 }
