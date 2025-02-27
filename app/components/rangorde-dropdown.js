3aa3e10aed982d0b8eabec8a0fec465842c83f13
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-02-27 14:00:16 +0100

    lmb-1353: make it possible to clear the rangorde

--- app/components/rangorde-dropdown.js
+++ app/components/rangorde-dropdown.js
@@ -1,11 +1,26 @@
 import Component from '@glimmer/component';
 
+import { action } from '@ember/object';
+
 import { rangordeStringMapping } from 'frontend-lmb/utils/rangorde';
 
 export default class RangordeDropdown extends Component {
+  noRangordeLabel = 'Geen rangorde';
+
   get labels() {
-    return Object.keys(rangordeStringMapping).map(
+    const options = Object.keys(rangordeStringMapping).map(
       (key) => `${key} ${this.args.mandaatLabel}`
     );
+
+    return [this.noRangordeLabel, ...options];
+  }
+
+  @action
+  onUpdateRangorde(rangordeLabel) {
+    let rangorde = rangordeLabel;
+    if (rangorde === this.noRangordeLabel) {
+      rangorde = null;
+    }
+    this.args.onNewRangorde(rangorde);
   }
 }
