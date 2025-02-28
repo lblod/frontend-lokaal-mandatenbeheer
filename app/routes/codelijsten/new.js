d7516ecee9e54e4d1041b824b5dac101caeab85b
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-02-28 10:15:21 +0100

    lmb-1232: use codelijst util for checking concept labels + move concepten to the model for route new

--- app/routes/codelijsten/new.js
+++ app/routes/codelijsten/new.js
@@ -1,5 +1,6 @@
 import Route from '@ember/routing/route';
 
+import { A } from '@ember/array';
 import { service } from '@ember/service';
 
 export default class CodelijstenNewRoute extends Route {
@@ -14,6 +15,7 @@ export default class CodelijstenNewRoute extends Route {
 
     return {
       codelijst,
+      concepten: A([]),
     };
   }
 }
