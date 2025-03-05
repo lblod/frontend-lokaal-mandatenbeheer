f101a1bac4f2236b51265adfb7c14401c2135f2e
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-03-05 16:11:53 +0100

    lmb-1353: use rangordeLabel for rangorde instead of bestuursfunctiecode

--- app/components/mandatarissen/update-state.js
+++ app/components/mandatarissen/update-state.js
@@ -161,7 +161,7 @@ export default class MandatarissenUpdateState extends Component {
   }
 
   get mandaatLabel() {
-    return this.mandaat.get('bestuursfunctie.label') || 'schepen';
+    return this.mandaat?.rangordeLabel;
   }
 
   get rangordePlaceholder() {
