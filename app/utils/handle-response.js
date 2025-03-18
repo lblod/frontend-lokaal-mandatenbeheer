65c0c9e3228fdcc0ced4a05020d23bb9800523ba
Ben Fidlers <ben.fidlers@qmino.com>
2025-03-18 13:24:10 +0100

    add handleApiCall method with toaster messages

--- app/utils/handle-response.js
+++ app/utils/handle-response.js
@@ -1,10 +1,25 @@
 import ApiError from './api-error';
+import { showErrorToast, showSuccessToast } from './toasts';
 
 export const handleResponse = async (response) => {
   const jsonResponse = await response.json();
   if (!response.ok) {
     console.error(jsonResponse.message);
     throw new ApiError(jsonResponse.message, response.status);
+  }
+  return jsonResponse;
+};
+
+export const handleResponseWithToast = async (
+  response,
+  toaster,
+  successMessage
+) => {
+  const jsonResponse = await response.json();
+  if (!response.ok) {
+    showErrorToast(toaster, jsonResponse.message);
+  } else {
+    showSuccessToast(toaster, successMessage);
   }
   return jsonResponse;
 };
