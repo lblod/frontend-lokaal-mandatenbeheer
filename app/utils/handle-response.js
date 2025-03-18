15110a64630b1e4a572151ca85df273b3d576167
Ben Fidlers <ben.fidlers@qmino.com>
2025-03-18 13:16:02 +0100

    improve api status check error handling

--- app/utils/handle-response.js
+++ app/utils/handle-response.js
@@ -1,9 +1,8 @@
 import ApiError from './api-error';
-import { STATUS_CODE } from './constants';
 
 export const handleResponse = async (response) => {
   const jsonResponse = await response.json();
-  if (response.status !== STATUS_CODE.OK) {
+  if (!response.ok) {
     console.error(jsonResponse.message);
     throw new ApiError(jsonResponse.message, response.status);
   }
