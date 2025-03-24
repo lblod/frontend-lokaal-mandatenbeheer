46595e3e6df6bdfb401d3cd3607a8f1f9e3376f8
karel kremer <karel.kremer@redpencil.io>
2025-03-24 17:02:39 +0100

    keep history items

--- app/components/mandatarissen/timeline.js
+++ app/components/mandatarissen/timeline.js
@@ -1,11 +1,80 @@
 import Component from '@glimmer/component';
 import { service } from '@ember/service';
-import { action } from '@ember/object';
 import moment from 'moment';
+
+import { trackedFunction } from 'reactiveweb/function';
+import { use } from 'ember-resources';
+
+async function getCorrections(mandataris) {
+  const result = await fetch(
+    `/form-content/mandataris-edit/instances/${mandataris.id}/history`
+  );
+
+  const json = await result.json();
+  return json.instances;
+}
+function getHistory() {
+  return trackedFunction(async () => {
+    const mandatarissen = this.args.mandatarissen;
+    const newHistory = await Promise.all(
+      mandatarissen.map(async (mandataris) => {
+        let corrections = await getCorrections(mandataris);
+        const historyEntry = {
+          mandataris,
+          corrections,
+        };
+        return historyEntry;
+      })
+    );
+
+    const userIdsInHistory = new Set();
+    newHistory.forEach((h) => {
+      h.corrections.forEach((c) => {
+        userIdsInHistory.add(c.creator);
+      });
+    });
+
+    let users = [];
+    if (userIdsInHistory.size !== 0) {
+      users = await this.store.query('gebruiker', {
+        filter: {
+          id: Array.from(userIdsInHistory).join(','),
+        },
+      });
+    }
+
+    const userIdToUser = {};
+    users.forEach((u) => {
+      userIdToUser[u.id] = u;
+    });
+
+    return newHistory
+      .map((h) => {
+        return {
+          ...h,
+          corrections: h.corrections.map((c) => ({
+            ...c,
+            creator: userIdToUser[c.creator],
+          })),
+        };
+      })
+      .sort((a, b) => {
+        if (!b?.mandataris?.start) {
+          return -1;
+        }
+        if (!a?.mandataris?.start) {
+          return 1;
+        }
+        return b.mandataris.start.getTime() - a.mandataris.start.getTime();
+      });
+  });
+}
 
 export default class MandatarissenTimeline extends Component {
   @service features;
-  @service router;
+  @service store;
+
+  @use(getHistory) history;
 
   get isFeatureEnabled() {
     return this.features.isEnabled('shacl-report');
@@ -33,15 +102,14 @@ export default class MandatarissenTimeline extends Component {
         selected: false,
       });
     }
+    if (this.history?.value) {
+      events.forEach((event) => {
+        event.corrections = this.history.value.find(
+          (h) => h.mandataris.id === event.mandataris.id
+        )?.corrections;
+      });
+    }
     events[events.length - 1].last = true;
     return events;
-  }
-
-  @action
-  selectEvent(event) {
-    this.router.transitionTo(
-      'mandatarissen.persoon.mandataris',
-      event.mandataris.id
-    );
   }
 }
