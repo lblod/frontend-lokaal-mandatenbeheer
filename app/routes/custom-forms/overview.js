fd3294356f2f9ef2e5b0d18f228e5497088c141d
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-03-25 11:31:27 +0100

    lmb-1214: show an overview of custom forms

--- app/routes/custom-forms/overview.js
+++ app/routes/custom-forms/overview.js
@@ -5,9 +5,21 @@ import { service } from '@ember/service';
 export default class CustomFormsOverviewRoute extends Route {
   @service store;
 
-  async model() {
-    const formDefinitions = await this.store.findAll('form');
-    console.log({ formDefinitions });
-    return formDefinitions;
+  queryParams = {
+    page: { refreshModel: true },
+    size: { refreshModel: true },
+    sort: { refreshModel: true },
+  };
+
+  async model(params) {
+    const formDefinitions = await this.store.query('form', {
+      sort: params.sort || '-target-label',
+      page: {
+        number: params.page,
+        size: params.size,
+      },
+    });
+
+    return { formDefinitions };
   }
 }
