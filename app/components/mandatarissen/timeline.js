975580078a60f194888b3f8232df0523e96ab1cc
karel kremer <karel.kremer@redpencil.io>
2025-03-25 12:57:56 +0100

    remove history from timeline as it is now correctly fetched in the route

--- app/components/mandatarissen/timeline.js
+++ app/components/mandatarissen/timeline.js
@@ -2,79 +2,9 @@ import Component from '@glimmer/component';
 import { service } from '@ember/service';
 import moment from 'moment';
 
-import { trackedFunction } from 'reactiveweb/function';
-import { use } from 'ember-resources';
-
-async function getCorrections(mandataris) {
-  const result = await fetch(
-    `/form-content/mandataris-edit/instances/${mandataris.id}/history`
-  );
-
-  const json = await result.json();
-  return json.instances;
-}
-function getHistory() {
-  return trackedFunction(async () => {
-    const mandatarissen = this.args.mandatarissen;
-    const newHistory = await Promise.all(
-      mandatarissen.map(async (mandataris) => {
-        let corrections = await getCorrections(mandataris);
-        const historyEntry = {
-          mandataris,
-          corrections,
-        };
-        return historyEntry;
-      })
-    );
-
-    const userIdsInHistory = new Set();
-    newHistory.forEach((h) => {
-      h.corrections.forEach((c) => {
-        userIdsInHistory.add(c.creator);
-      });
-    });
-
-    let users = [];
-    if (userIdsInHistory.size !== 0) {
-      users = await this.store.query('gebruiker', {
-        filter: {
-          id: Array.from(userIdsInHistory).join(','),
-        },
-      });
-    }
-
-    const userIdToUser = {};
-    users.forEach((u) => {
-      userIdToUser[u.id] = u;
-    });
-
-    return newHistory
-      .map((h) => {
-        return {
-          ...h,
-          corrections: h.corrections.map((c) => ({
-            ...c,
-            creator: userIdToUser[c.creator],
-          })),
-        };
-      })
-      .sort((a, b) => {
-        if (!b?.mandataris?.start) {
-          return -1;
-        }
-        if (!a?.mandataris?.start) {
-          return 1;
-        }
-        return b.mandataris.start.getTime() - a.mandataris.start.getTime();
-      });
-  });
-}
-
 export default class MandatarissenTimeline extends Component {
   @service features;
   @service store;
-
-  @use(getHistory) history;
 
   get isFeatureEnabled() {
     return this.features.isEnabled('shacl-report');
@@ -102,9 +32,9 @@ export default class MandatarissenTimeline extends Component {
         selected: false,
       });
     }
-    if (this.history?.value) {
+    if (this.args.history) {
       events.forEach((event) => {
-        event.corrections = this.history.value.find(
+        event.corrections = this.args.history.find(
           (h) => h.mandataris.id === event.mandataris.id
         )?.corrections;
       });
