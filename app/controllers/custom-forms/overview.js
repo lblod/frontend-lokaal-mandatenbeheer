7ac3502d41902419ab6c715ad900cdd60554bfd5
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-03-27 15:49:35 +0100

    lmb-1214: add button to remove the form definition (no usage check)

--- app/controllers/custom-forms/overview.js
+++ app/controllers/custom-forms/overview.js
@@ -1,8 +1,32 @@
 import Controller from '@ember/controller';
 
+import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { service } from '@ember/service';
+
+import { showErrorToast, showSuccessToast } from 'frontend-lmb/utils/toasts';
 
 export default class CustomFormsOverviewController extends Controller {
+  @service toaster;
+
   @tracked page = 0;
   @tracked size = 20;
+
+  @action
+  async deleteForms(formModel) {
+    try {
+      await formModel.destroyRecord();
+      showSuccessToast(
+        this.toaster,
+        'Formulier definitie and instances succesvol verwijderd',
+        'Formulier'
+      );
+    } catch (error) {
+      showErrorToast(
+        this.toaster,
+        'Er liep iets mis bij het verwijderen van het formulier',
+        'Formulier'
+      );
+    }
+  }
 }
