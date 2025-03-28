cfff3b5a17b4e7dccbca50f692933e7df6a8dc73
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-03-28 14:08:42 +0100

    lmb-1218: fetch if the form has usages in the instances route

--- app/controllers/custom-forms/instances/index.js
+++ app/controllers/custom-forms/instances/index.js
@@ -5,13 +5,20 @@ import { A } from '@ember/array';
 import { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 
-import { task, timeout } from 'ember-concurrency';
 import { SEARCH_TIMEOUT } from 'frontend-lmb/utils/constants';
+
+import { task, timeout } from 'ember-concurrency';
+import { showErrorToast, showSuccessToast } from 'frontend-lmb/utils/toasts';
 
 export default class CustomFormsInstancesIndexController extends Controller {
   queryParams = ['page', 'size', 'sort', 'filter'];
 
   @service router;
+  @service toaster;
+
+  @tracked isDeleteModalOpen;
+  @tracked isDeleting;
+
   @tracked isEditFormDefinitionOpen;
   @tracked page = 0;
   @tracked sort = 'uri';
@@ -60,5 +67,48 @@ export default class CustomFormsInstancesIndexController extends Controller {
       return;
     }
     throw new Error('Niet bewaren aub', ttl);
+  }
+
+  @action
+  openDeleteFormModal(formModel) {
+    this.isDeleteModalOpen = true;
+    this.formToDelete = formModel;
+  }
+
+  @action
+  async deleteForm() {
+    this.isDeleting = true;
+    try {
+      await this.model.form.destroyRecord();
+      showSuccessToast(
+        this.toaster,
+        'Formulier definitie and instances succesvol verwijderd',
+        'Formulier'
+      );
+    } catch (error) {
+      showErrorToast(
+        this.toaster,
+        'Er liep iets mis bij het verwijderen van het formulier',
+        'Formulier'
+      );
+    }
+    this.isDeleting = false;
+    this.isDeleteModalOpen = false;
+    this.formToDelete = null;
+  }
+
+  get removeFormModalText() {
+    if (this.model.usage.hasUsage) {
+      const usageCount = this.model.usage.usageUris.length;
+      let text = (variableText) =>
+        `${variableText} van dit type gevonden. Door verder te gaan zal de definitie met zijn implementaties definitief verwijderd worden.`;
+
+      if (usageCount > 1) {
+        return text(`Er werden ${usageCount} formulieren `);
+      }
+      return text(`Er werd ${usageCount} formulier `);
+    }
+
+    return 'Door verder te gaan zal de formulier definitie verwijderd worden.';
   }
 }
