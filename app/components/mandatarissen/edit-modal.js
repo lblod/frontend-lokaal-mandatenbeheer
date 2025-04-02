0beac231b972aee7af4380b41716cb0a571c2052
Ben Fidlers <ben.fidlers@qmino.com>
2025-04-02 15:12:23 +0200

    add second stage modal

--- app/components/mandatarissen/edit-modal.js
+++ app/components/mandatarissen/edit-modal.js
@@ -1,10 +1,13 @@
 import Component from '@glimmer/component';
 
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class MandatarissenEditModalComponent extends Component {
+  @tracked isSecondModalOpen = false;
+
   get hasChanges() {
-    return false;
+    return true;
   }
 
   get disabled() {
@@ -16,7 +19,23 @@ export default class MandatarissenEditModalComponent extends Component {
   }
 
   @action
+  confirmMandatarisChanges() {
+    this.args.onClose();
+    this.isSecondModalOpen = true;
+  }
+
+  @action
+  saveForm() {
+    this.isSecondModalOpen = false;
+  }
+
+  @action
   cancel() {
     this.args.onClose();
+  }
+
+  @action
+  cancelSecondModal() {
+    this.isSecondModalOpen = false;
   }
 }
