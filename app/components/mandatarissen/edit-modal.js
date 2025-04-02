033e31fbddbcdd48b0a20e3266d8b54d09f20cfc
Ben Fidlers <ben.fidlers@qmino.com>
2025-04-02 15:29:58 +0200

    add content to second stage edit mandataris

--- app/components/mandatarissen/edit-modal.js
+++ app/components/mandatarissen/edit-modal.js
@@ -1,10 +1,16 @@
 import Component from '@glimmer/component';
 
-import { action } from '@ember/object';
+import { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { showErrorToast, showSuccessToast } from 'frontend-lmb/utils/toasts';
 
 export default class MandatarissenEditModalComponent extends Component {
+  @service toaster;
+
   @tracked isSecondModalOpen = false;
+  @tracked reasonForChangeOptions = ['Update state', 'Corrigeer fouten'];
+  @tracked reasonForChange;
 
   get hasChanges() {
     return true;
@@ -25,7 +31,27 @@ export default class MandatarissenEditModalComponent extends Component {
   }
 
   @action
+  selectReasonForChange(reason) {
+    this.reasonForChange = reason;
+  }
+
+  @action
   saveForm() {
+    if (this.reasonForChange == 'Corrigeer fouten') {
+      // Add corrigeer fouten logic here
+      console.log('Corrigeer fouten');
+    } else if (this.reasonForChange == 'Update state') {
+      // Add update state logic here
+      console.log('Update state');
+    } else {
+      showErrorToast(
+        this.toaster,
+        'Geen geldige reden voor aanpassing geselecteerd'
+      );
+      return;
+    }
+    showSuccessToast(this.toaster, 'De mandataris werd succesvol aangepast');
+    this.reasonForChange = null;
     this.isSecondModalOpen = false;
   }
 
