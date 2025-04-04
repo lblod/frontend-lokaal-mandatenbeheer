0092ef485462777ce17b1277fce4907e9ee846b8
Ben Fidlers <ben.fidlers@qmino.com>
2025-04-04 13:11:38 +0200

    add some better safety checking greaterThan and lessThan validations

--- app/utils/form-validations/greater-than.js
+++ app/utils/form-validations/greater-than.js
@@ -1,4 +1,6 @@
-import { EXT } from 'frontend-lmb/rdf/namespaces';
+import { EXT, SHACL, XSD } from 'frontend-lmb/rdf/namespaces';
+import { Literal } from 'rdflib';
+import moment from 'moment';
 
 export function greaterThan([inputValue], options) {
   const comparisonPredicate = options.store.any(
@@ -16,7 +18,22 @@ export function greaterThan([inputValue], options) {
   if (!threshold) {
     return false;
   }
-  return inputValue.value >= threshold.value;
+
+  const inputLiteral = Literal.toJS(inputValue);
+  const thresholdLiteral = Literal.toJS(threshold);
+
+  const isDateTime = options.store.match(
+    options.constraintUri,
+    SHACL('datatype'),
+    XSD('dateTime'),
+    options.formGraph
+  ).length;
+
+  if (isDateTime) {
+    return moment(inputLiteral).isAfter(moment(thresholdLiteral));
+  } else {
+    return inputLiteral > thresholdLiteral;
+  }
 }
 
 function getValue(predicate, options) {
