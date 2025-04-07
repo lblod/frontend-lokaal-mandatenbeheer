73f8971d2532075ae547f4a72b5c9fefc69c4290
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-04-07 11:17:57 +0200

    lmb-1502: the form types are shown in the dropdown

--- app/components/custom-form/link-to-form-instance.js
+++ app/components/custom-form/link-to-form-instance.js
@@ -4,15 +4,21 @@ import { A } from '@ember/array';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
+import { trackedFunction } from 'reactiveweb/function';
+import { use } from 'ember-resources';
+
+import { API } from 'frontend-lmb/utils/constants';
 export default class CustomFormLinkToFormInstance extends Component {
   @tracked formType;
   @tracked form;
   @tracked fields = A([]);
   @tracked selectedFormFields = A([]);
 
+  @use(getFormTypes) getFormTypes;
+
   @action
   selectFormType(type) {
-    console.log({ type });
+    this.formType = type;
   }
 
   @action
@@ -26,7 +32,21 @@ export default class CustomFormLinkToFormInstance extends Component {
   }
 
   get formTypes() {
-    return [];
+    const types = this.getFormTypes?.value;
+
+    if (!types) {
+      return [];
+    }
+    return [
+      {
+        groupName: 'Eigen types',
+        options: types.customTypes,
+      },
+      {
+        groupName: 'Standaar types',
+        options: types.defaultTypes,
+      },
+    ];
   }
 
   get forms() {
@@ -36,4 +56,18 @@ export default class CustomFormLinkToFormInstance extends Component {
   get fieldOptions() {
     return [];
   }
+}
+
+function getFormTypes() {
+  return trackedFunction(async () => {
+    const response = await fetch(
+      `${API.FORM_CONTENT_SERVICE}/custom-form/form-types`
+    );
+
+    if (!response.ok) {
+      console.error('Er ging iets mis bij het ophalen van de formulier types');
+      return [];
+    }
+    return await response.json();
+  });
 }
