c1ce0dace93fb4237b1ccfa293bcb6cec5802fd0
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-04-10 13:58:45 +0200

    lmb-1531: add the refresh button to the report page and add basic element for shopwing the refresh button

--- app/components/refresh-validations-button.js
+++ app/components/refresh-validations-button.js
@@ -1,3 +1,18 @@
 import Component from '@glimmer/component';
 
-export default class RefreshValidationsButton extends Component {}
+import { tracked } from '@glimmer/tracking';
+
+import { task } from 'ember-concurrency';
+import moment from 'moment';
+
+export default class RefreshValidationsButton extends Component {
+  @tracked lastSyncDate = new Date();
+
+  refresh = task(async () => {
+    this.lastSyncDate = new Date();
+  });
+
+  get lastRefreshedDate() {
+    return moment(this.lastSyncDate).format('DD-MM-YYYY HH:mm:ss');
+  }
+}
