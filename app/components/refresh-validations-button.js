faaed1252b551568e50f1bc7cd93bb30e9bd6514
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-04-10 14:37:40 +0200

    lmb-1531: add a spinning renew icon when loading

--- app/components/refresh-validations-button.js
+++ app/components/refresh-validations-button.js
@@ -1,11 +1,14 @@
 import Component from '@glimmer/component';
 
+import { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 
 import { task } from 'ember-concurrency';
 import moment from 'moment';
 
 export default class RefreshValidationsButton extends Component {
+  @service validatie;
+
   @tracked lastSyncDate = new Date();
 
   refresh = task(async () => {
@@ -14,5 +17,9 @@ export default class RefreshValidationsButton extends Component {
 
   get lastRefreshedDate() {
     return moment(this.lastSyncDate).format('DD-MM-YYYY HH:mm:ss');
+  }
+
+  get isLoading() {
+    return !this.validatie.hasReport;
   }
 }
