752694be138d059b1fc57506858585ae0fbc3bf9
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-04-14 16:16:22 +0200

    lmb-1478: shwo the fields of the form in the dropdown

---
+++ app/components/custom-form/edit-custom-field.js
@@ -0,0 +1,45 @@
+import Component from '@glimmer/component';
+
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+import { trackedFunction } from 'reactiveweb/function';
+import { use } from 'ember-resources';
+
+import { API } from 'frontend-lmb/utils/constants';
+import { showErrorToast } from 'frontend-lmb/utils/toasts';
+
+export default class CustomFormEditCustomField extends Component {
+  @use(getFieldsForForm) getFieldsForForm;
+
+  @tracked selectedField;
+
+  get fields() {
+    return this.getFieldsForForm?.value || [];
+  }
+
+  @action
+  updateSelectedField(field) {
+    this.selectedField = field;
+  }
+}
+
+function getFieldsForForm() {
+  return trackedFunction(async () => {
+    const response = await fetch(
+      `${API.FORM_CONTENT_SERVICE}/custom-form/${this.args.formDefinitionId}/fields`
+    );
+
+    if (!response.ok) {
+      showErrorToast(
+        this.toaster,
+        `Er liep iets mis bij het ophalen van de velden voor formulier met id: ${this.args.formDefinitionId}`,
+        'Formulier'
+      );
+    }
+
+    const result = await response.json();
+
+    return result.fields;
+  });
+}
