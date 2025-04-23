b5149a5a39d69d46bd3777690b1e2fb5af3d0df1
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-04-23 09:33:15 +0200

    lmb-1502: fetcvh the initial selected instances and combine them with the other instances

--- app/components/custom-form/link-to-form-instance.js
+++ app/components/custom-form/link-to-form-instance.js
@@ -22,17 +22,19 @@ export default class CustomFormLinkToFormInstance extends SelectorComponent {
 
   @use(getAllFormLabels) getAllFormLabels;
   @use(getInstances) getInstances;
+  @use(getInitialSelectedInstances) getInitialSelectedInstances;
 
   @consume('form-context') formContext;
 
-  // @tracked formOptions = A([]);
-  // @tracked summaryLabels = [];
+  @tracked extraInstanceUris = [];
   @tracked selectedInstances = A([]);
   @tracked instanceDisplayLabels = [];
   @tracked pageToLoad = 0;
   @tracked searchFilter;
-  // @tracked isLoadingMoreOptions;
-  @tracked canShowLoadMoreOptions = true;
+  @tracked isLoadingMoreOptions;
+  @tracked areAllInstancesFetched;
+
+  LOAD_MORE_ID = 'load-more-instances';
 
   get formTypeId() {
     return this.storeOptions.store.any(
@@ -51,104 +53,83 @@ export default class CustomFormLinkToFormInstance extends SelectorComponent {
     return this.getInstances?.value || [];
   }
 
+  get initialSelectedInstances() {
+    return this.getInitialSelectedInstances?.value || [];
+  }
+
   get instancesAsOptions() {
     const keysOfLabels = this.instanceDisplayLabels.map((label) => label.name);
-    return this.instances.map((instance) => {
-      return {
+    const uniqueInstanceIds = [];
+    const instances = [];
+    for (const instance of [
+      ...this.instances,
+      ...this.initialSelectedInstances,
+    ]) {
+      if (uniqueInstanceIds.includes(instance.id)) {
+        continue;
+      } else {
+        uniqueInstanceIds.push(instance.id);
+      }
+      instances.push({
         option: keysOfLabels.map((key) => {
           return {
             key: key,
             value: instance[key],
           };
         }),
-        id: instance.id,
-      };
-    });
+        uri: instance.uri,
+      });
+    }
+
+    if (!this.areAllInstancesFetched) {
+      instances.push({
+        id: this.LOAD_MORE_ID,
+      });
+    }
+
+    return instances;
   }
 
-  async loadOptions() {}
+  async loadOptions() {
+    const matches = triplesForPath(this.storeOptions);
+    if (matches.values.length > 0) {
+      this.extraInstanceUris.push(...matches.values.map((v) => v.value));
+    }
+  }
 
   @action
-  selectInstance(selectedInstances) {
+  async selectInstance(selectedInstances) {
+    if (
+      selectedInstances.filter((f) => f.id === this.LOAD_MORE_ID)?.length === 1
+    ) {
+      this.isLoadingMoreOptions = true;
+      // await this.setFormOptions();
+      this.isLoadingMoreOptions = false;
+      return;
+    }
+
     this.selectedInstances.clear();
     this.selectedInstances.pushObjects(selectedInstances);
     const matches = triplesForPath(this.storeOptions, true).values;
-    console.log({ matches });
     matches
       .filter(
-        (m) => !selectedInstances.find((instance) => m.value === instance.id)
+        (m) => !selectedInstances.find((instance) => m.value === instance.uri)
       )
       .forEach((m) => updateSimpleFormValue(this.storeOptions, undefined, m));
     selectedInstances
-      .filter((instance) => !matches.find((m) => instance.id === m.value))
+      .filter((instance) => !matches.find((m) => instance.uri === m.value))
       .forEach((option) =>
-        updateSimpleFormValue(this.storeOptions, new Literal(option.id))
+        updateSimpleFormValue(this.storeOptions, new Literal(option.uri))
       );
     super.updateSelectedItems();
   }
 
-  // @action
-  // async selectFormsOfType(forms) {
-  //   if (forms.filter((f) => f.id === 'load-more-options')?.length === 1) {
-  //     this.isLoadingMoreOptions = true;
-  //     await this.setFormOptions();
-  //     this.isLoadingMoreOptions = false;
-  //     return;
-  //   }
-  //   this.forms.clear();
-  //   this.forms.pushObjects(forms);
-  //   const matches = triplesForPath(this.storeOptions, true).values;
-  //   matches
-  //     .filter((m) => !forms.find((form) => m.value === form.uri))
-  //     .forEach((m) => updateSimpleFormValue(this.storeOptions, undefined, m));
-  //   forms
-  //     .filter((form) => !matches.find((m) => form.uri === m.value))
-  //     .forEach((option) =>
-  //       updateSimpleFormValue(this.storeOptions, new NamedNode(option.uri))
-  //     );
-  //   super.updateSelectedItems();
-  // }
-
-  // async setFormOptions(instanceUris = null) {
-  //   const formInstances = await this.fetchFormsForType(instanceUris);
-  //   if (!this.isLoadingMoreOptions) {
-  //     this.forms.clear();
-  //   }
-  //   if (formInstances.length === 0) {
-  //     return;
-  //   }
-  //   if (this.formOptions.length > 0) {
-  //     this.formOptions.popObject();
-  //   }
-  //   const addedOptionUris = this.formOptions.map((o) => o.instance?.uri);
-  //   const uniqueFormOptionsToAdd = formInstances.map((instance) => {
-  //     if (addedOptionUris.includes(instance.instance.uri)) {
-  //       return;
-  //     } else {
-  //       addedOptionUris.push(instance.instance.uri);
-  //     }
-  //     return {
-  //       id: instance.instance.id,
-  //       uri: instance.instance.uri,
-  //       values: Object.keys(instance.displayInstance).map((key) => {
-  //         return { key, value: instance.displayInstance[key] };
-  //       }),
-  //     };
-  //   });
-  //   this.formOptions.pushObjects(uniqueFormOptionsToAdd);
-  //   if (this.canShowLoadMoreOptions) {
-  //     this.formOptions.pushObject({
-  //       id: 'load-more-options',
-  //     });
-  //   }
-  // }
-
-  // @action
-  // preventCloseOnLoadMoreOptions() {
-  //   if (this.isLoadingMoreOptions) {
-  //     return false;
-  //   }
-  // }
+  @action
+  preventCloseOnLoadMoreOptions() {
+    if (this.isLoadingMoreOptions) {
+      return false;
+    }
+  }
 
   // searchForm = task({ restartable: true }, async (input) => {
   //   await timeout(INPUT_DEBOUNCE);
@@ -160,76 +141,6 @@ export default class CustomFormLinkToFormInstance extends SelectorComponent {
   //   await this.setFormOptions();
   // });
 
-  // @action
-  // async fetchFormsForType(instanceUris = null) {
-  //   const allLabels = await this.semanticFormRepository.getHeaderLabels(
-  //     this.formTypeId
-  //   );
-  //   const summaryLabels = allLabels.filter((label) => label.isShownInSummary);
-  //   const filterParams = {
-  //     labels: summaryLabels,
-  //     page: this.pageToLoad,
-  //     size: 20,
-  //     filter: this.searchFilter,
-  //   };
-  //   if (!this.searchFilter) {
-  //     delete filterParams.filter;
-  //   }
-
-  //   const instances = [];
-  //   const formInfo = await this.semanticFormRepository.fetchInstances(
-  //     { id: this.formTypeId },
-  //     filterParams
-  //   );
-  //   instances.push(...formInfo.instances);
-
-  //   this.summaryLabels = formInfo.labels;
-  //   const currentPage = formInfo.instances.meta.pagination.self.number;
-  //   const lastPage = formInfo.instances.meta.pagination.last.number;
-  //   this.pageToLoad =
-  //     formInfo.instances.meta.pagination.next?.number ?? currentPage;
-  //   this.canShowLoadMoreOptions = lastPage !== currentPage;
-  //   if (instanceUris) {
-  //     const formInfoForUris = await this.fetchInstancesForUris(instanceUris);
-  //     instances.push(...formInfoForUris.instances);
-  //   }
-
-  //   const addedInstanceUris = [];
-  //   return instances
-  //     .map((instance) => {
-  //       if (addedInstanceUris.includes(instance.uri)) {
-  //         return;
-  //       } else {
-  //         addedInstanceUris.push(instance.uri);
-  //       }
-
-  //       let cleanedUpInstance = {};
-  //       for (const label of formInfo.labels) {
-  //         cleanedUpInstance[label.name] = instance[label.name];
-  //       }
-  //       return {
-  //         displayInstance: cleanedUpInstance,
-  //         instance,
-  //       };
-  //     })
-  //     .filter((isInstance) => isInstance);
-  // }
-
-  // async loadOptions() {
-  //   const matches = triplesForPath(this.storeOptions);
-  //   console.log('matches', matches);
-  //   if (matches.values.length > 0) {
-  //     const selectedFormUris = matches.values.map((v) => v.value);
-  //     console.log('formTypeId', this.formTypeId);
-  //     if (this.formTypeId) {
-  //       await this.setFormOptions(selectedFormUris);
-  //       this.forms.pushObjects(
-  //         this.formOptions.filter((form) => selectedFormUris.includes(form.uri))
-  //       );
-  //     }
-  //   }
-  // }
-
   async fetchInstancesForUris(uris) {
     const response = await fetch(
       `${API.FORM_CONTENT_SERVICE}/${this.formTypeId}/get-instances-by-uri`,
@@ -239,7 +150,7 @@ export default class CustomFormLinkToFormInstance extends SelectorComponent {
           'Content-Type': JSON_API_TYPE,
         },
         body: JSON.stringify({
-          labels: this.summaryLabels,
+          labels: this.labelsForFormType,
           uris,
         }),
       }
@@ -251,12 +162,17 @@ export default class CustomFormLinkToFormInstance extends SelectorComponent {
       return null;
     }
 
-    return await response.json();
+    const result = await response.json();
+    return result.instances;
   }
 }
 
 function getInstances() {
   return trackedFunction(async () => {
+    if (!this.formTypeId) {
+      return [];
+    }
+
     const filterParams = {
       labels: this.labelsForFormType,
       page: this.pageToLoad,
@@ -273,23 +189,15 @@ function getInstances() {
     const pagination = formInfo.instances.meta.pagination;
     console.log({ pagination });
     if (this.pageToLoad === pagination.last.number) {
-      console.log(`all instances fetched`);
-      this.canShowLoadMoreOptions = false;
+      this.areAllInstancesFetched = true;
     }
     this.pageToLoad = pagination.next?.number || this.pageToLoad;
     this.instanceDisplayLabels = formInfo.labels;
-
-    const availableInstanceIds = this.instances.map((i) => i.id);
-    if (availableInstanceIds.length === 0) {
-      return formInfo.instances;
-    }
-    const newInstances = formInfo.instances.filter(
-      (i) => !availableInstanceIds.includes(i.id)
+    const instancesWithoutLoadMoreOption = this.instances.filter(
+      (i) => i.id !== this.LOAD_MORE_ID
     );
-    const instances = [...this.instances, ...newInstances];
-    instances.meta.pagination = pagination;
 
-    return instances;
+    return [...instancesWithoutLoadMoreOption, ...formInfo.instances];
   });
 }
 
@@ -299,5 +207,14 @@ function getAllFormLabels() {
       this.formTypeId
     );
     return allLabels.filter((label) => label.isShownInSummary);
+  });
+}
+
+function getInitialSelectedInstances() {
+  return trackedFunction(async () => {
+    if (!this.formTypeId || this.extraInstanceUris.length === 0) {
+      return [];
+    }
+    return await this.fetchInstancesForUris(this.extraInstanceUris);
   });
 }
