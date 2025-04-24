b02cc64cb8acb95be761de2814ca58260b04cb8e
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-04-24 09:52:00 +0200

    lmb-1460: copy date-input component content from mastrer branch

--- app/components/date-input.js
+++ app/components/date-input.js
@@ -5,115 +5,133 @@ import { guidFor } from '@ember/object/internals';
 
 import { restartableTask, timeout } from 'ember-concurrency';
 import moment from 'moment';
-import { trackedFunction } from 'reactiveweb/function';
-import { use } from 'ember-resources';
 
 import { INPUT_DEBOUNCE, NULL_DATE } from 'frontend-lmb/utils/constants';
 import { endOfDay } from 'frontend-lmb/utils/date-manipulation';
 
-function getWarningMessages() {
-  return trackedFunction(async () => {
-    if (this.errorMessage) {
-      return [];
-    }
-
-    const warnings = [];
-    if (
-      !isDateInRange(this.args.value, this.minDate, this.maxDate) &&
-      this.inputIsValidDate
-    ) {
-      const formattedMinDate = moment(this.minDate).format('DD-MM-YYYY');
-      const formattedMaxDate = moment(this.maxDate).format('DD-MM-YYYY');
-      if (this.minDate && this.maxDate) {
-        warnings.push(
-          `Kies een datum tussen ${formattedMinDate} en ${formattedMaxDate}.`
-        );
-      }
-
-      if (this.minDate && !this.maxDate) {
-        warnings.push(`Kies een datum vanaf ${formattedMinDate}`);
-      }
-
-      if (!this.minDate && this.maxDate) {
-        warnings.push(`Kies een datum tot ${formattedMaxDate}`);
-      }
-    }
-    return warnings;
-  });
-}
 export default class DateInputComponent extends Component {
   elementId = `date-${guidFor(this)}`;
-  @use(getWarningMessages) getWarningMessages;
 
   @tracked dateInputString;
-  @tracked inputHasBeenFocused;
+  @tracked warningMessage;
+  @tracked errorMessage;
+  @tracked invalidErrorMessage;
 
   constructor() {
     super(...arguments);
     if (this.args.value && isValidDate(this.args.value)) {
       let date;
       if (this.args?.endOfDay) {
-        date = endOfDay(this.args.value);
+        date = this.args.value;
         this.dateInputString = moment(date).format('DD-MM-YYYY');
       } else {
         date = moment(this.args.value).toDate();
         this.dateInputString = moment(this.args.value).format('DD-MM-YYYY');
       }
+      this.processDate(date);
     }
-  }
-
-  get inputIsValidDate() {
-    return isValidDate(this.args.value);
-  }
-
-  get warningMessages() {
-    return this.getWarningMessages?.value || [];
-  }
-
-  get showWarnings() {
-    return this.warningMessages.length >= 1;
-  }
-
-  get errorMessage() {
-    if (
-      !isValidDate(this.args.value) &&
-      this.args.isRequired &&
-      this.inputHasBeenFocused
-    ) {
-      return 'Dit is geen geldige datum';
-    }
-    return null;
-  }
-
-  get minDate() {
-    return isValidDate(this.args.from) ? this.args.from : null;
-  }
-
-  get maxDate() {
-    return isValidDate(this.args.to) ? this.args.to : null;
   }
 
   onChange = restartableTask(async (event) => {
     await timeout(INPUT_DEBOUNCE);
-    this.inputHasBeenFocused = true;
+
     const inputValue = event.target?.value;
     this.dateInputString = inputValue;
-    const date = moment(inputValue, 'DD-MM-YYYY', true)?.toDate() ?? null;
-    if (this.args?.endOfDay && date) {
-      this.args.onChange?.(endOfDay(date));
-    } else {
-      this.args.onChange?.(date);
+
+    const date = this.processDate(
+      moment(inputValue, 'DD-MM-YYYY', true).toDate()
+    );
+
+    if (!this.args.isRequired && !isValidDate(date)) {
+      this.invalidErrorMessage = null;
     }
+
+    if (!isValidDate(date)) {
+      this.args.onChange?.(null);
+      return;
+    }
+    this.args.onChange?.(date);
   });
+
+  isDateInRange(date, min, max) {
+    if (!date) {
+      return false;
+    }
+    if (!min && !max) {
+      return true;
+    }
+    if (!min && max) {
+      return moment(date).isSameOrBefore(moment(max));
+    }
+    if (!max && min) {
+      return moment(date).isSameOrAfter(moment(min));
+    }
+
+    return moment(date).isBetween(moment(min), moment(max), 'day', '[]');
+  }
+
+  processDate(date) {
+    if (this.args?.endOfDay) {
+      date = endOfDay(date);
+    }
+    if (!isValidDate(date)) {
+      this.invalidErrorMessage = `Datum is ongeldig.`;
+      this.errorMessage = null;
+      this.warningMessage = null;
+
+      return date;
+    }
+    this.invalidErrorMessage = null;
+
+    const minDate = isValidDate(this.args.from) ? this.args.from : null;
+    const maxDate =
+      isValidDate(this.args.to) &&
+      !moment(this.args.to).isSame(moment(NULL_DATE))
+        ? this.args.to
+        : null;
+
+    if (!this.isDateInRange(date, minDate, maxDate)) {
+      const stringMinDate = isValidDate(minDate)
+        ? moment(minDate).format('DD-MM-YYYY')
+        : null;
+      const stringMaxDate = isValidDate(maxDate)
+        ? moment(maxDate).format('DD-MM-YYYY')
+        : null;
+
+      this.warningMessage = this.getErrorMessageForDateRange(
+        stringMinDate,
+        stringMaxDate
+      );
+    } else {
+      this.warningMessage = null;
+    }
+
+    return date;
+  }
+
+  getErrorMessageForDateRange(minDate, maxDate) {
+    if (minDate && maxDate) {
+      return `Kies een datum tussen ${minDate} en ${maxDate}.`;
+    }
+
+    if (minDate && !maxDate) {
+      return `Kies een datum vanaf ${minDate}`;
+    }
+
+    if (!minDate && maxDate) {
+      return `Kies een datum tot ${maxDate}`;
+    }
+
+    return null;
+  }
+
+  get errorMessages() {
+    return `${this.invalidErrorMessage ?? ''} ${this.invalidErrorMessage ? '\n' : ''} ${this.errorMessage ?? ''}`;
+  }
 }
 
 export function isValidDate(date) {
-  return (
-    date &&
-    date instanceof Date &&
-    !isNaN(date) &&
-    !moment(date).isSame(moment(NULL_DATE))
-  );
+  return date && date instanceof Date && !isNaN(date);
 }
 
 export function isDateInRange(date, min, max) {
