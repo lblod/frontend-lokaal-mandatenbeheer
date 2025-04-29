5cd018385ff48114e245ae5dff2bef92a1a1bb6b
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-04-29 13:18:09 +0200

    lmb-1502: can get and set the instances by fetching by the formDefintionUri

--- app/components/custom-form/link-to-form-instance.js
+++ app/components/custom-form/link-to-form-instance.js
@@ -12,8 +12,6 @@ import {
 import { NamedNode } from 'rdflib';
 import { consume } from 'ember-provide-consume-context';
 import { task } from 'ember-concurrency';
-import { trackedFunction } from 'reactiveweb/function';
-import { use } from 'ember-resources';
 
 import { API, JSON_API_TYPE } from 'frontend-lmb/utils/constants';
 import { EXT } from 'frontend-lmb/rdf/namespaces';
@@ -22,8 +20,6 @@ export default class CustomFormLinkToFormInstance extends SelectorComponent {
   @service store;
   @service semanticFormRepository;
 
-  @use(getAllFormLabels) getAllFormLabels;
-
   @consume('form-context') formContext;
 
   @tracked instanceObjectsOfOptions = [];
@@ -31,7 +27,7 @@ export default class CustomFormLinkToFormInstance extends SelectorComponent {
   @tracked instances = A([]);
   @tracked selectedInstances = A([]);
   @tracked instanceDisplayLabels = [];
-  @tracked pageToLoad;
+  @tracked pageToLoad = 0;
   @tracked lastPageOfInstances;
   @tracked searchFilter;
   @tracked isLoadingMoreOptions;
@@ -45,17 +41,13 @@ export default class CustomFormLinkToFormInstance extends SelectorComponent {
     this.isSettingInitialSelectedOptions.perform();
   }
 
-  get formTypeId() {
+  get formTypeUri() {
     return this.storeOptions.store.any(
       this.args.field.uri,
       EXT('linkedFormType'),
       undefined,
       this.storeOptions.formGraph
     )?.value;
-  }
-
-  get labelsForFormType() {
-    return this.getAllFormLabels?.value || [];
   }
 
   get isFetchingInstances() {
@@ -121,23 +113,23 @@ export default class CustomFormLinkToFormInstance extends SelectorComponent {
     super.updateSelectedItems();
   }
 
-  async fetchInstancesForUris(uris) {
+  async fetchInstancesForUris(instanceUris) {
     const response = await fetch(
-      `${API.FORM_CONTENT_SERVICE}/${this.formTypeId}/get-instances-by-uri`,
+      `${API.FORM_CONTENT_SERVICE}/instances/by-form-definition-uri?page[number]=${this.pageToLoad}`,
       {
         method: 'POST',
         headers: {
           'Content-Type': JSON_API_TYPE,
         },
         body: JSON.stringify({
-          labels: this.labelsForFormType,
-          uris,
+          formDefinitionUri: this.formTypeUri,
+          instanceUris,
         }),
       }
     );
     if (!response.ok) {
       console.error(
-        `Er ging iets mis bij het ophalen van instances met uris ${uris.split(', ')}`
+        `Er ging iets mis bij het ophalen van instances met uris ${instanceUris.split(', ')}`
       );
       return null;
     }
@@ -172,14 +164,13 @@ export default class CustomFormLinkToFormInstance extends SelectorComponent {
   });
 
   getInstances = task({ enqueue: true }, async (page = -1, lastPage = 1) => {
-    if (!this.formTypeId || !lastPage || page >= lastPage) {
+    if (!this.formTypeUri || !lastPage || page >= lastPage) {
       return;
     }
 
     page++;
 
     const filterParams = {
-      labels: this.labelsForFormType,
       page,
       size: 20,
       filter: this.searchFilter,
@@ -187,24 +178,12 @@ export default class CustomFormLinkToFormInstance extends SelectorComponent {
     if (!this.searchFilter) {
       delete filterParams.filter;
     }
-    const formInfo = await this.semanticFormRepository.fetchInstances(
-      { id: this.formTypeId },
-      filterParams
-    );
+    const formInfo = await this.fetchInstancesForUris([], filterParams);
     this.instanceDisplayLabels = formInfo.labels;
     this.instances.pushObjects(formInfo.instances);
-    this.getInstances.perform(
-      page,
-      formInfo.instances.meta.pagination.last?.number
-    );
-  });
-}
-
-function getAllFormLabels() {
-  return trackedFunction(async () => {
-    const allLabels = await this.semanticFormRepository.getHeaderLabels(
-      this.formTypeId
-    );
-    return allLabels.filter((label) => label.isShownInSummary);
+    // this.getInstances.perform(
+    //   page,
+    //   formInfo.instances.meta.pagination.last?.number
+    // );
   });
 }
