540b39608b7645af938e96a163c831681ed459a0
karel kremer <karel.kremer@redpencil.io>
2025-04-30 10:02:58 +0200

    only show usage of person if the user actually clicks it

--- app/controllers/mandatarissen/persoon/index.js
+++ app/controllers/mandatarissen/persoon/index.js
@@ -1,12 +1,27 @@
 import Controller from '@ember/controller';
 import { service } from '@ember/service';
 import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class MandatarissenPersoonIndexController extends Controller {
   @service router;
+  @service features;
+
+  @tracked showUsage = false;
+
+  get canShowUsage() {
+    return (
+      this.features.isEnabled('editable-forms') && this.model.usages.length > 0
+    );
+  }
 
   @action
   onSave() {
     this.router.refresh();
+  }
+
+  @action
+  toggleShowUsage() {
+    this.showUsage = !this.showUsage;
   }
 }
