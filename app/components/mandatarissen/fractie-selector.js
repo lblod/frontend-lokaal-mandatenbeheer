67021e4744f297e77d055910f136d3702bbdee0f
Ben Fidlers <ben.fidlers@qmino.com>
2025-05-14 17:35:48 +0200

    remove isUpdatingState option from faction selector

--- app/components/mandatarissen/fractie-selector.js
+++ app/components/mandatarissen/fractie-selector.js
@@ -51,59 +51,18 @@ export default class MandatenbeheerFractieSelectorComponent extends Component {
       return;
     }
 
-    if (this.args.isUpdatingState) {
-      this.fractieOptions = await this.mandatarisApi.getMandatarisFracties(
-        this.args.mandataris.id
+    const samenwerkingsFracties =
+      await this.fractieApi.samenwerkingForBestuursperiode(
+        this.args.bestuursperiode.id
       );
-
-      if (this.fractieOptions.length == 0) {
-        const currentFractie = await this.persoonApi.getCurrentFractie(
-          this.person.id,
-          this.args.bestuursperiode.id
-        );
-        if (currentFractie) {
-          this.fractieOptions = [currentFractie];
-          if (currentFractie.isOnafhankelijk) {
-            return;
-          }
-        } else {
-          this.fractieOptions =
-            await this.fractieApi.samenwerkingForBestuursperiode(
-              this.args.bestuursperiode.id
-            );
-        }
-      } else if (
-        this.fractieOptions.length > 1 ||
-        this.fractieOptions.at(0).isOnafhankelijk
-      ) {
-        return;
-      }
-
-      // If the onafhankelijke fractie was already present, we returned earlier,
-      // so now we can add a new onafhankelijke fractie
+    const availableFractions = [...samenwerkingsFracties];
+    if (!this.args.isCreating) {
       let onafhankelijkeFractie =
         await this.fractieService.getOrCreateOnafhankelijkeFractie(
           this.person,
           this.args.bestuursperiode,
           this.args.bestuurseenheid
         );
-      this.fractieOptions = [...this.fractieOptions, onafhankelijkeFractie];
-      return;
-    }
-
-    // This is the correct mistakes, all fracties are possible. onafhankelijke is only possible if not creating
-    const samenwerkingsFracties =
-      await this.fractieApi.samenwerkingForBestuursperiode(
-        this.args.bestuursperiode.id
-      );
-    let onafhankelijkeFractie =
-      await this.fractieService.getOrCreateOnafhankelijkeFractie(
-        this.person,
-        this.args.bestuursperiode,
-        this.args.bestuurseenheid
-      );
-    const availableFractions = [...samenwerkingsFracties];
-    if (!this.args.isCreating) {
       availableFractions.push(onafhankelijkeFractie);
     }
     this.fractieOptions = availableFractions;
