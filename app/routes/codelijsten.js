47cfc4361d498baa3b0d9089d0a50da469969bde
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-06-03 15:24:42 +0200

    lmb-1519: add route check if feature is enable for eigen gegevens + codelijsten

--- app/routes/codelijsten.js
+++ app/routes/codelijsten.js
@@ -4,8 +4,14 @@ import { service } from '@ember/service';
 
 export default class CodelijstenRoute extends Route {
   @service session;
+  @service features;
+  @service router;
 
   beforeModel(transition) {
     this.session.requireAuthentication(transition, 'login');
+
+    if (this.features.isEnabled('editable-forms') === false) {
+      this.router.replaceWith('index');
+    }
   }
 }
