ac7ea54e5a6bf5559a5ae85af411aa63352ad24c
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-06-04 13:23:05 +0200

    lmb-1556: fix the add form not being latest ttl

--- app/routes/custom-forms/instances/new.js
+++ app/routes/custom-forms/instances/new.js
@@ -1,12 +1,16 @@
 import Route from '@ember/routing/route';
 
 import { service } from '@ember/service';
+
 export default class CustomFormsInstancesNewRoute extends Route {
   @service semanticFormRepository;
 
   async model() {
     const parent = this.modelFor('custom-forms.instances');
+    const formDefinition = await this.semanticFormRepository.getFormDefinition(
+      parent.form.id
+    );
 
-    return { formDefinition: parent.formDefinition };
+    return { formDefinition };
   }
 }
