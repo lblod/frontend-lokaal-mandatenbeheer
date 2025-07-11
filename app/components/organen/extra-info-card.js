820b24765f8673e5fcb2fef3eb1e6e3dc422459d
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-07-11 15:52:48 +0200

    lmb-1679: create orgaan extra info card

---
+++ app/components/organen/extra-info-card.js
@@ -0,0 +1,24 @@
+import Component from '@glimmer/component';
+
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { service } from '@ember/service';
+
+export default class OrganenExtraInfoCard extends Component {
+  @service router;
+
+  @tracked formInitialized = false;
+  @tracked isModalActive = false;
+
+  @action
+  noop() {
+    return;
+  }
+  @action toggleModal() {
+    this.isModalActive = !this.isModalActive;
+  }
+  @action onSave() {
+    this.isModalActive = false;
+    this.router.refresh();
+  }
+}
