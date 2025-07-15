e101cdddd10ee3ac13b9a83644d92e4cab500fa6
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-07-15 14:45:33 +0200

    lmb-1652: the predicaat input will check with the form-content service if it is valid

--- app/components/predicaat-input.js
+++ app/components/predicaat-input.js
@@ -1,62 +1,50 @@
 import Component from '@glimmer/component';
 
-import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
-import { isValidUri } from 'frontend-lmb/utils/is-valid-uri';
+
+import {
+  API,
+  INPUT_DEBOUNCE,
+  JSON_API_TYPE,
+} from 'frontend-lmb/utils/constants';
+
+import {} from 'reactiveweb/function';
+import { task, timeout } from 'ember-concurrency';
 
 export default class PredicaatInput extends Component {
   @tracked uri;
+  @tracked isValidUri = true;
+  @tracked isAllowed = true;
 
   get predicaatUri() {
     return this.uri || this.args.value;
   }
 
-  get isValid() {
-    if (!this.predicaatUri) {
-      return true;
-    }
-
-    return isValidUri(this.uri);
-  }
-
-  get isBlockedUri() {
-    if (!this.isValid) {
-      return false;
-    }
-
-    return [
-      'http://www.w3.org/1999/02/22-rdf-syntax-ns#>Type',
-      'http://lblod.data.gift/vocabularies/forms/displayType',
-      'http://lblod.data.gift/vocabularies/forms/includes',
-      'http://lblod.data.gift/vocabularies/forms/initGenerator',
-      'http://lblod.data.gift/vocabularies/forms/validatedBy',
-      'http://lblod.data.gift/vocabularies/forms/targetType',
-      'http://lblod.data.gift/vocabularies/forms/targetLabel',
-      'http://lblod.data.gift/vocabularies/forms/initGenerator',
-      'http://lblod.data.gift/vocabularies/forms/prototype',
-      'http://lblod.data.gift/vocabularies/forms/dataGenerator',
-      'http://lblod.data.gift/vocabularies/forms/shape',
-      'http://lblod.data.gift/vocabularies/forms/prefix',
-      'http://lblod.data.gift/vocabularies/forms/forType',
-      'http://lblod.data.gift/vocabularies/forms/showInSummary',
-      'http://www.w3.org/ns/shacl#name',
-      'http://www.w3.org/ns/shacl#order',
-      'http://www.w3.org/ns/shacl#datatype',
-      'http://www.w3.org/ns/shacl#path',
-      'http://www.w3.org/ns/shacl#group',
-      'http://www.w3.org/ns/shacl#severity',
-      'http://www.w3.org/ns/shacl#resultMessage',
-      'http://mu.semte.ch/vocabularies/ext/ValueToCompare',
-      'http://mu.semte.ch/vocabularies/ext/prefix',
-      'http://mu.semte.ch/vocabularies/ext/withHistory',
-      'http://mu.semte.ch/vocabularies/core/uuid',
-    ].includes(this.predicaatUri);
-  }
-
-  @action
-  onUpdate(event) {
+  onUpdate = task({ restartable: true }, async (event) => {
     this.uri = event.target?.value;
-    const isValid = this.isValid && !this.isBlockedUri;
-    this.args.onUpdate?.(this.uri, isValid);
-  }
+    this.args.onUpdate?.(this.uri, false);
+    await timeout(INPUT_DEBOUNCE);
+
+    const response = await fetch(
+      `${API.FORM_CONTENT_SERVICE}/custom-form/field/is-uri-allowed-as-path`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': JSON_API_TYPE,
+        },
+        body: JSON.stringify({
+          uri: this.uri,
+        }),
+      }
+    );
+    const jsonResponse = await response.json();
+    if (!response.ok) {
+      console.error({ jsonResponse });
+    }
+
+    this.isValidUri = jsonResponse?.isValidUri;
+    this.isAllowed = this.isValidUri ? jsonResponse?.isAllowed : true;
+
+    this.args.onUpdate?.(this.uri, this.isValidUri && this.isAllowed);
+  });
 }
