a5740e3e47d75a2d3fc0f32299aff0edb67ace08
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-07-18 16:07:15 +0200

    lmb-1703: mandataris table only hides it when it is OCMW and NOT bcsd

--- app/components/organen/mandataris-table.js
+++ app/components/organen/mandataris-table.js
@@ -9,15 +9,16 @@ export default class OrganenMandatarisTableComponent extends Component {
     return this.args.bestuursorgaan.containsPoliticalMandates;
   }
   get hidePublicationStatus() {
-    if (this.currentSession.group.isOCMW) {
-      return true;
-    }
-
     return Promise.all([
       this.args.bestuursorgaan.isDecretaal,
       this.args.bestuursorgaan.isPolitieraad,
-    ]).then(([isDecretaal, isPolitieRaad]) => {
-      return !isDecretaal || isPolitieRaad;
+      this.args.bestuursorgaan.isBCSD,
+    ]).then(([isDecretaal, isPolitieRaad, isBCSD]) => {
+      return (
+        !isDecretaal ||
+        isPolitieRaad ||
+        (!isBCSD && this.currentSession.group.isOCMW)
+      );
     });
   }
   get showOwnership() {
