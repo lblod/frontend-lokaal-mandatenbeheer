57fbcbbb103f75f0212b34cb83855c428cfe6e34
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-07-29 16:53:31 +0200

    lmb-1695: create replacement fractie component WIP

---
+++ app/components/fractie/create-replacement.js
@@ -0,0 +1,77 @@
+import Component from '@glimmer/component';
+
+import { action } from '@ember/object';
+import { service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import { showErrorToast } from 'frontend-lmb/utils/toasts';
+
+export default class FractieCreateReplacement extends Component {
+  @service toaster;
+  @service fractieApi;
+
+  @tracked label;
+  @tracked isInteractedWithField;
+  @tracked isLoading;
+
+  @action
+  updateLabel(event) {
+    this.isInteractedWithField = true;
+    this.label = event.target?.value;
+  }
+
+  @action
+  async createReplacement() {
+    this.isLoading = true;
+    try {
+      await this.fractieApi.createReplacement(
+        this.args.fractieId,
+        this.args.bestuursperiode?.id
+      );
+      this.isLoading = false;
+      this.args.onCompleted?.();
+    } catch (error) {
+      this.isLoading = false;
+      showErrorToast(
+        this.toaster,
+        'Er liep iets mis bij het updaten van de fractie',
+        'Fractie'
+      );
+    }
+  }
+
+  get errorMessage() {
+    if (!this.isInteractedWithField) {
+      return null;
+    }
+    return this.errorMessageMapping.find((error) => error.condition)?.message;
+  }
+
+  get errorMessageMapping() {
+    return [
+      {
+        condition: this.label?.trim() === '',
+        message: 'Dit veld is verplicht',
+      },
+      {
+        condition: this.args.fracties.find(
+          (fractie) =>
+            fractie.id !== this.args.fractieId &&
+            fractie.naam === this.label?.trim()
+        ),
+        message: 'Er is al een fractie met dit label',
+      },
+    ];
+  }
+
+  get isDisabled() {
+    return !this.label || this.isSameAsOriginal || this.errorMessage;
+  }
+
+  get isSameAsOriginal() {
+    return this.args.fracties.find(
+      (fractie) =>
+        this.args.fractieId === fractie.id &&
+        fractie.naam === this.label?.trim()
+    );
+  }
+}
