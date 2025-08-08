5ff6f984bc5dc57da21e8fb72a4bb5c3685bcbc1
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-08-08 14:51:18 +0200

    delete: fix lint + remove the unused method
    - Removed util to-user-readable-listing
    
    Refs: lmb-1695

--- app/components/mandaat/folded-fracties.js
+++ app/components/mandaat/folded-fracties.js
@@ -3,7 +3,6 @@ import Component from '@glimmer/component';
 import { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 
-import { toUserReadableListing } from 'frontend-lmb/utils/to-user-readable-listing';
 import moment from 'moment';
 import { task } from 'ember-concurrency';
 
