4a6f0d0d6d8e4e4355bf20a4408b2c0bccb785a4
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2026-09-07 11:56:18 +0200

    refactor: filter out the fractie options depending on the start and end date of the mandataris
    Refs: lmb-1859

--- app/components/mandatarissen/fractie-selector.js
+++ app/components/mandatarissen/fractie-selector.js
@@ -3,8 +3,9 @@ import Component from '@glimmer/component';
 import { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-
 import { task } from 'ember-concurrency';
+import moment from 'moment';
+
 import { showErrorToast } from 'frontend-lmb/utils/toasts';
 import { JSON_API_TYPE } from 'frontend-lmb/utils/constants';
 
@@ -63,7 +64,28 @@ export default class MandatenbeheerFractieSelectorComponent extends Component {
         );
       availableFractions.push(onafhankelijkeFractie);
     }
-    this.fractieOptions = availableFractions;
+    this.fractieOptions = availableFractions.filter((_fractie) => {
+      const mandatarisStart = this.args.mandatarisStartDate;
+      const mandatarisEnd = this.args.mandatarisEndDate;
+
+      const fractieStartsBeforeOrOnMandatarisStart =
+        !_fractie.startDate ||
+        !mandatarisStart ||
+        moment(_fractie.startDate).isSameOrBefore(mandatarisStart);
+
+      let fractieCoversMandatarisEnd;
+      if (mandatarisEnd) {
+        fractieCoversMandatarisEnd =
+          !_fractie.endDate ||
+          moment(_fractie.endDate).isSameOrAfter(mandatarisEnd);
+      } else {
+        fractieCoversMandatarisEnd = !_fractie.endDate;
+      }
+
+      return (
+        fractieStartsBeforeOrOnMandatarisStart && fractieCoversMandatarisEnd
+      );
+    });
   }
 
   @action
