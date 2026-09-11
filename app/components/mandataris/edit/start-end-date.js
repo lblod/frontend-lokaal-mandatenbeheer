65bc39d300c40d3452e37ebf661981f5fe1f23b5
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2026-09-11 11:00:07 +0200

    refactor: early return when the start or end datum is before the min max date
    Refs: lmb-1859

--- app/components/mandataris/edit/start-end-date.js
+++ app/components/mandataris/edit/start-end-date.js
@@ -63,19 +63,18 @@ export default class MandatarisEditStartEndDate extends Component {
     if (date && !isValidDate(date)) {
       return 'Dit is geen geldige datum';
     }
-    if (!isDateInRange(date, this.minDate, this.maxDate) && isValidDate(date)) {
+    if (moment(date).isBefore(moment(this.minDate), 'day')) {
+      return `Kies een datum vanaf ${moment(this.minDate).format('DD-MM-YYYY')}`;
+    }
+    if (moment(date).isAfter(moment(this.maxDate), 'day')) {
+      return `Kies een datum tot ${moment(this.maxDate).format('DD-MM-YYYY')}`;
+    }
+
+    if (!isDateInRange(date, this.minDate, this.maxDate)) {
       const formattedMinDate = moment(this.minDate).format('DD-MM-YYYY');
       const formattedMaxDate = moment(this.maxDate).format('DD-MM-YYYY');
       if (this.minDate && this.maxDate) {
         return `Kies een datum tussen ${formattedMinDate} en ${formattedMaxDate}.`;
-      }
-
-      if (this.minDate && !this.maxDate) {
-        return `Kies een datum vanaf ${formattedMinDate}`;
-      }
-
-      if (!this.minDate && this.maxDate) {
-        return `Kies een datum tot ${formattedMaxDate}`;
       }
     }
     return null;
